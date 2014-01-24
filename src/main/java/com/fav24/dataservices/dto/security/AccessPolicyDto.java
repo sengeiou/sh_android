e20174cd8b309ae25a6a3d3c127311d46f28c97c
Josep Miquel Vera Maiquez <josepmiquel.vera@fav24.com>
2014-01-24 18:00:13 +0000

    GMBACKEND-81 Construcción data-services

---
+++ src/main/java/com/fav24/dataservices/dto/security/AccessPolicyDto.java
@@ -0,0 +1,45 @@
+package com.fav24.dataservices.dto.security;
+
+import com.fav24.dataservices.dto.BaseDto;
+import com.fav24.dataservices.security.AccessPolicy;
+
+
+/**
+ * Clase que contiene la estructura de una petición de consulta de
+ * políticas de acceso
+ * 
+ * @author Fav24
+ */
+public class AccessPolicyDto extends BaseDto {
+
+	private static final long serialVersionUID = 2649617444051699918L;
+
+	private AccessPolicy accessPolicy;
+
+
+	/**
+	 * Retorna la estructura de políticas de acceso de las entidades.
+	 * 
+	 * @return la estructura de políticas de acceso de las entidades.
+	 */
+	public AccessPolicy getAccessPolicy() {
+		return accessPolicy;
+	}
+
+	/**
+	 * Asigna la estructura de políticas de acceso de las entidades.
+	 * 
+	 * @param generic La estructura de políticas de acceso de las entidades.
+	 */
+	public void setAccessPolicy(AccessPolicy accessPolicy) {
+		this.accessPolicy = accessPolicy;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return "AccessPolicyTO [accessPolicy=" + accessPolicy + ", toString()=" + super.toString() + "]";
+	}
+}
