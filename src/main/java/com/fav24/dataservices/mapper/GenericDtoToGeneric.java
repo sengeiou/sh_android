7c513073cd63f96d1709c651176f88b77cca14b3
Josep Miquel Vera Maiquez <josepmiquel.vera@fav24.com>
2014-02-06 10:55:18 +0000

    GMBACKEND-81 Construcción data-services

--- src/main/java/com/fav24/dataservices/mapper/GenericDtoToGeneric.java
+++ src/main/java/com/fav24/dataservices/mapper/GenericDtoToGeneric.java
@@ -1,12 +1,18 @@
 package com.fav24.dataservices.mapper;
 
+import java.util.ArrayList;
+
 import com.fav24.dataservices.domain.Generic;
+import com.fav24.dataservices.domain.Operation;
+import com.fav24.dataservices.domain.Requestor;
 import com.fav24.dataservices.dto.GenericDto;
+import com.fav24.dataservices.dto.OperationDto;
+import com.fav24.dataservices.exception.ServerException;
 
 
 /**
  * 
- * Clase encargada del mapeo entre el objeto de transferencia Generic y el objeto de dominio GenericDto.
+ * Clase encargada del mapeo entre el objeto de transferencia GenericDto y el objeto de dominio Generic.
  * 
  * @author Fav24
  *
@@ -17,7 +23,20 @@ public class GenericDtoToGeneric extends Mapper<GenericDto, Generic> {
 	 * {@inheritDoc}
 	 */
 	@Override
-	protected Generic map(GenericDto origin) {
-		return null;
+	protected Generic map(GenericDto origin) throws ServerException {
+
+		Generic generic = new Generic();
+
+		generic.setRequestor((Requestor) Mapper.Map(origin.getRequestor()));
+
+		if (origin.getOps() != null) {
+			generic.setOperations(new ArrayList<Operation>(origin.getOps().length));
+
+			for (OperationDto operation : origin.getOps()) {
+				generic.getOperations().add((Operation) Mapper.Map(operation));
+			}
+		}
+
+		return generic;
 	}
 }
--- src/main/java/com/fav24/dataservices/mapper/GenericToGenericDto.java
+++ src/main/java/com/fav24/dataservices/mapper/GenericToGenericDto.java
@@ -2,6 +2,9 @@ package com.fav24.dataservices.mapper;
 
 import com.fav24.dataservices.domain.Generic;
 import com.fav24.dataservices.dto.GenericDto;
+import com.fav24.dataservices.dto.OperationDto;
+import com.fav24.dataservices.dto.RequestorDto;
+import com.fav24.dataservices.exception.ServerException;
 
 
 /**
@@ -17,7 +20,21 @@ public class GenericToGenericDto extends Mapper<Generic, GenericDto> {
 	 * {@inheritDoc}
 	 */
 	@Override
-	protected GenericDto map(Generic origin) {
-		return null;
+	protected GenericDto map(Generic origin) throws ServerException {
+
+		GenericDto generic = new GenericDto();
+
+		generic.setRequestor((RequestorDto) Mapper.Map(origin.getRequestor()));
+
+		if (origin.getOperations() != null) {
+
+			generic.setOps(new OperationDto[origin.getOperations().size()]);
+
+			for (int i=0; i<origin.getOperations().size(); i++) {
+				generic.getOps()[i] = (OperationDto) Mapper.Map(origin.getOperations().get(i));
+			}
+		}
+
+		return generic;
 	}
 }
