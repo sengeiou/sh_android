a70ec88ec6eb3b769252f19219e5dfb339afc852
Josep Miquel Vera Maiquez <josepmiquel.vera@fav24.com>
2014-03-07 16:41:32 +0000

    GMBACKEND-81 Construcción data-services. Inclusión de la caché de RETRIEVE.

--- src/main/java/com/fav24/dataservices/service/MainService.java
+++ src/main/java/com/fav24/dataservices/service/MainService.java
@@ -22,6 +22,13 @@ public interface MainService {
 	public static final String ERROR_STATS_DATASOURCE_GET_INFO_FAILED = "G001";
 	public static final String ERROR_STATS_DATASOURCE_GET_INFO_FAILED_MESSAGE = "Error al obtener la información de la fuente de datos de uso.";
 
+	public static final String MAX_MEMORY = "MaxMemory"; //Memória máxima a usar. Se configura mediante el parámetro - Xmx
+	public static final String TOTAL_MEMORY = "TotalMemory"; //Hasta donde se puede expandir el heap. Se configura mediante el parámetro -Xms
+	public static final String FREE_MEMORY = "FreeMemory"; //Memoria disponible en el sistema. 
+	public static final String USED_MEMORY = "UsedMemory"; //Memoria en uso. 
+
+	public static final String TOTAL_STORAGE_SPACE = "TotalStorageSpace"; //Capacidad total de almacenamiento.
+	public static final String TOTAL_USABLE_STORAGE_SPACE = "TotalUsableStorageSpace"; //Espacio disponible de almacenamiento.
 
 	/**
 	 * Retorna un mapa de atributos con la información de la fuente de datos a publicar.
@@ -40,4 +47,21 @@ public interface MainService {
 	 * @throws ServerException 
 	 */
 	public Map<String, String> getStatsDataSourceInformation() throws ServerException;
+	
+	/**
+	 * Retorna la información de estado de la memoria, en la máquina virtual.
+	 *  
+	 * @return la información de estado de la memoria, en la máquina virtual.
+	 */
+	public Map<String, Long> getSystemMemoryStatus();
+	
+	/**
+	 * Retorna un mapa con el conjunto de elementos de almacenamiento, y su información asociada en cuanto a:
+	 * 
+	 * {@linkplain #TOTAL_STORAGE_SPACE}
+	 * {@linkplain #TOTAL_USABLE_STORAGE_SPACE}
+	 * 
+	 * @return un mapa con el conjunto de elementos de almacenamiento, y su información asociada.
+	 */
+	public Map<String, Map<String, Long>> getSystemStorageStatus();
 }
--- src/main/java/com/fav24/dataservices/service/impl/MainServiceImpl.java
+++ src/main/java/com/fav24/dataservices/service/impl/MainServiceImpl.java
@@ -1,7 +1,13 @@
 package com.fav24.dataservices.service.impl;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,5 +72,54 @@ public class MainServiceImpl implements MainService {
 
 			throw new ServerException(ERROR_STATS_DATASOURCE_GET_INFO_FAILED, ERROR_STATS_DATASOURCE_GET_INFO_FAILED_MESSAGE);
 		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Map<String, Long> getSystemMemoryStatus() {
+
+		Map<String, Long> systemMemoryStatus = new HashMap<String, Long>();
+
+		long maxMemory = Runtime.getRuntime().maxMemory(); 
+		long totalMemory = Runtime.getRuntime().totalMemory();
+		long usedMemory = totalMemory - Runtime.getRuntime().freeMemory(); 
+		long freeMemory = maxMemory - usedMemory;
+
+		systemMemoryStatus.put(MAX_MEMORY, maxMemory);
+		systemMemoryStatus.put(TOTAL_MEMORY, totalMemory);
+		systemMemoryStatus.put(USED_MEMORY, usedMemory);
+		systemMemoryStatus.put(FREE_MEMORY, freeMemory);
+
+		return systemMemoryStatus;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Map<String, Map<String, Long>> getSystemStorageStatus() {
+
+		Map<String, Map<String, Long>> systemStorageStatus = new HashMap<String, Map<String, Long>>();
+
+		for (Path root : FileSystems.getDefault().getRootDirectories())
+		{
+			Map<String, Long> storageStatus = new HashMap<String, Long>();
+
+			try	{
+
+				FileStore store = Files.getFileStore(root);
+
+				storageStatus.put(TOTAL_STORAGE_SPACE, store.getTotalSpace());
+				storageStatus.put(TOTAL_USABLE_STORAGE_SPACE, store.getUsableSpace());
+
+				systemStorageStatus.put(root.toString(), storageStatus);
+			}
+			catch (IOException e) {
+			}
+		}
+
+		return systemStorageStatus;
 	}
 }
