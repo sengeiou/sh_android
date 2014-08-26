4d27df72bc786f8e7791efc6a6ee7a1f58845383
Mari Bañuls Ondara <mari.banyuls@fav24.com>
2014-08-26 10:30:53 +0000

    Configuració

---
+++ GolesTests/GolesTests.m
@@ -0,0 +1,34 @@
+//
+//  GolesTests.m
+//  GolesTests
+//
+//  Created by Christian Cabarrocas on 11/08/14.
+//  Copyright (c) 2014 Fav24. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GolesTests : XCTestCase
+
+@end
+
+@implementation GolesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
