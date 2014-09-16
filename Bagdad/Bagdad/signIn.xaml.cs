2244b597f7057ecfccc300d5076aca9c1be15425
Teo Rodríguez Giménez <teo.rodriguez@fav24.com>
2014-09-16 10:45:04 +0000

    BGD-4 Login UI

---
+++ Bagdad/Bagdad/signIn.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Bagdad
+{
+    public partial class SignIn : PhoneApplicationPage
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: All the functions needed to Signing In
+        }
+
+        //Prevents the TextBox of autoChange Background color on got Focus
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Media.SolidColorBrush Brush2 = new System.Windows.Media.SolidColorBrush();
+            Brush2.Color = System.Windows.Media.Colors.Transparent;
+            ((TextBox)sender).Background = Brush2;
+        }
+
+        //Prevents the PasswordBox of autoChange Background color on got Focus
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Media.SolidColorBrush Brush2 = new System.Windows.Media.SolidColorBrush();
+            Brush2.Color = System.Windows.Media.Colors.Transparent;
+            ((PasswordBox)sender).Background = Brush2;
+        }
+    }
+}
