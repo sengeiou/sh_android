8b4a1d9cc77a84909c7a7bc63f9d58e5931e2d45
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-17 15:57:31 +0000

    BGD-4 Login Se ha añadido un status_server_failure para el login

--- app/src/main/java/gm/mobi/android/task/events/LoginResultEvent.java
+++ app/src/main/java/gm/mobi/android/task/events/LoginResultEvent.java
@@ -7,26 +7,35 @@ public class LoginResultEvent {
 
     public static final int STATUS_SUCCESS = 1;
     public static final int STATUS_INVALID = 2;
+    public static final int STATUS_SERVER_FAILURE = 3;
 
     private Exception error;
     private int status;
     private User signedUser;
-
+    private String code, message;
     // Private constructor, create through static methods
     private LoginResultEvent(int status) {
         this.status = status;
     }
 
-    //TODO pasar los parámetros necesarios para mantener la sesión
+    private LoginResultEvent(int status, String code, String message){
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
     public static LoginResultEvent successful(User user) {
         LoginResultEvent loginResultEvent = new LoginResultEvent(STATUS_SUCCESS);
         loginResultEvent.setSignedUser(user);
         return loginResultEvent;
-
     }
 
     public static LoginResultEvent invalid() {
         return new LoginResultEvent(STATUS_INVALID);
+    }
+
+    public static LoginResultEvent serverError(String code, String message){
+        return new LoginResultEvent(STATUS_SERVER_FAILURE,code,message);
     }
 
     public int getStatus() {
