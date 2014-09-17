709750ad8a2f3999bc6b187f250b266a9ea668e5
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-17 16:58:48 +0000

    BGD-11 TimeLine Create ShotMapper

---
+++ app/src/main/java/gm/mobi/android/db/mappers/ShotMapper.java
@@ -0,0 +1,56 @@
+package gm.mobi.android.db.mappers;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import gm.mobi.android.db.GMContract;
+import gm.mobi.android.db.objects.Shot;
+
+/**
+ * Created by InmaculadaAlcon on 17/09/2014.
+ */
+public class ShotMapper extends GenericMapper {
+
+    public static Shot fromCursor(Cursor c) {
+        Shot shot = new Shot();
+        shot.setIdShot(c.getInt(c.getColumnIndex(GMContract.ShotTable.ID_SHOT)));
+        shot.setIdUser(c.getInt(c.getColumnIndex(GMContract.ShotTable.ID_USER)));
+        shot.setComment(c.getString(c.getColumnIndex(GMContract.ShotTable.COMMENT)));
+        setSynchronizedFromCursor(c, shot);
+        return shot;
+    }
+
+    public static ContentValues toContentValues(Shot shot) {
+        ContentValues cv = new ContentValues();
+        cv.put(GMContract.ShotTable.ID_SHOT, shot.getIdShot());
+        cv.put(GMContract.ShotTable.ID_USER, shot.getIdUser());
+        cv.put(GMContract.ShotTable.COMMENT, shot.getComment());
+        setSynchronizedToContentValues(cv, shot);
+        return cv;
+    }
+
+    public static Shot fromDto(Map<String, Object> dto) {
+        Shot shot = new Shot();
+        shot.setIdShot((Integer) dto.get(GMContract.ShotTable.ID_SHOT));
+        shot.setIdUser((Integer) dto.get(GMContract.ShotTable.ID_USER));
+        shot.setComment((String) dto.get(GMContract.ShotTable.COMMENT));
+        setSynchronizedFromDto(dto, shot);
+        return shot;
+    }
+
+    public static Map<String, Object> toDto(Shot shot) {
+        Map<String, Object> dto = new HashMap<>();
+
+        dto.put(GMContract.ShotTable.ID_SHOT, shot == null ? null : shot.getIdShot());
+        dto.put(GMContract.ShotTable.ID_USER, shot == null ? null : shot.getIdUser());
+        dto.put(GMContract.ShotTable.COMMENT, shot == null ? null : shot.getComment());
+        setSynchronizedToDto(dto, shot);
+
+        return dto;
+    }
+
+}
+
