4b4e697ea248124186c84bbda5c2094f15768429
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-18 13:09:42 +0000

    BGD-11 Timeline He modificado el ID por ID user, que es lo que usamos en toda la app. Y en este caso afecta en el set id del user

--- app/src/debug/java/gm/mobi/android/service/BagdadMockService.java
+++ app/src/debug/java/gm/mobi/android/service/BagdadMockService.java
@@ -21,7 +21,7 @@ public class BagdadMockService implements BagdadService {
     public User login(String id, String password) {
         if (id.equals("rafa.vazsan@gmail.com") || id.equals("sloydev")) {
             User mockUser = new User();
-            mockUser.setId(1);
+            mockUser.setIdUser(1);
             mockUser.setName("Rafa");
             mockUser.setSessionToken("Nnananananananana");
             mockUser.setEmail("rafa.vazsan@gmail.com");
