29d70b19d4411abdd1ddaff9c6d85f7a308f4816
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-18 08:26:38 +0000

    BGD-4 Login throws JSONException en login

--- app/src/main/java/gm/mobi/android/service/BagdadService.java
+++ app/src/main/java/gm/mobi/android/service/BagdadService.java
@@ -1,12 +1,14 @@
 package gm.mobi.android.service;
 
+import org.json.JSONException;
+
 import java.io.IOException;
 
 import gm.mobi.android.db.objects.User;
 
 public interface BagdadService {
 
-    public User login(String id, String password) throws IOException;
+    public User login(String id, String password) throws IOException, JSONException;
 
 
 }
