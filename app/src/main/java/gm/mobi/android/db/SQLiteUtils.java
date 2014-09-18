f4c00e3538b3b987a7a26d56d166fab8ce89a622
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-18 13:14:02 +0000

    BGD-11 TimeLine Se ha añadido el CreateTable de Shot

--- app/src/main/java/gm/mobi/android/db/SQLiteUtils.java
+++ app/src/main/java/gm/mobi/android/db/SQLiteUtils.java
@@ -6,29 +6,42 @@ import gm.mobi.android.db.GMContract.*;
 
 public class SQLiteUtils {
 
-   public static final String CREATE_TABLE_USER = "CREATE TABLE IF NOT EXISTS " + UserTable.TABLE+ " ("
-           + UserTable.ID + " INT NOT NULL PRIMARY KEY,"
-           + UserTable.FAVOURITE_TEAM_ID + " INT NOT NULL,"
-           + UserTable.SESSION_TOKEN+ " VARCHAR(255) NULL,"
-           + UserTable.USER_NAME+ " VARCHAR(255) NULL,"
-           + UserTable.EMAIL+ " VARCHAR(255) NULL,"
-           + UserTable.NAME+ " VARCHAR(255) NULL,"
-           + UserTable.PHOTO+ " VARCHAR(1024) NOT NULL,"
-           + SyncColumns.CSYS_BIRTH + " DATETIME NOT NULL,"
-           + SyncColumns.CSYS_MODIFIED + " DATETIME NOT NULL,"
-           + SyncColumns.CSYS_DELETED + " DATETIME NULL,"
-           + SyncColumns.CSYS_REVISION + " INT NOT NULL,"
-           + SyncColumns.CSYS_SYNCHRONIZED + " CHAR(1) NULL);";
+    public static final String CREATE_TABLE_USER = "CREATE TABLE IF NOT EXISTS " + UserTable.TABLE + " ("
+            + UserTable.ID + " INT NOT NULL PRIMARY KEY,"
+            + UserTable.FAVOURITE_TEAM_ID + " INT NOT NULL,"
+            + UserTable.SESSION_TOKEN + " VARCHAR(255) NULL,"
+            + UserTable.USER_NAME + " VARCHAR(255) NULL,"
+            + UserTable.EMAIL + " VARCHAR(255) NULL,"
+            + UserTable.NAME + " VARCHAR(255) NULL,"
+            + UserTable.PHOTO + " VARCHAR(1024) NOT NULL,"
+            + SyncColumns.CSYS_BIRTH + " DATETIME NOT NULL,"
+            + SyncColumns.CSYS_MODIFIED + " DATETIME NOT NULL,"
+            + SyncColumns.CSYS_DELETED + " DATETIME NULL,"
+            + SyncColumns.CSYS_REVISION + " INT NOT NULL,"
+            + SyncColumns.CSYS_SYNCHRONIZED + " CHAR(1) NULL);";
 
-    public static final String CREATE_TABLE_SHOT = "CREATE TABLE IF NOT EXISTS "+ShotTable.TABLE+" ("
-            + ShotTable.ID_SHOT+" INT NOT NULL,"
-            + ShotTable.ID_USER+" INT NOT NULL,"
-            + ShotTable.COMMENT+" VARCHAR(255) NOT NULL,"
-            + SyncColumns.CSYS_BIRTH+" DATETIME NOT NULL,"
-            + SyncColumns.CSYS_MODIFIED+" DATETIME NOT NULL,"
-            + SyncColumns.CSYS_DELETED+" DATETIME NULL,"
-            + SyncColumns.CSYS_REVISION+" INT NOT NULL,"
-            + SyncColumns.CSYS_SYNCHRONIZED+" CHAR(1) NULL," +
-            " PRIMARY KEY("+ShotTable.ID_SHOT+"), " +
-            " CONSTRAINT 'FK_SHOT_USER1' FOREIGN KEY("+ShotTable.ID_USER+") REFERENCES "+UserTable.TABLE+"("+ShotTable.ID_USER+") ON DELETE NO ACTION ON UPDATE NO ACTION );";
+    public static final String CREATE_TABLE_SHOT = "CREATE TABLE IF NOT EXISTS " + ShotTable.TABLE + " ("
+            + ShotTable.ID_SHOT + " INT NOT NULL,"
+            + ShotTable.ID_USER + " INT NOT NULL,"
+            + ShotTable.COMMENT + " VARCHAR(255) NOT NULL,"
+            + SyncColumns.CSYS_BIRTH + " DATETIME NOT NULL,"
+            + SyncColumns.CSYS_MODIFIED + " DATETIME NOT NULL,"
+            + SyncColumns.CSYS_DELETED + " DATETIME NULL,"
+            + SyncColumns.CSYS_REVISION + " INT NOT NULL,"
+            + SyncColumns.CSYS_SYNCHRONIZED + " CHAR(1) NULL," +
+            " PRIMARY KEY(" + ShotTable.ID_SHOT + "), " +
+            " CONSTRAINT 'FK_SHOT_USER1' FOREIGN KEY(" + ShotTable.ID_USER + ") REFERENCES " + UserTable.TABLE + "(" + ShotTable.ID_USER + ") ON DELETE NO ACTION ON UPDATE NO ACTION );";
+
+    public static final String CREATE_TABLE_FOLLOW = "CREATE TABLE IF NOT EXISTS " + FollowTable.TABLE + " ("
+            + FollowTable.ID_USER + " INT NOT NULL,"
+            + FollowTable.ID_FOLLOWED_USER + " INT NOT NULL,"
+            + SyncColumns.CSYS_BIRTH + " DATETIME NOT NULL,"
+            + SyncColumns.CSYS_MODIFIED + " DATETIME NOT NULL,"
+            + SyncColumns.CSYS_DELETED + " DATETIME NULL,"
+            + SyncColumns.CSYS_REVISION + " INT NOT NULL,"
+            + SyncColumns.CSYS_SYNCHRONIZED + " CHAR(1) NULL,"
+            + " PRIMARY KEY(" + FollowTable.ID_USER + "," + FollowTable.ID_FOLLOWED_USER + ")"
+            + " CONSTRAINT 'fk_User_User1' FOREIGN KEY (" + FollowTable.ID_USER + ") REFERENCES " + UserTable.TABLE + "(" + UserTable.ID + ") ON DELETE NO ACTION ON UPDATE NO ACTION,"
+            + " CONSTRAINT 'fk_User_User2' FOREIGN KEY (" + FollowTable.ID_FOLLOWED_USER + ") REFERENCES " + UserTable.TABLE + "(" + UserTable.ID + ") ON DELETE NO ACTION ON UPDATE NO ACTION)";
+
 }
