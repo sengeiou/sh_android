398efc8afd23ecf6123f44a136048aa8b30fea81
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-19 07:04:12 +0000

    BGD-11 TimeLine He quitado el deprecated de Constants y he añadido allí, las variables de NEXUS_AND/NEXUS_OR, COMPARATORS...

--- app/src/main/java/gm/mobi/android/constant/Constants.java
+++ app/src/main/java/gm/mobi/android/constant/Constants.java
@@ -1,7 +1,28 @@
 package gm.mobi.android.constant;
 
 
-@Deprecated
 public class Constants {
     public static final Long ANDROID_PLATFORM  = 0L;
+    /** GenericDto Constants **/
+    // Nexus names
+    public static String NEXUS_AND = "and";
+    public static String NEXUS_OR = "or";
+
+    // Comparator
+    public static String COMPARATOR_NOT_EQUAL = "ne";
+    public static String COMPARATOR_EQUAL = "eq";
+    public static String COMPARATOR_GREAT_THAN = "gt";
+    public static String COMPARATOR_GREAT_EQUAL_THAN = "ge";
+    public static String COMPARATOR_LESS_THAN = "lt";
+    public static String COMPARATOR_LESS_EQUAL_THAN="le";
+
+    //Operation names
+    public static String OPERATION_RETRIEVE = "retrieve";
+    public static String OPERATION_CREATE = "create";
+    public static String OPERATION_CREATEUPDATE = "createupdate";
+    public static String OPERATION_UPDATE_CREATE = "updatecreate";
+    public static String OPERATION_DELETE = "delete";
+    public static String OPERATION_UPDATE = "update";
+
+
 }
