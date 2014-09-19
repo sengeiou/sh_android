11e6ff615e621483e6c4e4b88505fdca857fe1e5
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-19 07:30:25 +0000

    BGD-11 TimeLine Se ha cambiado el package de LoginResultEvent

--- app/src/main/java/gm/mobi/android/ui/activities/registro/EmailLoginActivity.java
+++ app/src/main/java/gm/mobi/android/ui/activities/registro/EmailLoginActivity.java
@@ -31,7 +31,7 @@ import gm.mobi.android.GolesApplication;
 import gm.mobi.android.R;
 import gm.mobi.android.db.objects.User;
 import gm.mobi.android.task.events.ConnectionNotAvailableEvent;
-import gm.mobi.android.task.events.LoginResultEvent;
+import gm.mobi.android.task.events.loginregister.LoginResultEvent;
 import gm.mobi.android.task.jobs.LoginUserJob;
 import gm.mobi.android.ui.activities.MainActivity;
 import gm.mobi.android.ui.base.BaseActivity;
