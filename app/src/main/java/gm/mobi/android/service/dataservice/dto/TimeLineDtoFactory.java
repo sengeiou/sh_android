053dcb8fdb0de8a6ad1f90b477a5a0d14c26db87
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-22 16:09:31 +0000

    TimeLineDtoFactory

---
+++ app/src/main/java/gm/mobi/android/service/dataservice/dto/TimeLineDtoFactory.java
@@ -0,0 +1,56 @@
+package gm.mobi.android.service.dataservice.dto;
+
+import android.content.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import gm.mobi.android.constant.Constants;
+import gm.mobi.android.db.GMContract;
+import gm.mobi.android.db.mappers.ShotMapper;
+import gm.mobi.android.service.dataservice.generic.FilterDto;
+import gm.mobi.android.service.dataservice.generic.FilterItemDto;
+import gm.mobi.android.service.dataservice.generic.GenericDto;
+import gm.mobi.android.service.dataservice.generic.MetadataDto;
+import gm.mobi.android.service.dataservice.generic.OperationDto;
+
+public class TimeLineDtoFactory extends DtoFactory{
+
+    public static final String ALIAS_GET_SHOTS =  "GET_SHOTS";
+
+    public static GenericDto getShotsOperationDto(List<Integer> followingUserIds, Long date, Context context){
+        OperationDto od = new OperationDto();
+        FilterDto filter = new FilterDto(Constants.NEXUS_AND,new FilterItemDto[]{},getShotsFilter(followingUserIds,new Date(date), context));
+        MetadataDto md = new MetadataDto(Constants.OPERATION_RETRIEVE, GMContract.ShotTable.TABLE, true, null, 0L, 1000L, filter);
+        od.setMetadata(md);
+        Map<String,Object>[] map = new HashMap[1];
+        map[0] = ShotMapper.toDto(null);
+        od.setData(map);
+        return UserDtoFactory.getGenericDtoFromOperation(ALIAS_GET_SHOTS, od);
+    }
+
+    public static FilterDto[] getShotsFilter(List<Integer> followingUserIds, Date date, Context context){
+        FilterDto[] mFilterDto = new FilterDto[2];
+        FilterItemDto[] mFilterItemDtos = new FilterItemDto[followingUserIds.size()];
+        int i = 0;
+        for(Integer userId: followingUserIds){
+            mFilterItemDtos[i] = new FilterItemDto(Constants.COMPARATOR_EQUAL, GMContract.ShotTable.ID_USER,userId);
+
+            i++;
+        }
+        mFilterDto[0] =
+                new FilterDto(Constants.NEXUS_OR,
+                        mFilterItemDtos
+                        ,null);
+        mFilterDto[1] = new FilterDto(Constants.NEXUS_OR,null,getTimeFilterDto(date, context));
+
+        return mFilterDto;
+    }
+
+
+
+
+
+}
