6996d60240c27ca987c31fd6a238a0f0516287b8
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-22 09:38:29 +0000

    Se ha añadido el método getFollows

--- app/src/main/java/gm/mobi/android/service/BagdadService.java
+++ app/src/main/java/gm/mobi/android/service/BagdadService.java
@@ -1,12 +1,17 @@
 package gm.mobi.android.service;
 
-import java.io.IOException;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
+import java.io.IOException;
+import java.util.List;
+
+import gm.mobi.android.db.objects.Follow;
 import gm.mobi.android.db.objects.User;
 
 public interface BagdadService {
 
     public User login(String id, String password) throws IOException;
-
+    public List<Follow> getFollows(Integer idUser, Context context, SQLiteDatabase db) throws IOException;
 
 }
