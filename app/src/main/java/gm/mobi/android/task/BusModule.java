81c48289f08de84c7fd3f025cdc0fbe3de840f05
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-22 16:05:41 +0000

    Adding UsersJob and ShotsJob

--- app/src/main/java/gm/mobi/android/task/BusModule.java
+++ app/src/main/java/gm/mobi/android/task/BusModule.java
@@ -13,6 +13,8 @@ import gm.mobi.android.task.jobs.loginregister.GetFacebookProfileJob;
 import gm.mobi.android.task.jobs.loginregister.LoginUserJob;
 import gm.mobi.android.task.jobs.loginregister.RegisterNewUserJob;
 import gm.mobi.android.task.jobs.timeline.FollowsJob;
+import gm.mobi.android.task.jobs.timeline.ShotsJob;
+import gm.mobi.android.task.jobs.timeline.UsersJob;
 import gm.mobi.android.ui.activities.registro.EmailLoginActivity;
 import gm.mobi.android.ui.activities.registro.EmailRegistrationActivity;
 import gm.mobi.android.ui.activities.registro.FacebookRegistroActivity;
@@ -21,6 +23,8 @@ import gm.mobi.android.ui.activities.registro.FacebookRegistroActivity;
         injects = {
                 GetFacebookProfileJob.class,
                 LoginUserJob.class,
+                UsersJob.class,
+                ShotsJob.class,
                 FollowsJob.class,
                 RegisterNewUserJob.class,
                 EmailLoginActivity.class,
