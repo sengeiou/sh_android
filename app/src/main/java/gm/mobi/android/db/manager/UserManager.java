0d0bdae87ef796235fd166831d20935d3bb76cb5
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-22 16:10:01 +0000

    Adding method saveUsers, for saving a list of Users

--- app/src/main/java/gm/mobi/android/db/manager/UserManager.java
+++ app/src/main/java/gm/mobi/android/db/manager/UserManager.java
@@ -6,10 +6,15 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.sql.SQLException;
+import java.util.List;
 
+import gm.mobi.android.db.GMContract;
 import gm.mobi.android.db.GMContract.*;
+import gm.mobi.android.db.mappers.FollowMapper;
 import gm.mobi.android.db.mappers.UserMapper;
+import gm.mobi.android.db.objects.Follow;
 import gm.mobi.android.db.objects.User;
+
 
 public class UserManager {
 
@@ -25,10 +30,23 @@ public class UserManager {
         return user;
     }
 
+    public static void saveUsers(SQLiteDatabase db, List<User> userList) throws  SQLException{
+        for(User user: userList){
+            ContentValues contentValues = UserMapper.toContentValues(user);
+            db.beginTransaction();
+            db.insertWithOnConflict(GMContract.UserTable.TABLE,null,contentValues,SQLiteDatabase.CONFLICT_REPLACE);
+            db.endTransaction();
+        }
+    }
+
     public static void saveUser(SQLiteDatabase db, User u) throws SQLException {
         ContentValues contentValues = UserMapper.toContentValues(u);
         long res = db.insertWithOnConflict(UserTable.TABLE, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
-        //TODO error handling? if(res<0)
+
+        if(res<0){
+
+            //TODO error handling? if(res<0)
+        }
     }
 
 }
