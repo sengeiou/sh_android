e6cb81bcf621bea575095f1745ba9223f8180b61
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-22 16:05:23 +0000

    getShotsByUserIdList, getUserByUserIdList

--- app/src/main/java/gm/mobi/android/service/BagdadService.java
+++ app/src/main/java/gm/mobi/android/service/BagdadService.java
@@ -7,11 +7,13 @@ import java.io.IOException;
 import java.util.List;
 
 import gm.mobi.android.db.objects.Follow;
+import gm.mobi.android.db.objects.Shot;
 import gm.mobi.android.db.objects.User;
 
 public interface BagdadService {
 
     public User login(String id, String password) throws IOException;
     public List<Follow> getFollows(Integer idUser, Context context, SQLiteDatabase db) throws IOException;
-
+    public List<Shot> getShotsByUserIdList(List<Integer> followingUserIds, Context context,SQLiteDatabase db) throws IOException;
+    public List<User> getUsersByUserIdList(List<Integer> userIds, Context context, SQLiteDatabase db) throws IOException;
 }
