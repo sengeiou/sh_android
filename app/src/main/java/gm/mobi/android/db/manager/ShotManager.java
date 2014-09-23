d263622f2f29e11dbeeebefb20a432f3a2af1001
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-23 09:16:22 +0000

    Adding delete method, change name of parameter Shot follow by Shot shot, In case a shot to insert, check if CSYS_DELETED is not null, in case We delete that element from our local database

--- app/src/main/java/gm/mobi/android/db/manager/ShotManager.java
+++ app/src/main/java/gm/mobi/android/db/manager/ShotManager.java
@@ -12,17 +12,40 @@ import gm.mobi.android.db.objects.Shot;
 
 public class ShotManager {
 
-    public static void saveShot(SQLiteDatabase db, Shot follow) throws SQLException {
-        ContentValues contentValues = ShotMapper.toContentValues(follow);
-        long res = db.insertWithOnConflict(GMContract.ShotTable.TABLE, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
+    /**
+     * Insert a Shot
+     * */
+    public static void saveShot(SQLiteDatabase db, Shot shot) throws SQLException {
+        long res;
+        ContentValues contentValues = ShotMapper.toContentValues(shot);
+        if(contentValues.getAsLong(GMContract.ShotTable.CSYS_DELETED)!=null){
+            res = deleteShot(db,shot);
+        }else{
+            res = db.insertWithOnConflict(GMContract.ShotTable.TABLE, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
+        }
     }
 
+    /**
+     * Insert a shot list
+     * */
     public static void saveShots(SQLiteDatabase db, List<Shot> shotList){
+        long res;
         for(Shot shot: shotList){
             ContentValues contentValues = ShotMapper.toContentValues(shot);
             db.beginTransaction();
-            db.insertWithOnConflict(GMContract.ShotTable.TABLE,null,contentValues,SQLiteDatabase.CONFLICT_REPLACE);
+            if(contentValues.getAsLong(GMContract.ShotTable.CSYS_DELETED) != null ){
+                res = deleteShot(db, shot);
+            }else{
+                res = db.insertWithOnConflict(GMContract.ShotTable.TABLE,null,contentValues,SQLiteDatabase.CONFLICT_REPLACE);
+            }
             db.endTransaction();
         }
-    }
+   }
+
+    /***
+     * Delete a shot
+     * */
+   public static long deleteShot(SQLiteDatabase db, Shot shot){
+        return db.delete(GMContract.ShotTable.TABLE, GMContract.ShotTable.ID_SHOT,new String[]{String.valueOf(shot.getIdShot())});
+   }
 }
