6e2b403f6879e268754bd3e70eebe322113374da
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-23 14:07:02 +0000

    Se ha tipificado las llamadas, dependiendo si son llamadas para shots nuevos, para la primera vez o para shots más antiguos

--- app/src/main/java/gm/mobi/android/task/jobs/timeline/ShotsJob.java
+++ app/src/main/java/gm/mobi/android/task/jobs/timeline/ShotsJob.java
@@ -4,12 +4,14 @@ import android.app.Application;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Switch;
 
 import com.path.android.jobqueue.Params;
 import com.path.android.jobqueue.network.NetworkUtil;
 import com.squareup.otto.Bus;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -17,12 +19,13 @@ import javax.inject.Inject;
 import gm.mobi.android.GolesApplication;
 import gm.mobi.android.db.manager.FollowManager;
 import gm.mobi.android.db.manager.ShotManager;
-import gm.mobi.android.db.manager.UserManager;
 import gm.mobi.android.db.objects.Shot;
 import gm.mobi.android.db.objects.User;
 import gm.mobi.android.exception.ServerException;
 import gm.mobi.android.service.BagdadService;
 import gm.mobi.android.task.events.ConnectionNotAvailableEvent;
+import gm.mobi.android.task.events.timeline.NewShotsReceivedEvent;
+import gm.mobi.android.task.events.timeline.OldShotsReceivedEvent;
 import gm.mobi.android.task.events.timeline.ShotsResultEvent;
 import gm.mobi.android.task.jobs.CancellableJob;
 
@@ -39,17 +42,24 @@ public class ShotsJob extends CancellableJob {
     @Inject
     BagdadService service;
 
+    public static final int FIRST_TIME = 0;
+    public static final int NEWER = 1;
+    public static final int OLDER = 2;
 
     private static final int PRIORITY = 4;
     private static final int RETRY_ATTEMPTS = 3;
     private Context mContext;
     public SQLiteDatabase mDb;
     public List<Integer> mFollowingUserIds;
+    public int shotRetrieveType;
+    public Shot mShot;
 
-    public ShotsJob(Context context, SQLiteDatabase db){
+    public ShotsJob(Context context, SQLiteDatabase db, int shotRetrieveType, Shot shot){
         super(new Params(PRIORITY));
         mContext = context;
         mDb = db;
+        this.mShot = shot;
+        this.shotRetrieveType = shotRetrieveType;
         GolesApplication.get(mContext).inject(this);
     }
 
@@ -61,6 +71,7 @@ public class ShotsJob extends CancellableJob {
 
     @Override
     public void onRun() throws Throwable {
+        List<Shot> shots = new ArrayList<>();
         if (isCancelled()) return;
         if (!mNetworkUtil.isConnected(app)) {
             bus.post(new ConnectionNotAvailableEvent());
@@ -71,25 +82,52 @@ public class ShotsJob extends CancellableJob {
             mFollowingUserIds = FollowManager.getUserFollowingIds(mDb, user.getIdUser());
             //Adding owner user, because I need to retrieve all the shots including my owns
             mFollowingUserIds.add(user.getIdUser());
-            List<Shot> shots = service.getShotsByUserIdList(mFollowingUserIds, mContext, mDbHelper.getWritableDatabase());
+            switch (shotRetrieveType){
+                case FIRST_TIME:
+                    shots = service.getShotsByUserIdList(mFollowingUserIds, mContext, mDbHelper.getWritableDatabase());
+                    break;
+                case NEWER:
+                    shots = service.getNewShots(mFollowingUserIds, mContext, mDbHelper.getWritableDatabase(), mShot);
+                    break;
+                case OLDER:
+                    shots = service.getOlderShots(mFollowingUserIds, mContext, mDbHelper.getWritableDatabase(), mShot);
+                    break;
+            }
             if(shots != null){
                 ShotManager.saveShots(mDbHelper.getWritableDatabase(), shots);
-                ShotsResultEvent fResultEvent = new ShotsResultEvent(ShotsResultEvent.STATUS_SUCCESS);
-                fResultEvent.setShots(shots);
-                fResultEvent.setError(null);
-                bus.post(fResultEvent);
+                switch(shotRetrieveType){
+                    case FIRST_TIME:
+                        ShotsResultEvent firstTimeResultEvent = new ShotsResultEvent(ShotsResultEvent.STATUS_SUCCESS);
+                        firstTimeResultEvent.setShots(shots);
+                        firstTimeResultEvent.setError(null);
+                        bus.post(firstTimeResultEvent);
+                        break;
+                    case NEWER:
+                        NewShotsReceivedEvent newerResultEvent = new NewShotsReceivedEvent(NewShotsReceivedEvent.STATUS_SUCCESS);
+                        newerResultEvent.setShots(shots);
+                        newerResultEvent.setError(null);
+                        bus.post(newerResultEvent);
+                        break;
+                    case OLDER:
+                        OldShotsReceivedEvent olderResultEvent = new OldShotsReceivedEvent(OldShotsReceivedEvent.STATUS_SUCCESS);
+                        olderResultEvent.setShots(shots);
+                        olderResultEvent.setError(null);
+                        bus.post(olderResultEvent);
+                        break;
+                }
+
             }else{
-                sendServerError(null);
+                sendServerError(shotRetrieveType,null);
             }
 
         } catch (ServerException e) {
             if (e.getErrorCode().equals(ServerException.G025)) {
-                sendCredentialError();
+                sendCredentialError(shotRetrieveType);
             } else {
-                sendServerError(e);
+                sendServerError(shotRetrieveType, e);
             }
         } catch (IOException e) {
-            sendServerError(e);
+            sendServerError(shotRetrieveType, e);
         }
 
     }
@@ -105,14 +143,39 @@ public class ShotsJob extends CancellableJob {
     }
 
 
-    private void sendCredentialError() {
-        ShotsResultEvent fResultEvent = new ShotsResultEvent(ShotsResultEvent.STATUS_INVALID);
-        bus.post(fResultEvent.setInvalid());
+    private void sendCredentialError(int shotRetrieveType) {
+        switch (shotRetrieveType){
+            case FIRST_TIME:
+                ShotsResultEvent fResultEvent = new ShotsResultEvent(ShotsResultEvent.STATUS_INVALID);
+                bus.post(fResultEvent.setInvalid());
+                break;
+            case NEWER:
+                NewShotsReceivedEvent newShotsReceivedEvent = new NewShotsReceivedEvent(NewShotsReceivedEvent.STATUS_INVALID);
+                bus.post(newShotsReceivedEvent.setInvalid());
+                break;
+            case OLDER:
+                OldShotsReceivedEvent oldShotsReceivedEvent = new OldShotsReceivedEvent(OldShotsReceivedEvent.STATUS_INVALID);
+                bus.post(oldShotsReceivedEvent.setInvalid());
+                break;
+        }
     }
 
-    private void sendServerError(Exception e) {
-        ShotsResultEvent fResultEvent = new ShotsResultEvent(ShotsResultEvent.STATUS_SERVER_FAILURE);
-        bus.post(fResultEvent.setServerError(e));
+    private void sendServerError(int shotRetrieveType, Exception e) {
+        switch (shotRetrieveType) {
+            case FIRST_TIME:
+                ShotsResultEvent fResultEvent = new ShotsResultEvent(ShotsResultEvent.STATUS_SERVER_FAILURE);
+                bus.post(fResultEvent.setServerError(e));
+                break;
+            case NEWER:
+                NewShotsReceivedEvent newShotsReceivedEvent = new NewShotsReceivedEvent(NewShotsReceivedEvent.STATUS_SERVER_FAILURE);
+                bus.post(newShotsReceivedEvent.setInvalid());
+                break;
+            case OLDER:
+                OldShotsReceivedEvent oldShotsReceivedEvent = new OldShotsReceivedEvent(OldShotsReceivedEvent.STATUS_SERVER_FAILURE);
+                bus.post(oldShotsReceivedEvent.setInvalid());
+                break;
+        }
+
     }
 
 
