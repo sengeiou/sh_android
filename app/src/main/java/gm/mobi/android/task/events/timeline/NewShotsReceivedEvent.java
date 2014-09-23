2c27fb6f2c0c6e03b751bdaebbaea8667df8cc83
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-23 14:05:15 +0000

    NewShotReceivedEvent con el tipo Shot además de añadir un método setShots para obtenerlos en el get

--- app/src/main/java/gm/mobi/android/task/events/timeline/NewShotsReceivedEvent.java
+++ app/src/main/java/gm/mobi/android/task/events/timeline/NewShotsReceivedEvent.java
@@ -2,18 +2,38 @@ package gm.mobi.android.task.events.timeline;
 
 import java.util.List;
 
+import gm.mobi.android.db.objects.Shot;
+import gm.mobi.android.task.events.loginregister.ResultEvent;
 import gm.mobi.android.ui.adapters.TimelineAdapter;
 
-public class NewShotsReceivedEvent {
+public class NewShotsReceivedEvent extends ResultEvent{
 
-    private List<TimelineAdapter.MockShot> shots;
+    private List<Shot> shots;
 
-    public NewShotsReceivedEvent(List<TimelineAdapter.MockShot> shots) {
-
-        this.shots = shots;
+    public NewShotsReceivedEvent(int status) {
+        super(status);
     }
 
-    public List<TimelineAdapter.MockShot> getShots() {
+    public List<Shot> getShots() {
         return shots;
+    }
+
+    public void setShots(List<Shot> newerShots){
+        this.shots = newerShots;
+    }
+
+    @Override
+    public ResultEvent setSuccessful(Object o) {
+        return null;
+    }
+
+    @Override
+    public ResultEvent setInvalid() {
+        return null;
+    }
+
+    @Override
+    public ResultEvent setServerError(Exception e) {
+        return null;
     }
 }
