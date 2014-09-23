4a3ea9ab27d6c49dc6c7de916cdb08c66195bb53
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-23 14:03:55 +0000

    Se ha añadido un método que dado una entidad, nos devuelve si la tabla está vacía o no

--- app/src/main/java/gm/mobi/android/db/manager/GeneralManager.java
+++ app/src/main/java/gm/mobi/android/db/manager/GeneralManager.java
@@ -1,7 +1,10 @@
 package gm.mobi.android.db.manager;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
+import gm.mobi.android.db.GMContract;
 import gm.mobi.android.db.OpenHelper;
 import timber.log.Timber;
 
@@ -11,4 +14,18 @@ public class GeneralManager {
         context.deleteDatabase(OpenHelper.DATABASE_NAME);
         Timber.d("Database deleted");
     }
+
+
+    public static boolean isTableEmpty(SQLiteDatabase db, String entity) {
+        boolean res = true;
+        String raw_query = "SELECT * FROM "
+                + entity;
+        Cursor c = db.rawQuery(raw_query, new String[]{});
+        if (c.getCount() > 0) res = false;
+        c.close();
+
+        return res;
+    }
+
+
 }
