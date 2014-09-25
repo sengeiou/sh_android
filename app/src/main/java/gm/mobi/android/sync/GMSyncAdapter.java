3e9fad2b71bff76f212c96401b68165fa47b4d32
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-25 13:40:41 +0000

    Remove an empty line

--- app/src/main/java/gm/mobi/android/sync/GMSyncAdapter.java
+++ app/src/main/java/gm/mobi/android/sync/GMSyncAdapter.java
@@ -36,5 +36,6 @@ public class GMSyncAdapter extends AbstractThreadedSyncAdapter {
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         // Big boys stuff
+
     }
 }
