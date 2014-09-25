abb5f607fb5e9abc34d8674f42e14cf3147681ff
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-25 16:25:17 +0000

    Method getNumMaxRowsByEntity is

--- app/src/main/java/gm/mobi/android/db/manager/SyncTableManager.java
+++ app/src/main/java/gm/mobi/android/db/manager/SyncTableManager.java
@@ -1,20 +1,20 @@
 package gm.mobi.android.db.manager;
 
 import android.content.ContentValues;
-import android.content.Context;
+
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-
 import gm.mobi.android.db.GMContract.TablesSync;
 import gm.mobi.android.db.objects.TableSync;
 import gm.mobi.android.util.TimeUtils;
 
+
 public class SyncTableManager {
 
-    public static int NUMDAYS = 7;
+    public static int NUMDAYS = 15;
     
     public static Long getLastModifiedDate(SQLiteDatabase db, String entity) {
-        Long lastDateModified = null;
+        Long lastDateModified = 0L;
         if(GeneralManager.isTableEmpty(db,entity)){
             lastDateModified = TimeUtils.getNDaysAgo(NUMDAYS);
         }else{
@@ -32,8 +32,6 @@ public class SyncTableManager {
 
         return lastDateModified;
     }
-
-
     public static long insertOrUpdateSyncTable(SQLiteDatabase db, TableSync tableSync){
         ContentValues contentValues = new ContentValues();
         contentValues.put(TablesSync.ORDER,tableSync.getOrder());
@@ -47,6 +45,25 @@ public class SyncTableManager {
         return db.insertWithOnConflict(TablesSync.TABLE, null, contentValues,SQLiteDatabase.CONFLICT_REPLACE);
     }
 
+    public static int getNumMaxOfRowsByEntity(SQLiteDatabase db, String entity){
+        int numRows = 0;
 
+
+//        int numMax = SyncTableManager.getNumMaxOfRowsByEntity(mDbHelper.getReadableDatabase(), GMContract.ShotTable.TABLE);
+//        Timber.e("Número máximo de rows en la tabla %s es %d", GMContract.ShotTable.TABLE, numMax);
+
+        String[] column = new String[]{TablesSync.MAX_ROWS};
+        String[] stringArgs = new String[]{entity};
+
+        String args = TablesSync.ENTITY+"=?";
+
+        Cursor c = db.query(TablesSync.TABLE, column,args,stringArgs,null,null,null,null);
+        if(c.getCount()>0){
+            c.moveToFirst();
+            numRows = c.getInt(0);
+        }
+        c.close();
+        return numRows;
+    }
 
 }
