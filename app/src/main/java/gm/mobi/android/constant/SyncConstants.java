1316fe8b032c01809d1d5b269327a118a27b0dc0
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-26 15:01:43 +0000

    Adding SyncConstants

--- app/src/main/java/gm/mobi/android/constant/SyncConstants.java
+++ app/src/main/java/gm/mobi/android/constant/SyncConstants.java
@@ -1,6 +1,5 @@
 package gm.mobi.android.constant;
 
-
 import gm.mobi.android.BuildConfig;
 
 public class SyncConstants {
@@ -12,6 +11,28 @@ public class SyncConstants {
     public static final String ACCOUNT_TYPE = "gm.mobi.android";
 
     // The account name
+
+//    private static Account newAccount= new Account("default_account", ACCOUNT_TYPE);
     //TODO usar un nombre de cuenta real, ya que tenemos login
-    public static final String ACCOUNT_NAME = "Goles";
+    public static final String ACCOUNT_NAME = "Shootr";
+
+
+    //Sync Interval Constants
+    public static final long MILLISECONDS_PER_SECOND = 1000L;
+    public static final long SECONDS_PER_MINUTE = 60L;
+    public static final long SYNC_INTERVAL_IN_MINUTES = 1L;
+    public static final long SYNC_INTERVAL_SHOTS = 10L;
+    //Sync Interval
+    public static final long SYNC_INTERVAL_FOR_SHOTS = 10000L;
+
+
+    //Sync callTypes
+    public static String CALL_TYPE = "callType";
+
+
+    public static int GET_SHOTS_CALL_TYPE = 300;
+
+
+
+
 }
