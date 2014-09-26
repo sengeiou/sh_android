fddd67309b5cc3d83ca5cda654bc2235ddd9ce13
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-26 14:05:50 +0000

    Sync Adapter

--- app/src/main/java/gm/mobi/android/sync/GMSyncAdapter.java
+++ app/src/main/java/gm/mobi/android/sync/GMSyncAdapter.java
@@ -3,18 +3,33 @@ package gm.mobi.android.sync;
 import android.accounts.Account;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SyncResult;
 import android.os.Bundle;
 
+import com.path.android.jobqueue.JobManager;
+import com.squareup.otto.Bus;
+
+import javax.inject.Inject;
+
+import gm.mobi.android.GolesApplication;
+import gm.mobi.android.constant.SyncConstants;
+import gm.mobi.android.db.objects.User;
+import gm.mobi.android.service.dataservice.dto.TimelineDtoFactory;
+import gm.mobi.android.service.dataservice.dto.UserDtoFactory;
+import gm.mobi.android.task.jobs.timeline.TimelineJob;
+import timber.log.Timber;
+
 public class GMSyncAdapter extends AbstractThreadedSyncAdapter {
 
+    ContentResolver mContentResolver;
     /**
      * Compatibility with versions previous to 3.0
      */
-    public GMSyncAdapter(Context mContext, boolean autoInitialize){
-        super(mContext,autoInitialize);
-        setup();
+    public GMSyncAdapter(Context context, boolean autoInitialize){
+        super(context,autoInitialize);
+        setup(context);
     }
 
     /**
@@ -22,20 +37,33 @@ public class GMSyncAdapter extends AbstractThreadedSyncAdapter {
      */
     public GMSyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
         super(context, autoInitialize, allowParallelSyncs);
-        setup();
+        setup(context);
     }
 
     /**
      * Any initial setup tasks
      */
-    public void setup(){
+    public void setup(Context context){
         /* no-op */
+        mContentResolver = context.getContentResolver();
     }
-
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         // Big boys stuff
+        Timber.e("Entra en onPer" +
+                "formSync");
+        try{
+            synchronized (this){
+                int callType = extras.getInt(SyncConstants.CALL_TYPE);
+                if(callType == 0) return;
+                if(callType == SyncConstants.GET_SHOTS_CALL_TYPE){
+                    Timber.e("Entra en la sincronización para el tipo de llamada : %d",callType);
+                }
+            }
+        }catch(IllegalStateException e){
+            Timber.e("Exception onPerformSync",e.getMessage());
+        }
 
     }
 }
