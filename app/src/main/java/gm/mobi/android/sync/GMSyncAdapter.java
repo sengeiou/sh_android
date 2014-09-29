814f60903f7c5105723f3ecb00aa326ea18e8967
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-29 16:46:39 +0000

    Adding synchronization for deleting older shots

--- app/src/main/java/gm/mobi/android/sync/GMSyncAdapter.java
+++ app/src/main/java/gm/mobi/android/sync/GMSyncAdapter.java
@@ -6,6 +6,8 @@ import android.content.ContentProviderClient;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SyncResult;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import com.path.android.jobqueue.JobManager;
@@ -19,11 +21,14 @@ import gm.mobi.android.db.objects.User;
 import gm.mobi.android.service.dataservice.dto.TimelineDtoFactory;
 import gm.mobi.android.service.dataservice.dto.UserDtoFactory;
 import gm.mobi.android.task.jobs.timeline.TimelineJob;
+import gm.mobi.android.util.RemoveUtils;
 import timber.log.Timber;
 
 public class GMSyncAdapter extends AbstractThreadedSyncAdapter {
 
     ContentResolver mContentResolver;
+    @Inject
+    SQLiteOpenHelper mDbHelper;
     /**
      * Compatibility with versions previous to 3.0
      */
@@ -46,18 +51,20 @@ public class GMSyncAdapter extends AbstractThreadedSyncAdapter {
     public void setup(Context context){
         /* no-op */
         mContentResolver = context.getContentResolver();
+        GolesApplication.get(context).inject(this);
     }
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         // Big boys stuff
-        Timber.e("Entra en onPer" +
-                "formSync");
+        Timber.e("Entra en onPerformSync");
         try{
             synchronized (this){
                 int callType = extras.getInt(SyncConstants.CALL_TYPE);
                 if(callType == 0) return;
-                if(callType == SyncConstants.GET_SHOTS_CALL_TYPE){
+                if(callType == SyncConstants.REMOVE_OLD_SHOTS){
+                    Timber.e("Entra en la sincro para hacer el remove");
+                    RemoveUtils.removeOldShots(mDbHelper.getReadableDatabase());
                     Timber.e("Entra en la sincronización para el tipo de llamada : %d",callType);
                 }
             }
