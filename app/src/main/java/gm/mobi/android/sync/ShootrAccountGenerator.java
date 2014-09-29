51380420c4e21b1f9f3f932131d319449eb0f101
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-09-29 16:48:13 +0000

    Adding some parameters Rafa says

--- app/src/main/java/gm/mobi/android/sync/ShootrAccountGenerator.java
+++ app/src/main/java/gm/mobi/android/sync/ShootrAccountGenerator.java
@@ -2,10 +2,14 @@ package gm.mobi.android.sync;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.os.Bundle;
 
 import gm.mobi.android.constant.Constants;
 import gm.mobi.android.constant.SyncConstants;
+
+import static gm.mobi.android.constant.SyncConstants.AUTHORITY;
 
 public class ShootrAccountGenerator {
 
@@ -19,22 +23,30 @@ public class ShootrAccountGenerator {
         //Create account type and default account
         //Get an instance of Android Account Manager
         //Add the account name and type, no password or user data. If ok, return the Account object, otherwise report an error
-        Account newAccount = new Account(SyncConstants.ACCOUNT_NAME, SyncConstants.ACCOUNT_TYPE);
-        AccountManager accountManager = (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
+        Account mAccount = new Account(SyncConstants.ACCOUNT_NAME, SyncConstants.ACCOUNT_TYPE);
+        AccountManager mAccountManager = (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
 
-        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+        if (mAccountManager.addAccountExplicitly(mAccount, null, null)) {
              /*
              * If you don't set android:syncable="true" in
              * in your <provider> element in the manifest,
              * then call context.setIsSyncable(account, AUTHORITY, 1)
              * here.
              */
+            ContentResolver.setIsSyncable(mAccount,SyncConstants.AUTHORITY,1);
+            ContentResolver.setSyncAutomatically(mAccount,SyncConstants.AUTHORITY,true);
+
+            Bundle params  = new Bundle();
+            params.putInt(SyncConstants.CALL_TYPE,SyncConstants.REMOVE_OLD_SHOTS);
+            ContentResolver.addPeriodicSync(mAccount, AUTHORITY, params,SyncConstants.SYNC_INTERVAL_SHOTS);
+
         } else {
             /*
              * TODO The account exists or some other error occurred. Log this, report it,
              * or handle it internally.
              */
+
         }
-        return newAccount;
+        return mAccount;
     }
 }
