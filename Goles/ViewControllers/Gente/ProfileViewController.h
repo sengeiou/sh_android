9c59d071023ac3ffd969e5a230c16bc2c77858bd
Mari Bañuls Ondara <mari.banyuls@fav24.com>
2014-10-01 14:14:12 +0000

    BGD-17 Profile Scores
    - UI

---
+++ Goles/ViewControllers/Gente/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Shootr
+//
+//  Created by Maria Teresa Bañuls on 01/10/14.
+//  Copyright (c) 2014 Fav24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
