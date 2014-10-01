85fca94287e101f8b8f08558533fc8a3d081461b
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-10-01 16:22:29 +0000

    Adding the parameter favoriteTeam , for getting in the UserInfoResultEvent

--- app/src/main/java/gm/mobi/android/task/jobs/profile/GetUserInfoJob.java
+++ app/src/main/java/gm/mobi/android/task/jobs/profile/GetUserInfoJob.java
@@ -1,16 +1,22 @@
 package gm.mobi.android.task.jobs.profile;
 
+import android.app.Application;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.path.android.jobqueue.Job;
 import com.path.android.jobqueue.Params;
+import com.path.android.jobqueue.network.NetworkUtil;
 import com.squareup.otto.Bus;
 import gm.mobi.android.GolesApplication;
 import gm.mobi.android.db.manager.FollowManager;
+import gm.mobi.android.db.manager.TeamManager;
 import gm.mobi.android.db.manager.UserManager;
+import gm.mobi.android.db.mappers.TeamMapper;
+import gm.mobi.android.db.objects.Team;
 import gm.mobi.android.db.objects.User;
 import gm.mobi.android.service.BagdadService;
+import gm.mobi.android.task.events.ConnectionNotAvailableEvent;
 import gm.mobi.android.task.events.profile.UserInfoResultEvent;
 import java.io.IOException;
 import javax.inject.Inject;
@@ -24,6 +30,10 @@ public class GetUserInfoJob extends Job {
     @Inject SQLiteOpenHelper dbHelper;
     @Inject Bus bus;
     @Inject BagdadService service;
+    @Inject
+    NetworkUtil mNetworkUtil;
+    @Inject
+    Application app;
 
     private Long userId;
     private User currentUser;
@@ -32,7 +42,6 @@ public class GetUserInfoJob extends Job {
         super(new Params(PRIORITY));
         this.userId = userId;
         this.currentUser = currentUser;
-
         GolesApplication.get(context).inject(this);
     }
 
@@ -41,14 +50,16 @@ public class GetUserInfoJob extends Job {
     }
 
     @Override public void onRun() throws Throwable {
+        Team favTeam = null;
         User consultedUser = UserManager.getUserByIdUser(dbHelper.getReadableDatabase(), userId);
         if (consultedUser != null) {
             // Get relationship
             int followRelationship =
                 FollowManager.getFollowRelationship(dbHelper.getReadableDatabase(), currentUser,
                     consultedUser);
-
-            UserInfoResultEvent result = new UserInfoResultEvent(consultedUser, followRelationship);
+            Long idTeamFav = consultedUser.getFavouriteTeamId();
+            if(idTeamFav!=null) favTeam = TeamManager.getTeamByIdTeam(dbHelper.getReadableDatabase(),idTeamFav);
+            UserInfoResultEvent result = new UserInfoResultEvent(consultedUser, followRelationship, favTeam);
             bus.post(result);
             //TODO control de errores
         } else {
@@ -63,10 +74,12 @@ public class GetUserInfoJob extends Job {
             //Store user in db
             SQLiteDatabase db = dbHelper.getWritableDatabase();
             UserManager.saveUser(db, consultedUserFromService);
+            Long idTeamFav = consultedUser.getFavouriteTeamId();
+            if(idTeamFav!=null) favTeam = TeamManager.getTeamByIdTeam(dbHelper.getReadableDatabase(),idTeamFav);
             db.close();
 
-            UserInfoResultEvent result = new UserInfoResultEvent(consultedUserFromService,
-                0);//TODO meter follow obtenido y calculado
+            UserInfoResultEvent result = new UserInfoResultEvent(consultedUserFromService,0, favTeam);
+   //TODO meter follow obtenido y calculado
             bus.post(result);
         } catch (IOException e) {
             //TODO server error
@@ -83,6 +96,16 @@ public class GetUserInfoJob extends Job {
     }
 
     @Override protected boolean shouldReRunOnThrowable(Throwable throwable) {
+        return true;
+    }
+
+
+
+    private boolean checkNetwork() {
+        if (!mNetworkUtil.isConnected(app)) {
+            bus.post(new ConnectionNotAvailableEvent());
+            return false;
+        }
         return true;
     }
 }
