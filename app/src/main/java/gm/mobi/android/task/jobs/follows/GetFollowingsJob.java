ff75b4acf49d527e4d3d0e0a78aff2c8bd9d069e
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-10-01 16:21:39 +0000

    Create some methods for reducing the number of lines in the onRun method, also added getTeamsByIdTeams when we get the user following from server

--- app/src/main/java/gm/mobi/android/task/jobs/follows/GetFollowingsJob.java
+++ app/src/main/java/gm/mobi/android/task/jobs/follows/GetFollowingsJob.java
@@ -11,7 +11,9 @@ import com.squareup.otto.Bus;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -19,8 +21,10 @@ import gm.mobi.android.GolesApplication;
 import gm.mobi.android.db.GMContract;
 import gm.mobi.android.db.manager.FollowManager;
 import gm.mobi.android.db.manager.SyncTableManager;
+import gm.mobi.android.db.manager.TeamManager;
 import gm.mobi.android.db.manager.UserManager;
 import gm.mobi.android.db.objects.Follow;
+import gm.mobi.android.db.objects.Team;
 import gm.mobi.android.db.objects.User;
 import gm.mobi.android.exception.ServerException;
 import gm.mobi.android.service.BagdadService;
@@ -55,21 +59,55 @@ public class GetFollowingsJob extends CancellableJob{
         /* no-op */
     }
 
+
+    private List<Follow> getFollowingsIdsFromServer(){
+        List<Follow> followingsIds = new ArrayList<>();
+        Long modifiedFollows = SyncTableManager.getLastModifiedDate(mDbHelper.getReadableDatabase(), GMContract.FollowTable.TABLE);
+        try{
+            followingsIds = service.getFollows(currentUser.getIdUser(), modifiedFollows, UserDtoFactory.GET_FOLLOWING);
+
+        }catch(ServerException e){
+            if (e.getErrorCode().equals(ServerException.G025)) {
+                sendCredentialError();
+            } else {
+                sendServerError(e);
+            }
+        } catch (IOException e) {
+            sendServerError(e);
+        }
+        return followingsIds;
+    }
+
+    private List<User> getUsersByFollowingIdsFromServer(List<Long> followingsIds){
+        List<User> users = null;
+        try{
+            users = service.getUsersByUserIdList(followingsIds);
+        }catch(ServerException e){
+            if (e.getErrorCode().equals(ServerException.G025)) {
+                sendCredentialError();
+            } else {
+                sendServerError(e);
+            }
+        } catch (IOException e) {
+            sendServerError(e);
+        }
+        return users;
+    }
+
     @Override
     public void onRun() throws Throwable {
         if (isCancelled()) return;
-        if(!checkConnection()) return;
+        if (!checkConnection()) return;
 
-        try {
             // 1. Download followings ids
-            Long modifiedFollows = SyncTableManager.getLastModifiedDate(mDbHelper.getReadableDatabase(), GMContract.FollowTable.TABLE);
-            List<Follow> followings = service.getFollows(currentUser.getIdUser(), modifiedFollows, UserDtoFactory.GET_FOLLOWING);
-            if(followings == null) {
+            List<Follow> followings = getFollowingsIdsFromServer();
+            if (followings == null) {
                 sendServerError(null);
                 Timber.e("Unknown error downloading followings list");
                 return;
             }
             Timber.d("Downloaded %d following relations", followings.size());
+
             List<Long> followingIds = new ArrayList<>(followings.size());
             for (Follow following : followings) {
                 followingIds.add(following.getFollowedUser());
@@ -79,19 +117,23 @@ public class GetFollowingsJob extends CancellableJob{
                 sendSucces(null);
                 return;
             }
-
-            if (isCancelled()) return;
-            if(!checkConnection()) return;
-
             // 2. Download users from those followings
-//            Long modifiedUsers = SyncTableManager.getLastModifiedDate(mDbHelper.getReadableDatabase(), GMContract.UserTable.TABLE);
-            List<User> usersFollowing = service.getUsersByUserIdList(followingIds);
+            List<User> usersFollowing = getUsersByFollowingIdsFromServer(followingIds);
             if (usersFollowing == null) {
                 sendServerError(null);
                 Timber.e("Unknown error downloading followings' users");
                 return;
             }
+            List<Team> teams ;
+            Set<Long> idTeams = new HashSet<>();
+            for (User user : usersFollowing) {
+                if (user.getFavouriteTeamId() != null) {
+                    idTeams.add(user.getFavouriteTeamId());
+                }
+            }
+            teams = getTeamsByTeamIds(idTeams);
             Timber.d("Downloaded %d followings' users", usersFollowing.size());
+            Timber.d("Downloaded %d teams' users", teams.size());
 
             if (isCancelled()) return;
 
@@ -99,11 +141,17 @@ public class GetFollowingsJob extends CancellableJob{
             SQLiteDatabase db = mDbHelper.getWritableDatabase();
             FollowManager.saveFollows(db, followings);
             UserManager.saveUsers(db, usersFollowing);
+            TeamManager.saveTeams(db, teams);
             db.close();
             sendSucces(usersFollowing);
 
+    }
 
-        } catch (ServerException e) {
+    private Team getTeamByIdTeam(Long idTeam){
+        Team resTeam = null;
+        try{
+            resTeam= service.getTeamByIdTeam(idTeam);
+        }catch(ServerException e){
             if (e.getErrorCode().equals(ServerException.G025)) {
                 sendCredentialError();
             } else {
@@ -112,6 +160,24 @@ public class GetFollowingsJob extends CancellableJob{
         } catch (IOException e) {
             sendServerError(e);
         }
+        return  resTeam;
+    }
+
+    private List<Team> getTeamsByTeamIds(Set<Long> teamIds){
+        List<Team> resTeams = null;
+        Long modifiedTeams = SyncTableManager.getLastModifiedDate(mDbHelper.getReadableDatabase(), GMContract.TeamTable.TABLE);
+        try{
+            resTeams= service.getTeamsByIdTeams(teamIds, modifiedTeams);
+        }catch(ServerException e){
+            if (e.getErrorCode().equals(ServerException.G025)) {
+                sendCredentialError();
+            } else {
+                sendServerError(e);
+            }
+        } catch (IOException e) {
+            sendServerError(e);
+        }
+        return  resTeams;
     }
 
     private boolean checkConnection() {
