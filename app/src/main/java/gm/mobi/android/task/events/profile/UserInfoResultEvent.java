9520e7056ccbfe97c907b52bb45f590c33803d34
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-10-01 16:28:01 +0000

    Adding favourite team to ResultEvent

--- app/src/main/java/gm/mobi/android/task/events/profile/UserInfoResultEvent.java
+++ app/src/main/java/gm/mobi/android/task/events/profile/UserInfoResultEvent.java
@@ -1,18 +1,29 @@
 package gm.mobi.android.task.events.profile;
 
+import gm.mobi.android.db.objects.Team;
 import gm.mobi.android.db.objects.User;
 
 public class UserInfoResultEvent {
     private User user;
     private int relationship;
+    private Team favouriteTeam;
 
-    public UserInfoResultEvent(User user, int relationship) {
+    public UserInfoResultEvent(User user, int relationship, Team team) {
         this.user = user;
         this.relationship = relationship;
+        this.favouriteTeam = team;
     }
 
     public User getUser() {
         return user;
+    }
+
+    public Team getFavouriteTeam(){
+        return favouriteTeam;
+    }
+
+    public void setFavouriteTeam(Team team){
+        this.favouriteTeam = team;
     }
 
     public void setUser(User user) {
