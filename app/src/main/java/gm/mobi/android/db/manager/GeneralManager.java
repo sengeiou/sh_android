41734b7e58be7679479c6dd31f732fa456279198
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-10-01 11:06:35 +0000

    Se crea un método insertOrUpdate genérico

--- app/src/main/java/gm/mobi/android/db/manager/GeneralManager.java
+++ app/src/main/java/gm/mobi/android/db/manager/GeneralManager.java
@@ -1,5 +1,6 @@
 package gm.mobi.android.db.manager;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
@@ -19,8 +20,7 @@ public class GeneralManager {
 
     public static boolean isTableEmpty(SQLiteDatabase db, String entity) {
         boolean res = true;
-        String raw_query = "SELECT * FROM "
-                + entity;
+        String raw_query = "SELECT * FROM "+ entity;
         Cursor c = db.rawQuery(raw_query, new String[]{});
         if (c.getCount() > 0) res = false;
         c.close();
@@ -36,5 +36,23 @@ public class GeneralManager {
     public static long numberOfRows(SQLiteDatabase db, String table){
         long numRows = DatabaseUtils.queryNumEntries(db, table);
         return numRows;
+    }
+
+
+    public static long insertOrUpdate(SQLiteDatabase db, String tableName,ContentValues contentValues,String[] projection,String where, String[] args){
+        Cursor c;
+        long res = 0;
+        try{
+            c = db.query(tableName,projection,where, args, null, null, null );
+            if(c.getCount()>0){
+                res = db.update(tableName,contentValues,where,args);
+            }else{
+                res = db.insertWithOnConflict(tableName, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
+            }
+
+        }catch(Exception e){
+            Timber.e("[UPDATE EXCEPTION], insertOrUpdate in table %s with exception %s and message %s",tableName,e, e.getMessage());
+        }
+        return res;
     }
 }
