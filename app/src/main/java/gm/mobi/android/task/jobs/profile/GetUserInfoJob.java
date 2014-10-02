7bbd234ea06c3bd1ad2c3a9569289c2526c2a282
Inmaculada Alcón Piñero <inmaculada.alcon@fav24.com>
2014-10-02 16:10:42 +0000

    Cambios para obtener toda la información del user actualizada

--- app/src/main/java/gm/mobi/android/task/jobs/profile/GetUserInfoJob.java
+++ app/src/main/java/gm/mobi/android/task/jobs/profile/GetUserInfoJob.java
@@ -4,22 +4,34 @@ import android.app.Application;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
 import com.path.android.jobqueue.Job;
 import com.path.android.jobqueue.Params;
 import com.path.android.jobqueue.network.NetworkUtil;
 import com.squareup.otto.Bus;
 import gm.mobi.android.GolesApplication;
+import gm.mobi.android.db.GMContract;
 import gm.mobi.android.db.manager.FollowManager;
+import gm.mobi.android.db.manager.SyncTableManager;
 import gm.mobi.android.db.manager.TeamManager;
 import gm.mobi.android.db.manager.UserManager;
-import gm.mobi.android.db.mappers.TeamMapper;
+import gm.mobi.android.db.mappers.FollowMapper;
+import gm.mobi.android.db.objects.Follow;
 import gm.mobi.android.db.objects.Team;
 import gm.mobi.android.db.objects.User;
 import gm.mobi.android.service.BagdadService;
+import gm.mobi.android.service.dataservice.dto.UserDtoFactory;
 import gm.mobi.android.task.events.ConnectionNotAvailableEvent;
+import gm.mobi.android.task.events.follows.FollowsResultEvent;
 import gm.mobi.android.task.events.profile.UserInfoResultEvent;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.inject.Inject;
+
+import gm.mobi.android.task.jobs.follows.GetFollowingsJob;
 import timber.log.Timber;
 
 public class GetUserInfoJob extends Job {
@@ -27,7 +39,7 @@ public class GetUserInfoJob extends Job {
     private static final int PRIORITY = 3; //TODO definir valores estáticos para determinados casos
     private static final int RETRY_ATTEMPTS = 3;
 
-    @Inject SQLiteOpenHelper dbHelper;
+    @Inject SQLiteOpenHelper mDbHelper;
     @Inject Bus bus;
     @Inject BagdadService service;
     @Inject
@@ -49,38 +61,50 @@ public class GetUserInfoJob extends Job {
         /* noop */
     }
 
-    @Override public void onRun() throws Throwable {
+
+    public void retrieveDataFromDatabase(){
         Team favTeam = null;
-        User consultedUser = UserManager.getUserByIdUser(dbHelper.getReadableDatabase(), userId);
+        User consultedUser = UserManager.getUserByIdUser(mDbHelper.getReadableDatabase(), userId);
         if (consultedUser != null) {
             // Get relationship
-            int followRelationship =
-                FollowManager.getFollowRelationship(dbHelper.getReadableDatabase(), currentUser,
-                    consultedUser);
+            int followRelationship =FollowManager.getFollowRelationship(mDbHelper.getReadableDatabase(), currentUser,consultedUser);
             Long idTeamFav = consultedUser.getFavouriteTeamId();
-            if(idTeamFav!=null) favTeam = TeamManager.getTeamByIdTeam(dbHelper.getReadableDatabase(),idTeamFav);
+            if(idTeamFav!=null) favTeam = TeamManager.getTeamByIdTeam(mDbHelper.getReadableDatabase(),idTeamFav);
             UserInfoResultEvent result = new UserInfoResultEvent(consultedUser, followRelationship, favTeam);
             bus.post(result);
             //TODO control de errores
         } else {
-            Timber.i("User with id %d not found in local database. Retrieving from the service...",
-                userId);
+            Timber.i("User with id %d not found in local database. Retrieving from the service...",userId);
         }
+    }
 
+    public int getFollowRelationship(SQLiteDatabase db, User consultedUserFromService) throws IOException, SQLException {
+        int resFollowRelationship;
+        Follow getFollowingRelationshipBetweenMeAndUser = service.getFollowRelationship(consultedUserFromService.getIdUser(), currentUser.getIdUser(), UserDtoFactory.GET_FOLLOWING);
+        Follow getFollowerRelationshipBetweenMeAndUser = service.getFollowRelationship(consultedUserFromService.getIdUser(), currentUser.getIdUser(), UserDtoFactory.GET_FOLLOWERS);
+        FollowManager.saveFollow(db,getFollowerRelationshipBetweenMeAndUser);
+        FollowManager.saveFollow(db,getFollowingRelationshipBetweenMeAndUser);
+        resFollowRelationship = FollowManager.getFollowRelationship(db, currentUser,consultedUserFromService);
+        db.close();
+        return resFollowRelationship;
+    }
+
+    @Override public void onRun() throws Throwable {
+        //We make this for a speed screen update
+        retrieveDataFromDatabase();
         // Refresh anyways
         try {
+            SQLiteDatabase db = mDbHelper.getWritableDatabase();
             User consultedUserFromService = service.getUserByIdUser(userId);
-            //TODO retrieve follows relations also
-            //TODO retrieve team
-            //Store user in db
-            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            int followRelationship = getFollowRelationship(db, consultedUserFromService);
+
+            Team team = service.getTeamByIdTeam(consultedUserFromService.getFavouriteTeamId());
+            //Store user and team in db
             UserManager.saveUser(db, consultedUserFromService);
-            Long idTeamFav = consultedUser.getFavouriteTeamId();
-            if(idTeamFav!=null) favTeam = TeamManager.getTeamByIdTeam(dbHelper.getReadableDatabase(),idTeamFav);
+            TeamManager.insertOrUpdateTeam(db,team);
             db.close();
 
-            UserInfoResultEvent result = new UserInfoResultEvent(consultedUserFromService,0, favTeam);
-   //TODO meter follow obtenido y calculado
+            UserInfoResultEvent result = new UserInfoResultEvent(consultedUserFromService,followRelationship, team);
             bus.post(result);
         } catch (IOException e) {
             //TODO server error
