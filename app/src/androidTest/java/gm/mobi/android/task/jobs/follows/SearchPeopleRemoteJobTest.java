77751a78151718537de9cce72bd39cfd4b5c00bd
Rafael Vázquez Sánchez <rafael.vazquez@fav24.com>
2014-10-09 16:20:40 +0000

    Nuevo test de SearchRemotePeopleJob

---
+++ app/src/androidTest/java/gm/mobi/android/task/jobs/follows/SearchPeopleRemoteJobTest.java
@@ -0,0 +1,35 @@
+package gm.mobi.android.task.jobs.follows;
+
+import android.app.Application;
+import com.path.android.jobqueue.network.NetworkUtil;
+import com.squareup.otto.Bus;
+import gm.mobi.android.task.events.ConnectionNotAvailableEvent;
+import java.io.IOException;
+import java.sql.SQLException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(emulateSdk = 18)
+public class SearchPeopleRemoteJobTest {
+
+    @Test
+    public void postEventOnConnectionNotAvailable() throws IOException, SQLException {
+        Application app = Robolectric.application;
+        Bus bus = mock(Bus.class);
+        NetworkUtil networkUtil = mock(NetworkUtil.class);
+
+        SearchPeopleRemoteJob job = new SearchPeopleRemoteJob(app, bus, null, networkUtil);
+        job.run();
+
+        verify(bus).post(any(ConnectionNotAvailableEvent.class));
+    }
+
+}
