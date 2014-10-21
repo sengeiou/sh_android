96d25911c5861670a6bb2be6961be669f607daee
Teo Rodríguez Giménez <teo.rodriguez@fav24.com>
2014-10-21 15:36:51 +0000

    BGD-Refactor

--- Bagdad/Bagdad/Factories/BagdadFactory.cs
+++ Bagdad/Bagdad/Factories/BagdadFactory.cs
@@ -22,6 +22,96 @@ namespace Bagdad.Factories
             return new UserImageManager();
         }
 
+        public virtual UserViewModel CreateUserViewModel()
+        {
+            return new UserViewModel();
+        }
+
+        public virtual User CreateUser()
+        {
+            return new User();
+        }
+
+        public virtual User CreateFilledUserWithOutDeleteAndSynchronizedInfo(int _idUser, String _userName, String _name, String _photo, int _numFollowers, int _numFollowing, int _points, String _bio, String _website, String _favoriteTeamName, int _idFavoriteTeam, int _csys_revision, double _csys_birth, double _csys_modified)
+        {
+            return new User() 
+            { 
+                idUser = _idUser, 
+                userName = _userName, 
+                name = _name, 
+                photo = _photo, 
+                numFollowers = _numFollowers, 
+                numFollowing = _numFollowing, 
+                points = _points, 
+                bio = _bio, 
+                website = _website, 
+                favoriteTeamName = _favoriteTeamName, 
+                idFavoriteTeam = _idFavoriteTeam, 
+                csys_revision = _csys_revision, 
+                csys_birth = _csys_birth, 
+                csys_modified = _csys_modified 
+            };
+        }
+
+        public virtual User CreateFilledUserWithDeleteAndSynchronizedInfo(int _idUser, String _userName, String _name, String _photo, int _numFollowers, int _numFollowing, int _points, String _bio, String _website, String _favoriteTeamName, int _idFavoriteTeam, double _csys_birth, double _csys_modified, Double _csys_deleted, int _csys_revision, Char _csys_synchronized)
+        {
+            return new User()
+            {
+                idUser = _idUser,
+                userName = _userName,
+                name = _name,
+                photo = _photo,
+                numFollowers = _numFollowers,
+                numFollowing = _numFollowing,
+                points = _points,
+                bio = _bio,
+                website = _website,
+                favoriteTeamName = _favoriteTeamName,
+                idFavoriteTeam = _idFavoriteTeam,
+                csys_birth = _csys_birth,
+                csys_modified = _csys_modified,
+                csys_deleted = _csys_deleted, 
+                csys_revision = _csys_revision, 
+                csys_synchronized = _csys_synchronized 
+            };
+        }
+
+        public virtual Login CreateLogin()
+        {
+            return new Login();
+        }
+
+        public virtual Login CreateFullFilledLogin(int _idUser, String _sessionToken, String _email, int _idFavoriteTeamName, String _favoriteTeamName, String _userName, String _name, String _photo, String _bio, String _website, int _points, int _numFollowing, int _numFollowers, Double _csys_birth, Double _csys_modified, Double _csys_deleted, int _csys_revision, Char _csys_synchronized)
+        {
+            Login login = new Login() 
+            {
+                idUser = _idUser, 
+                sessionToken = _sessionToken, 
+                email = _email, 
+                idFavoriteTeam = _idFavoriteTeamName, 
+                favoriteTeamName = _favoriteTeamName, 
+                userName = _userName, 
+                name = _name, 
+                photo = _photo, 
+                bio = _bio, 
+                website = _website, 
+                points = _points, 
+                numFollowing = _numFollowing, 
+                numFollowers = _numFollowers, 
+                csys_birth = _csys_birth, 
+                csys_modified = _csys_modified, 
+                csys_deleted = _csys_deleted, 
+                csys_revision = _csys_revision, 
+                csys_synchronized = _csys_synchronized 
+            };
+            return login;
+        }
+
+        public virtual ServiceCommunication CreateServiceCommunication()
+        {
+            return new ServiceCommunication();
+        }
+
         public virtual FollowViewModel CreateFollowViewModel()
         {
             return new FollowViewModel();
