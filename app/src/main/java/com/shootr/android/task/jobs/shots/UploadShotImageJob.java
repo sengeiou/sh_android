54dfdb4110a9505a6dfe62163ab91a3ca5b13014
Rafa Vázquez <rafael.vazquez@fav24.com>
2014-12-11 19:34:29 +0100

    Realiza validación de imagen inválida a la subida

--- app/src/main/java/com/shootr/android/task/jobs/shots/UploadShotImageJob.java
+++ app/src/main/java/com/shootr/android/task/jobs/shots/UploadShotImageJob.java
@@ -3,14 +3,21 @@ package com.shootr.android.task.jobs.shots;
 import android.app.Application;
 import com.path.android.jobqueue.Params;
 import com.path.android.jobqueue.network.NetworkUtil;
+import com.shootr.android.exception.ShootrError;
 import com.shootr.android.service.PhotoService;
+import com.shootr.android.service.ShootrServerException;
 import com.shootr.android.task.events.profile.UploadShotImageEvent;
 import com.shootr.android.task.jobs.ShootrBaseJob;
+import com.shootr.android.task.validation.FieldValidationError;
+import com.shootr.android.task.validation.FieldValidationErrorEvent;
 import com.shootr.android.util.ImageResizer;
 import com.squareup.otto.Bus;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.inject.Inject;
 import org.json.JSONException;
 
@@ -22,8 +29,8 @@ public class UploadShotImageJob extends ShootrBaseJob<UploadShotImageEvent> {
 
     private File photoFile;
 
-    @Inject public UploadShotImageJob(Application application, Bus bus, NetworkUtil networkUtil, PhotoService photoService,
-      ImageResizer imageResizer) {
+    @Inject public UploadShotImageJob(Application application, Bus bus, NetworkUtil networkUtil,
+      PhotoService photoService, ImageResizer imageResizer) {
         super(new Params(PRIORITY), application, bus, networkUtil);
         this.photoService = photoService;
         this.imageResizer = imageResizer;
@@ -34,10 +41,33 @@ public class UploadShotImageJob extends ShootrBaseJob<UploadShotImageEvent> {
     }
 
     @Override protected void run() throws SQLException, IOException, JSONException {
-        File imageFile = getResizedImage(photoFile);
-        String imageUrl = uploadPhoto(imageFile);
+        try {
+            File imageFile = getResizedImage(photoFile);
+            String imageUrl = uploadPhoto(imageFile);
+            postSuccessfulEvent(new UploadShotImageEvent(imageUrl));
+        } catch (ShootrServerException serverException) {
+            ShootrError shootrError = serverException.getShootrError();
+            FieldValidationError fieldValidationError = fieldErrorFromServer(shootrError.getErrorCode());
+            if (fieldValidationError != null) {
+                postValidationError(fieldValidationError);
+            } else {
+                throw serverException;
+            }
+        }
+    }
 
-        postSuccessfulEvent(new UploadShotImageEvent(imageUrl));
+    private void postValidationError(FieldValidationError fieldValidationError) {
+        List<FieldValidationError> errorsListOfOneElement = Arrays.asList(fieldValidationError);
+        postCustomEvent(new FieldValidationErrorEvent(errorsListOfOneElement));
+    }
+
+    private FieldValidationError fieldErrorFromServer(String errorCode) {
+        boolean isImageError = errorCode.equals(ShootrError.ERROR_CODE_INVALID_IMAGE);
+        if (isImageError) {
+            return new FieldValidationError(errorCode, FieldValidationError.FIELD_IMAGE);
+        } else {
+            return null;
+        }
     }
 
     private String uploadPhoto(File imageFile) throws IOException, JSONException {
