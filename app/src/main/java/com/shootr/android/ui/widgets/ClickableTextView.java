7a577ed661ab21b1a444b8d22840a3555207f8e5
Rafa Vázquez <rafael.vazquez@fav24.com>
2014-12-17 13:19:21 +0100

    Magia negra en el ClickableTextView para mostrar indicador de press.
    Creado un TouchableUrlSpan que muestra el underline cuando se está 
    pulsando el enlace.
    
    Cuando se cancela el touch se cancela el press.

--- app/src/main/java/com/shootr/android/ui/widgets/ClickableTextView.java
+++ app/src/main/java/com/shootr/android/ui/widgets/ClickableTextView.java
@@ -3,14 +3,15 @@ package com.shootr.android.ui.widgets;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.os.Parcel;
 import android.support.annotation.NonNull;
 import android.text.Layout;
 import android.text.Selection;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.text.style.URLSpan;
 import android.text.util.Linkify;
 import android.util.AttributeSet;
@@ -26,6 +27,7 @@ public class ClickableTextView extends TextView {
     private static final String defaultSchema = allowedSchemas[0];
 
     private Pattern urlPattern;
+    private TouchableUrlSpan alreadyPressedSpan;
 
     public ClickableTextView(Context context) {
         super(context);
@@ -64,7 +66,7 @@ public class ClickableTextView extends TextView {
             int start = matcher.start();
             int end = matcher.end();
             String url = makeUrl(matcher.group());
-            ss.setSpan(new URLSpan(url), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ss.setSpan(new TouchableUrlSpan(url), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
         setText(ss, BufferType.SPANNABLE);
     }
@@ -112,34 +114,93 @@ public class ClickableTextView extends TextView {
 
             int action = event.getAction();
 
+            if (action == MotionEvent.ACTION_CANCEL) {
+                if (alreadyPressedSpan != null) {
+                    alreadyPressedSpan.setPressed(false);
+                }
+                alreadyPressedSpan = null;
+                Selection.removeSelection(buffer);
+            }
+
             if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN) {
-                int x = (int) event.getX();
-                int y = (int) event.getY();
 
-                x -= widget.getTotalPaddingLeft();
-                y -= widget.getTotalPaddingTop();
-
-                x += widget.getScrollX();
-                y += widget.getScrollY();
-
-                Layout layout = widget.getLayout();
-                int line = layout.getLineForVertical(y);
-                int off = layout.getOffsetForHorizontal(line, x);
-
-                ClickableSpan[] link = buffer.getSpans(off, off, ClickableSpan.class);
-
-                if (link.length != 0) {
+                TouchableUrlSpan touchedSpan = getTouchedSpan(event, widget, buffer);
+                if (touchedSpan != null) {
                     if (action == MotionEvent.ACTION_DOWN) {
-                        Selection.setSelection(buffer, buffer.getSpanStart(link[0]), buffer.getSpanEnd(link[0]));
+                        Selection.setSelection(buffer, buffer.getSpanStart(touchedSpan), buffer.getSpanEnd(touchedSpan));
+                        alreadyPressedSpan = touchedSpan;
+                        alreadyPressedSpan.setPressed(true);
                     } else if (action == MotionEvent.ACTION_UP) {
-                        link[0].onClick(widget);
+                        if (alreadyPressedSpan != null && alreadyPressedSpan == touchedSpan) {
+                            alreadyPressedSpan.onClick(widget);
+                            alreadyPressedSpan.setPressed(false);
+                        }
+                        alreadyPressedSpan = null;
+                        Selection.removeSelection(buffer);
                     }
                     return true;
+
                 } else {
+                    if (action == MotionEvent.ACTION_MOVE) {
+                        if (alreadyPressedSpan != null) {
+                            alreadyPressedSpan.setPressed(false);
+                            alreadyPressedSpan = null;
+                            Selection.removeSelection(buffer);
+                            return false;
+                        }
+                    }
                     Selection.removeSelection(buffer);
                 }
             }
+
         }
         return false;
+    }
+
+    private TouchableUrlSpan getTouchedSpan(MotionEvent event, TextView widget, Spannable buffer) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        x -= widget.getTotalPaddingLeft();
+        y -= widget.getTotalPaddingTop();
+
+        x += widget.getScrollX();
+        y += widget.getScrollY();
+
+        Layout layout = widget.getLayout();
+        int line = layout.getLineForVertical(y);
+        int off = layout.getOffsetForHorizontal(line, x);
+
+        TouchableUrlSpan[] link = buffer.getSpans(off, off, TouchableUrlSpan.class);
+
+        TouchableUrlSpan touchableUrlSpan = null;
+
+        if (link.length != 0) {
+            touchableUrlSpan = link[0];
+        }
+        return touchableUrlSpan;
+    }
+
+    private class TouchableUrlSpan extends URLSpan{
+
+        private boolean isPressed = false;
+
+        public TouchableUrlSpan(String url) {
+            super(url);
+        }
+
+        public TouchableUrlSpan(Parcel src) {
+            super(src);
+        }
+
+        public void setPressed(boolean isPressed) {
+            this.isPressed = isPressed;
+            invalidate();
+        }
+
+        @Override public void updateDrawState(TextPaint ds) {
+            super.updateDrawState(ds);
+            ds.setUnderlineText(isPressed);
+        }
     }
 }
