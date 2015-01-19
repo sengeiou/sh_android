cbbc90663d6b3ed83c3c5df8916f83890e3125f3
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-01-19 12:16:32 +0100

    Detectar shot Not Watching en  timeline y actualizar Watch Number

--- app/src/main/java/com/shootr/android/task/jobs/timeline/RetrieveNewShotsTimeLineJob.java
+++ app/src/main/java/com/shootr/android/task/jobs/timeline/RetrieveNewShotsTimeLineJob.java
@@ -3,6 +3,7 @@ package com.shootr.android.task.jobs.timeline;
 import android.app.Application;
 import com.path.android.jobqueue.network.NetworkUtil;
 import com.shootr.android.domain.repository.SessionRepository;
+import com.shootr.android.gcm.event.RequestWatchByPushEvent;
 import com.squareup.otto.Bus;
 
 import com.shootr.android.db.DatabaseContract;
@@ -21,31 +22,45 @@ import javax.inject.Inject;
 
 public class RetrieveNewShotsTimeLineJob extends TimelineJob<NewShotsReceivedEvent>{
 
+    private final Bus bus;
     private ShotManager shotManager;
     private ShootrService service;
-    private SessionRepository sessionRepository;
 
     @Inject public RetrieveNewShotsTimeLineJob(Application context, Bus bus, ShootrService service, NetworkUtil networkUtil,
       ShotManager shotManager, FollowManager followManager, SessionRepository sessionRepository) {
         super(context, bus, networkUtil, followManager, sessionRepository);
+        this.bus = bus;
         this.shotManager = shotManager;
         this.service = service;
-        this.sessionRepository = sessionRepository;
     }
 
     @Override protected void run() throws SQLException, IOException {
         List<ShotModel> updatedTimeline;
-        List<ShotEntity> newShots = new CopyOnWriteArrayList<>();
+        List<ShotEntity> newShots = new ArrayList<>();
 
         Long lastModifiedDate = shotManager.getLastModifiedDate(DatabaseContract.ShotTable.TABLE);
         if(!getFollowingIds().isEmpty()) {
              newShots = service.getNewShots(getFollowingIds(), lastModifiedDate);
+            detectNotWatchingAndNotify(newShots);
             //TODO what if newshots is empty?
             shotManager.saveShots(newShots);
         }
         updatedTimeline = shotManager.retrieveTimelineWithUsers();
         updatedTimeline = filterShots(updatedTimeline);
         postSuccessfulEvent(new NewShotsReceivedEvent(updatedTimeline, newShots.size()));
+    }
+
+    /**
+     * Nota: No me gusta nada de nada hacer esto así ¬¬
+     * @author Rafa
+     */
+    private void detectNotWatchingAndNotify(List<ShotEntity> newShots) {
+        for (ShotEntity newShot : newShots) {
+            if (newShot.getType() == ShotEntity.TYPE_WATCH_NEGATIVE) {
+                bus.post(new RequestWatchByPushEvent());
+                break;
+            }
+        }
     }
 
     @Override protected boolean isNetworkRequired() {
