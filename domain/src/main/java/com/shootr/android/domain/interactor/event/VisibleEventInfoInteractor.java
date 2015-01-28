16ce85851d39d19aec9d3d6900f6eebfb7abc486
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-01-28 18:08:39 +0100

    Modificado EventInfoInteractor para extraer la lógica del repositorio.
    Ahora es el interactor el que saca los datos y los relaciona.
    Utiliza repositorios de event y watch locales y remotos.
    Hace 2 llamadas de respuesta al callback.

--- domain/src/main/java/com/shootr/android/domain/interactor/event/VisibleEventInfoInteractor.java
+++ domain/src/main/java/com/shootr/android/domain/interactor/event/VisibleEventInfoInteractor.java
@@ -1,34 +1,92 @@
 package com.shootr.android.domain.interactor.event;
 
+import com.shootr.android.domain.Event;
 import com.shootr.android.domain.EventInfo;
+import com.shootr.android.domain.User;
+import com.shootr.android.domain.Watch;
+import com.shootr.android.domain.executor.PostExecutionThread;
 import com.shootr.android.domain.interactor.Interactor;
 import com.shootr.android.domain.interactor.InteractorHandler;
-import com.shootr.android.domain.repository.EventInfoRepository;
+import com.shootr.android.domain.repository.EventRepository;
+import com.shootr.android.domain.repository.LocalRepository;
+import com.shootr.android.domain.repository.RemoteRepository;
+import com.shootr.android.domain.repository.UserRepository;
+import com.shootr.android.domain.repository.WatchRepository;
+import java.util.List;
 import javax.inject.Inject;
 
 public class VisibleEventInfoInteractor implements Interactor {
 
     private final InteractorHandler interactorHandler;
-    private final EventInfoRepository eventInfoRepository;
+    private final PostExecutionThread postExecutionThread;
+    private final UserRepository userRepository;
+    private final WatchRepository remoteWatchRepository;
+    private final WatchRepository localWatchRepository;
+    private final EventRepository remoteEventRepository;
+    private final EventRepository localEventRepository;
+    private Callback callback;
 
-    @Inject public VisibleEventInfoInteractor(InteractorHandler interactorHandler, EventInfoRepository eventInfoRepository) {
+    @Inject public VisibleEventInfoInteractor(InteractorHandler interactorHandler,
+      PostExecutionThread postExecutionThread, @LocalRepository UserRepository userRepository,
+      @RemoteRepository WatchRepository remoteWatchRepository, @LocalRepository WatchRepository localWatchRepository,
+      @RemoteRepository EventRepository remoteEventRepository, @LocalRepository EventRepository localEventRepository) {
         this.interactorHandler = interactorHandler;
-        this.eventInfoRepository = eventInfoRepository;
+        this.postExecutionThread = postExecutionThread;
+        this.userRepository = userRepository;
+        this.remoteWatchRepository = remoteWatchRepository;
+        this.localWatchRepository = localWatchRepository;
+        this.remoteEventRepository = remoteEventRepository;
+        this.localEventRepository = localEventRepository;
     }
 
-    public void obtainEventInfo() {
+    public void obtainEventInfo(Callback callback) {
+        this.callback = callback;
         interactorHandler.execute(this);
     }
 
     @Override public void execute() throws Throwable {
-        eventInfoRepository.loadVisibleEventInfo(new EventInfoRepository.EventInfoCallback() {
-            @Override public void onLoaded(EventInfo eventInfo) {
-                interactorHandler.sendUiMessage(eventInfo);
-            }
+        obtainLocalEventInfo();
+        obtainRemoteEventInfo();
+    }
 
-            @Override public void onError(Throwable error) {
-                interactorHandler.sendError(error);
+    protected void obtainLocalEventInfo() {
+        EventInfo eventInfo = getEventInfo(localWatchRepository, localEventRepository);
+        notifyLoaded(eventInfo);
+    }
+
+    protected void obtainRemoteEventInfo() {
+        EventInfo eventInfo = getEventInfo(remoteWatchRepository, remoteEventRepository);
+        notifyLoaded(eventInfo);
+    }
+
+    protected EventInfo getEventInfo(WatchRepository watchRepository, EventRepository eventRepository) {
+        Watch currentVisibleWatch = watchRepository.getCurrentVisibleWatch();
+        Event visibleEvent = eventRepository.getEventById(currentVisibleWatch.getIdEvent());
+
+        List<User> people = userRepository.getPeople();
+        List<Watch> watchesFromPeople = watchRepository.getWatchesFromUsersAndEvent(people, visibleEvent.getId());
+
+        return buildEventInfo(visibleEvent, currentVisibleWatch, watchesFromPeople);
+    }
+
+    private EventInfo buildEventInfo(Event currentVisibleEvent, Watch visibleEventWatch, List<Watch> followingWatches) {
+        return EventInfo.builder()
+          .event(currentVisibleEvent)
+          .currentUserWatch(visibleEventWatch)
+          .watchers(followingWatches)
+          .build();
+    }
+
+    private void notifyLoaded(final EventInfo eventInfo) {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                callback.onLoaded(eventInfo);
             }
         });
+    }
+
+    public interface Callback {
+
+        void onLoaded(EventInfo eventInfo);
     }
 }
