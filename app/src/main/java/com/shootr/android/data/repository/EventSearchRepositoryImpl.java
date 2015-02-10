dcf68bdd3bc9df1c4e40376cfac3e19778845b69
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-02-10 16:11:06 +0100

    Al buscar eventos los guarda en la base de datos
    Para poder seleccionar un evento en offline y que el evento exista en local.

--- app/src/main/java/com/shootr/android/data/repository/EventSearchRepositoryImpl.java
+++ app/src/main/java/com/shootr/android/data/repository/EventSearchRepositoryImpl.java
@@ -1,33 +1,48 @@
 package com.shootr.android.data.repository;
 
+import com.shootr.android.data.entity.EventEntity;
 import com.shootr.android.data.entity.EventSearchEntity;
 import com.shootr.android.data.mapper.EventSearchEntityMapper;
 import com.shootr.android.data.repository.datasource.Cached;
+import com.shootr.android.data.repository.datasource.event.EventDataSource;
 import com.shootr.android.data.repository.datasource.event.EventSearchDataSource;
 import com.shootr.android.domain.EventSearchResult;
 import com.shootr.android.domain.repository.EventSearchRepository;
-import com.shootr.android.domain.repository.Remote;
+import com.shootr.android.domain.repository.Local;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 
 public class EventSearchRepositoryImpl implements EventSearchRepository {
 
     private final EventSearchDataSource cachedEventSearchDataSource;
+    private final EventDataSource localEventDataSource;
     private final EventSearchEntityMapper eventSearchEntityMapper;
 
     @Inject public EventSearchRepositoryImpl(@Cached EventSearchDataSource cachedEventSearchDataSource,
-      EventSearchEntityMapper eventSearchEntityMapper) {
+      @Local EventDataSource localEventDataSource, EventSearchEntityMapper eventSearchEntityMapper) {
         this.cachedEventSearchDataSource = cachedEventSearchDataSource;
+        this.localEventDataSource = localEventDataSource;
         this.eventSearchEntityMapper = eventSearchEntityMapper;
     }
 
     @Override public List<EventSearchResult> getDefaultEvents() {
         List<EventSearchEntity> defaultEvents = cachedEventSearchDataSource.getDefaultEvents();
+        saveEvents(defaultEvents);
         return eventSearchEntityMapper.transform(defaultEvents);
     }
 
     @Override public List<EventSearchResult> getEvents(String query) {
         List<EventSearchEntity> eventSearchEntities = cachedEventSearchDataSource.getEvents(query);
+        saveEvents(eventSearchEntities);
         return eventSearchEntityMapper.transform(eventSearchEntities);
+    }
+
+    private void saveEvents(List<EventSearchEntity> eventResults) {
+        List<EventEntity> eventEntities = new ArrayList<>(eventResults.size());
+        for (EventSearchEntity eventResult : eventResults) {
+            eventEntities.add(eventResult);
+        }
+        localEventDataSource.putEvents(eventEntities);
     }
 }
