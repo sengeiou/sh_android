2fd35054eaacdb328ec9da0bdd47f4c166da8e09
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-02-18 13:21:28 +0100

    Use generic json mapper instead of jackson concrete implementation

--- app/src/main/java/com/shootr/android/service/ApiModule.java
+++ app/src/main/java/com/shootr/android/service/ApiModule.java
@@ -3,6 +3,8 @@ package com.shootr.android.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shootr.android.domain.repository.PhotoService;
 import com.shootr.android.service.dataservice.ShootrPhotoService;
+import com.sloydev.jsonadapters.JsonAdapter;
+import com.sloydev.jsonadapters.jackson.JacksonAdapter;
 import dagger.Module;
 import dagger.Provides;
 import com.shootr.android.service.dataservice.ShootrDataService;
@@ -34,6 +36,10 @@ public final class ApiModule {
 
     @Provides @Singleton ObjectMapper provideObjectMapper() {
         return new ObjectMapper();
+    }
+
+    @Provides @Singleton JsonAdapter provideJsonAdapter(ObjectMapper objectMapper) {
+        return new JacksonAdapter(objectMapper);
     }
 
     @Provides @Singleton Endpoint provideEndpoint() {
