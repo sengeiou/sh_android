69d0f0350ca4ec6866c257caae38c960b5321ea3
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-03-04 12:22:22 +0100

    Cambiada transiciones de DraftItemView por animaciones a mano
    Usa animaciones de expand/collapse programadas a mano, tal como hace 
    el historial de llamadas de lollipop
    (https://android.googlesource.com/platform/packages/apps/Dialer/+/lollipop-release/src/com/android/dialer/calllog/CallLogFragment.java #552)

--- app/src/main/java/com/shootr/android/ui/widgets/DraftItemView.java
+++ app/src/main/java/com/shootr/android/ui/widgets/DraftItemView.java
@@ -1,11 +1,14 @@
 package com.shootr.android.ui.widgets;
 
-import android.animation.LayoutTransition;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
-import android.os.Build;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 import butterknife.ButterKnife;
@@ -22,6 +25,11 @@ public class DraftItemView extends ForegroundRelativeLayout {
     @InjectView(R.id.shot_separator) View separator;
 
     private boolean isExpanded;
+    private float expandedElevation;
+    private int fadeInStartDelay;
+    private int fadeInDuration;
+    private int fadeOutDuration;
+    private int expandCollapseDuration;
 
     public DraftItemView(Context context) {
         super(context);
@@ -41,7 +49,11 @@ public class DraftItemView extends ForegroundRelativeLayout {
 
     public void init() {
         ButterKnife.inject(this, this);
-        setupTransitions();
+        expandedElevation = getResources().getDimension(R.dimen.card_elevation);
+        fadeInStartDelay = getResources().getInteger(R.integer.draft_buttons_fade_in_start);
+        fadeInDuration = getResources().getInteger(R.integer.draft_buttons_fade_in_duration);
+        fadeOutDuration = getResources().getInteger(R.integer.draft_buttons_fade_out_duration);
+        expandCollapseDuration = getResources().getInteger(R.integer.draft_expand_collapse_duration);
     }
 
     public void collapse() {
@@ -53,6 +65,8 @@ public class DraftItemView extends ForegroundRelativeLayout {
         setBackgroundColor(0x00ffffff);
         separator.setVisibility(View.VISIBLE);
         isExpanded = false;
+
+        handleExpandCollapseAnimation();
     }
 
     public void expand() {
@@ -60,25 +74,76 @@ public class DraftItemView extends ForegroundRelativeLayout {
             return;
         }
         setBackgroundColor(getResources().getColor(R.color.white));
-        ViewCompat.setTranslationZ(this, getResources().getDimension(R.dimen.card_elevation));
+        ViewCompat.setTranslationZ(this, expandedElevation);
         draftButtons.setVisibility(View.VISIBLE);
         separator.setVisibility(View.INVISIBLE);
         isExpanded = true;
+
+        handleExpandCollapseAnimation();
+    }
+
+    private void handleExpandCollapseAnimation() {
+        final ViewGroup view = this;
+        final int startingHeight = view.getHeight();
+        final ViewTreeObserver observer = this.getViewTreeObserver();
+        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override public boolean onPreDraw() {
+                if (observer.isAlive()) {
+                    observer.removeOnPreDrawListener(this);
+                }
+
+                final int endingHeight = view.getHeight();
+                final int distance = Math.abs(endingHeight - startingHeight);
+                final int baseHeight = Math.min(endingHeight, startingHeight);
+                final boolean isExpand = endingHeight > startingHeight;
+
+                view.getLayoutParams().height = startingHeight;
+                if (!isExpand) {
+                    draftButtons.setVisibility(VISIBLE);
+                }
+
+                // Buttons animation
+                if (isExpand) {
+                    draftButtons.setAlpha(0f);
+                    draftButtons.animate().alpha(1f).setStartDelay(fadeInStartDelay).setDuration(fadeInDuration).start();
+                } else {
+                    draftButtons.setAlpha(1f);
+                    draftButtons.animate().alpha(0f).setDuration(fadeOutDuration).start();
+                }
+                view.requestLayout();
+
+                //Expansion and shadow animation
+                final ValueAnimator animator = isExpand ? ValueAnimator.ofFloat(0f, 1f) : ValueAnimator.ofFloat(1f, 0f);
+                //TODO scroll to visible
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override public void onAnimationUpdate(ValueAnimator animation) {
+                        Float value = (Float) animation.getAnimatedValue();
+                        view.getLayoutParams().height = (int) (value * distance + baseHeight);
+                        float elevation = expandedElevation * value;
+                        ViewCompat.setTranslationZ(view, elevation);
+                        view.requestLayout();
+                    }
+                });
+
+                // Set final values after animation
+                animator.addListener(new AnimatorListenerAdapter() {
+                    @Override public void onAnimationEnd(Animator animation) {
+                        view.getLayoutParams().height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                        if (isExpand) {
+                            //draftButtons.setAlpha(1f);
+                        } else {
+                            draftButtons.setVisibility(GONE);
+                        }
+                    }
+                });
+
+                animator.setDuration(expandCollapseDuration);
+                animator.start();
+
+                return false;
+            }
+        });
     }
 
 
-    private void setupTransitions() {
-        LayoutTransition transition = new LayoutTransition();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            transition.disableTransitionType(LayoutTransition.DISAPPEARING);
-        } else {
-            transition.setAnimator(LayoutTransition.DISAPPEARING, null);
-        }
-        transition.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
-        transition.setDuration(LayoutTransition.CHANGE_DISAPPEARING, 250);
-        transition.setStartDelay(LayoutTransition.APPEARING, 50);
-        transition.setDuration(LayoutTransition.APPEARING, 200);
-        transition.setDuration(LayoutTransition.CHANGE_APPEARING, 100);
-        this.setLayoutTransition(transition);
-    }
 }
