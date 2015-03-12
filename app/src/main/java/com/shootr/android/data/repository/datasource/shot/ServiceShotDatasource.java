b758f700d7228cb9f1b25a3801ee89dc960e63a1
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-03-12 17:53:21 +0100

    Filtra shots tipo 2 al pedirlos a servidor y notifica por bus

--- app/src/main/java/com/shootr/android/data/repository/datasource/shot/ServiceShotDatasource.java
+++ app/src/main/java/com/shootr/android/data/repository/datasource/shot/ServiceShotDatasource.java
@@ -2,18 +2,23 @@ package com.shootr.android.data.repository.datasource.shot;
 
 import com.shootr.android.data.entity.ShotEntity;
 import com.shootr.android.domain.TimelineParameters;
+import com.shootr.android.domain.bus.BusPublisher;
+import com.shootr.android.domain.bus.WatchUpdateRequest;
 import com.shootr.android.domain.exception.ServerCommunicationException;
 import com.shootr.android.service.ShootrService;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 
 public class ServiceShotDatasource implements ShotDataSource {
 
     private final ShootrService shootrService;
+    private final BusPublisher busPublisher;
 
-    @Inject public ServiceShotDatasource(ShootrService shootrService) {
+    @Inject public ServiceShotDatasource(ShootrService shootrService, BusPublisher busPublisher) {
         this.shootrService = shootrService;
+        this.busPublisher = busPublisher;
     }
 
     @Override public ShotEntity putShot(ShotEntity shotEntity) {
@@ -32,9 +37,29 @@ public class ServiceShotDatasource implements ShotDataSource {
 
     @Override public List<ShotEntity> getShotsForTimeline(TimelineParameters parameters) {
         try {
-            return shootrService.getShotsByParameters(parameters);
+            return filterSyncShots(shootrService.getShotsByParameters(parameters));
         } catch (IOException e) {
             throw new ServerCommunicationException(e);
+        }
+    }
+
+    private List<ShotEntity> filterSyncShots(List<ShotEntity> shotEntities) {
+        notifySyncTrigger(shotEntities);
+        List<ShotEntity> filtered = new ArrayList<>();
+        for (ShotEntity shotEntity : shotEntities) {
+            if (shotEntity.getType() != ShotEntity.TYPE_TRIGGER_SYNC_NOT_SHOW) {
+                filtered.add(shotEntity);
+            }
+        }
+        return filtered;
+    }
+
+    private void notifySyncTrigger(List<ShotEntity> newShots) {
+        for (ShotEntity newShot : newShots) {
+            if (newShot.getType() == ShotEntity.TYPE_TRIGGER_SYNC || newShot.getType() == ShotEntity.TYPE_TRIGGER_SYNC_NOT_SHOW) {
+                busPublisher.post(new WatchUpdateRequest.Event());
+                break;
+            }
         }
     }
 }
