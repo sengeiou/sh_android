659b14a8c9630292725745dbe3a26e9ce6fc75d9
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-03-17 19:28:19 +0100

    Mejorado navdrawer header
    Aumentado tamaño (mal copiado)
    Cambiada imagen de fondo por una material

--- app/src/main/java/com/shootr/android/ui/activities/BaseNavDrawerToolbarActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/BaseNavDrawerToolbarActivity.java
@@ -1,21 +1,25 @@
 package com.shootr.android.ui.activities;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.LayoutRes;
-import android.view.ViewGroup;
 import com.shootr.android.R;
+import com.shootr.android.domain.repository.SessionRepository;
 import com.shootr.android.ui.NavigationDrawerDecorator;
 import com.shootr.android.ui.ToolbarDecorator;
 import com.shootr.android.ui.ViewContainerDecorator;
 import com.shootr.android.ui.base.BaseDecoratedActivity;
+import com.shootr.android.util.PicassoWrapper;
 import java.util.Arrays;
 import java.util.List;
+import javax.inject.Inject;
 
 public abstract class BaseNavDrawerToolbarActivity extends BaseDecoratedActivity {
 
     private NavigationDrawerDecorator navigationDrawerDecorator;
     private ToolbarDecorator toolbarDecorator;
+
+    @Inject SessionRepository sessionRepository;
+    @Inject PicassoWrapper picasso;
 
     @Override protected void setContent(@LayoutRes int layoutResource) {
         super.setContent(layoutResource);
@@ -25,7 +29,7 @@ public abstract class BaseNavDrawerToolbarActivity extends BaseDecoratedActivity
     }
 
     @Override protected List<ViewContainerDecorator> getDecorators() {
-        navigationDrawerDecorator = new NavigationDrawerDecorator(this, getNavDrawerItemId());
+        navigationDrawerDecorator = new NavigationDrawerDecorator(this, picasso, getNavDrawerItemId());
         toolbarDecorator = new ToolbarDecorator(this);
         return Arrays.asList(navigationDrawerDecorator, toolbarDecorator);
     }
@@ -52,6 +56,7 @@ public abstract class BaseNavDrawerToolbarActivity extends BaseDecoratedActivity
                 finish();
             }
         });
+        navigationDrawerDecorator.bindUser(sessionRepository.getCurrentUser());
     }
 
     public ToolbarDecorator getToolbarDecorator() {
