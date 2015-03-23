a6a84d7ef858925a397e36b0bffa7877b13eece6
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-03-23 18:56:58 +0100

    Cambiado endpoint del dataservices

--- app/src/main/java/com/shootr/android/service/ApiModule.java
+++ app/src/main/java/com/shootr/android/service/ApiModule.java
@@ -22,7 +22,7 @@ import javax.inject.Singleton;
 )
 public final class ApiModule {
 
-    public static final String PRODUCTION_API_URL = "http://api.shootr.com/data-services/rest/generic/";
+    public static final String PRODUCTION_API_URL = "http://dataservices.shootr.com/data-services/rest/generic/";
 
     @Provides @Singleton ShootrService provideShootrService(ShootrDataService dataService) {
         return dataService;
