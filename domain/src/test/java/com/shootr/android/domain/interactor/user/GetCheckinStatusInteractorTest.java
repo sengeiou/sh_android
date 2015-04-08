90808665c0ebdb401c98034c8d74d4761729332f
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-04-08 12:19:35 +0200

    Creado Interactor para obtener el estado del checkin del usuario
    Añadido campo a User que indica si ha hecho checkin en su evento actual.

---
+++ domain/src/test/java/com/shootr/android/domain/interactor/user/GetCheckinStatusInteractorTest.java
@@ -0,0 +1,73 @@
+package com.shootr.android.domain.interactor.user;
+
+import com.shootr.android.domain.User;
+import com.shootr.android.domain.executor.PostExecutionThread;
+import com.shootr.android.domain.executor.TestPostExecutionThread;
+import com.shootr.android.domain.interactor.Interactor;
+import com.shootr.android.domain.interactor.InteractorHandler;
+import com.shootr.android.domain.interactor.TestInteractorHandler;
+import com.shootr.android.domain.repository.SessionRepository;
+import com.shootr.android.domain.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.when;
+
+public class GetCheckinStatusInteractorTest {
+
+    private GetCheckinStatusInteractor interactor;
+
+    @Mock SessionRepository sessionRepository;
+    @Mock UserRepository localUserRepository;
+    @Spy SpyCallback spyCallback = new SpyCallback();
+
+    @Before public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        InteractorHandler interactorHandler = new TestInteractorHandler();
+        PostExecutionThread postExecutionThread = new TestPostExecutionThread();
+        interactor = new GetCheckinStatusInteractor(interactorHandler, postExecutionThread,
+          localUserRepository,
+          sessionRepository);
+    }
+
+    @Test public void shouldCallbackFalseWhenCheckinInUserIsFalse() throws Exception {
+        when(localUserRepository.getUserById(anyLong())).thenReturn(currentUserWithoutCheckin());
+
+        interactor.loadCheckinStatus(spyCallback);
+
+        assertThat(spyCallback.result).isFalse();
+    }
+    @Test public void shouldCallbackTrueWhenCheckinInUserIsTrue() throws Exception {
+        when(localUserRepository.getUserById(anyLong())).thenReturn(currentUserWithCheckin());
+
+        interactor.loadCheckinStatus(spyCallback);
+
+        assertThat(spyCallback.result).isTrue();
+    }
+
+    private User currentUserWithCheckin() {
+        User user = new User();
+        user.setCheckedIn(true);
+        return user;
+    }
+
+    private User currentUserWithoutCheckin() {
+        User user = new User();
+        user.setCheckedIn(false);
+        return user;
+    }
+
+    private class SpyCallback implements Interactor.Callback<Boolean> {
+
+        Boolean result;
+
+        @Override public void onLoaded(Boolean result) {
+            this.result = result;
+        }
+    }
+}
