0bf3399c7f3bb6a6e6fc7abf716472c6a15f2b7d
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-04-15 09:28:02 +0200

    Creado interactor para obtener replies de un shot

--- app/src/main/java/com/shootr/android/ui/presenter/ShotDetailPresenter.java
+++ app/src/main/java/com/shootr/android/ui/presenter/ShotDetailPresenter.java
@@ -1,48 +1,47 @@
 package com.shootr.android.ui.presenter;
 
+import com.shootr.android.domain.Shot;
+import com.shootr.android.domain.interactor.Interactor;
+import com.shootr.android.domain.interactor.shot.GetRepliesFromShotInteractor;
 import com.shootr.android.ui.model.ShotModel;
+import com.shootr.android.ui.model.mappers.ShotModelMapper;
 import com.shootr.android.ui.views.ShotDetailView;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import javax.inject.Inject;
 
 public class ShotDetailPresenter implements Presenter {
 
+    private final GetRepliesFromShotInteractor getRepliesFromShotInteractor;
+    private final ShotModelMapper shotModelMapper;
+
     private ShotDetailView shotDetailView;
     private ShotModel shotModel;
 
-    @Inject public ShotDetailPresenter() {
+    @Inject public ShotDetailPresenter(GetRepliesFromShotInteractor getRepliesFromShotInteractor,
+      ShotModelMapper shotModelMapper) {
+        this.getRepliesFromShotInteractor = getRepliesFromShotInteractor;
+        this.shotModelMapper = shotModelMapper;
     }
 
     public void initialize(ShotDetailView shotDetailView, ShotModel shotModel) {
         this.shotDetailView = shotDetailView;
         this.shotModel = shotModel;
         this.setViewContent(shotModel);
+        this.loadReplies();
+    }
+
+    private void loadReplies() {
+        getRepliesFromShotInteractor.loadReplies(shotModel.getIdShot(), new Interactor.Callback<List<Shot>>() {
+            @Override public void onLoaded(List<Shot> replies) {
+                List<ShotModel> replyModels = shotModelMapper.transform(replies);
+                shotDetailView.renderReplies(replyModels);
+            }
+        });
     }
 
     private void setViewContent(ShotModel shotModel) {
         shotDetailView.renderShot(shotModel);
         shotDetailView.setReplyUsername(shotModel.getUsername());
-        shotDetailView.renderReplies(mockReplies());
-    }
-
-    private List<ShotModel> mockReplies() {
-        return Arrays.asList(
-          mockReply(),
-          mockReply(),
-          mockReply(),
-          mockReply()
-        );
-    }
-
-    private ShotModel mockReply() {
-        ShotModel shotModel = new ShotModel();
-        shotModel.setUsername("usuario");
-        shotModel.setComment("Hola, esto es un reply");
-        shotModel.setPhoto("http://i.ytimg.com/vi/mSFTRoBY99s/hqdefault.jpg");
-        shotModel.setCsysBirth(new Date());
-        return shotModel;
     }
 
     public void imageClick() {
---
+++ domain/src/main/java/com/shootr/android/domain/interactor/shot/GetRepliesFromShotInteractor.java
@@ -0,0 +1,55 @@
+package com.shootr.android.domain.interactor.shot;
+
+import com.shootr.android.domain.Shot;
+import com.shootr.android.domain.executor.PostExecutionThread;
+import com.shootr.android.domain.interactor.Interactor;
+import com.shootr.android.domain.interactor.InteractorHandler;
+import com.shootr.android.domain.repository.Local;
+import com.shootr.android.domain.repository.Remote;
+import com.shootr.android.domain.repository.ShotRepository;
+import java.util.List;
+import javax.inject.Inject;
+
+public class GetRepliesFromShotInteractor implements Interactor {
+
+    private final InteractorHandler interactorHandler;
+    private final PostExecutionThread postExecutionThread;
+    private final ShotRepository localShotRepository;
+    private final ShotRepository remoteShotRepository;
+    private Long shotId;
+    private Callback<List<Shot>> callback;
+
+    @Inject public GetRepliesFromShotInteractor(InteractorHandler interactorHandler,
+      PostExecutionThread postExecutionThread, @Local ShotRepository localShotRepository,
+      @Remote ShotRepository remoteShotRepository) {
+        this.interactorHandler = interactorHandler;
+        this.postExecutionThread = postExecutionThread;
+        this.localShotRepository = localShotRepository;
+        this.remoteShotRepository = remoteShotRepository;
+    }
+
+    public void loadReplies(Long shotId, Callback<List<Shot>> callback) {
+        this.shotId = shotId;
+        this.callback = callback;
+        interactorHandler.execute(this);
+    }
+
+    @Override public void execute() throws Throwable {
+        List<Shot> localReplies = localShotRepository.getReplies(shotId);
+        if (!localReplies.isEmpty()) {
+            notifyLoaded(localReplies);
+        }
+        List<Shot> updatedReplies = remoteShotRepository.getReplies(shotId);
+        if (!updatedReplies.isEmpty()) {
+            notifyLoaded(updatedReplies);
+        }
+    }
+
+    private void notifyLoaded(final List<Shot> result) {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                callback.onLoaded(result);
+            }
+        });
+    }
+}
