36df2d01e9a96ebbfb6fa8eb0289b65705e001c6
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-04-22 12:41:06 +0200

    Validaciones UI
    Añadidas las validaciones de Server que muestran en UI errores de registro

--- domain/src/main/java/com/shootr/android/domain/interactor/user/CreateAccountInteractor.java
+++ domain/src/main/java/com/shootr/android/domain/interactor/user/CreateAccountInteractor.java
@@ -2,7 +2,9 @@ package com.shootr.android.domain.interactor.user;
 
 import com.shootr.android.domain.User;
 import com.shootr.android.domain.exception.DomainValidationException;
+import com.shootr.android.domain.exception.ShootrError;
 import com.shootr.android.domain.exception.ShootrException;
+import com.shootr.android.domain.exception.ShootrServerException;
 import com.shootr.android.domain.executor.PostExecutionThread;
 import com.shootr.android.domain.interactor.Interactor;
 import com.shootr.android.domain.interactor.InteractorHandler;
@@ -48,7 +50,7 @@ public class CreateAccountInteractor implements Interactor {
                 shootrUserService.createAccount(user.getUsername(), user.getEmail(),password);
                 notifyLoaded();
             }catch (ShootrException shootrException){
-                notifyError(shootrException);
+                handleServerError(shootrException);
             }
         }
     }
@@ -87,5 +89,49 @@ public class CreateAccountInteractor implements Interactor {
         user.setUsername(username);
         user.setEmail(email);
         return user;
+    }
+
+    private void handleServerError(ShootrException e) {
+        if (e.getCause() instanceof ShootrServerException) {
+            ShootrServerException serverException = (ShootrServerException) e.getCause();
+            String errorCode = serverException.getShootrError().getErrorCode();
+            FieldValidationError validationError = validationErrorFromCode(errorCode);
+            if (validationError != null) {
+                notifyError(new DomainValidationException(validationError));
+                return;
+            }
+        }
+        notifyError(e);
+    }
+
+    private FieldValidationError validationErrorFromCode(String errorCode) {
+        int field = fieldFromErrorCode(errorCode);
+        if (field != 0) {
+            return new FieldValidationError(errorCode, field);
+        } else {
+            return null;
+        }
+    }
+
+    private int fieldFromErrorCode(String errorCode) {
+        switch (errorCode) {
+            case ShootrError.ERROR_CODE_REGISTRATION_EMAIL_IN_USE:
+            case ShootrError.ERROR_CODE_REGISTRATION_EMAIL_INVALID_FORMAT:
+            case ShootrError.ERROR_CODE_REGISTRATION_EMAIL_NULL:
+                return CreateUserValidator.FIELD_EMAIL;
+            case ShootrError.ERROR_CODE_REGISTRATION_USERNAME_DUPLICATE:
+            case ShootrError.ERROR_CODE_REGISTRATION_USERNAME_INVALID_CHARACTERS:
+            case ShootrError.ERROR_CODE_REGISTRATION_USERNAME_NULL:
+            case ShootrError.ERROR_CODE_REGISTRATION_USERNAME_TOO_LONG:
+            case ShootrError.ERROR_CODE_REGISTRATION_USERNAME_TOO_SHORT:
+                return CreateUserValidator.FIELD_USERNAME;
+            case ShootrError.ERROR_CODE_REGISTRATION_PASSWORD_EQUALS_USERNAME:
+            case ShootrError.ERROR_CODE_REGISTRATION_PASSWORD_INVALID_CHARACTERS:
+            case ShootrError.ERROR_CODE_REGISTRATION_PASSWORD_NULL:
+            case ShootrError.ERROR_CODE_REGISTRATION_PASSWORD_TOO_LONG:
+            case ShootrError.ERROR_CODE_REGISTRATION_PASSWORD_TOO_SHORT:
+                return CreateUserValidator.FIELD_PASSWORD;
+        }
+        return 0;
     }
 }
