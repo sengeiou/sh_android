926fbd9f5aa5673564db549efb7c6c7f7df2606e
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-04-23 16:53:22 +0200

    Manejo excepciones en performLogin de DataserviceLoginGateway
    Se ha cambiado el método para permitir capturar excepciones tanto de server communication como errores provocados por el uso de un login erróneo

--- app/src/main/java/com/shootr/android/data/service/DataserviceLoginGateway.java
+++ app/src/main/java/com/shootr/android/data/service/DataserviceLoginGateway.java
@@ -4,7 +4,10 @@ import com.shootr.android.data.entity.UserEntity;
 import com.shootr.android.data.mapper.UserEntityMapper;
 import com.shootr.android.domain.LoginResult;
 import com.shootr.android.domain.User;
+import com.shootr.android.domain.exception.ServerCommunicationException;
+import com.shootr.android.domain.service.user.LoginException;
 import com.shootr.android.domain.service.user.LoginGateway;
+import com.shootr.android.exception.ServerException;
 import com.shootr.android.service.ShootrService;
 import java.io.IOException;
 import javax.inject.Inject;
@@ -14,15 +17,23 @@ public class DataserviceLoginGateway implements LoginGateway {
     private final ShootrService shootrService;
     private final UserEntityMapper userEntityMapper;
 
-    @Inject public DataserviceLoginGateway(ShootrService shootrService, UserEntityMapper userEntityMapper){
+    @Inject public DataserviceLoginGateway(ShootrService shootrService, UserEntityMapper userEntityMapper) {
         this.shootrService = shootrService;
         this.userEntityMapper = userEntityMapper;
     }
 
     @Override public LoginResult performLogin(String usernameOrEmail, String password) throws IOException {
-        UserEntity loggedInUserEntity = shootrService.login(usernameOrEmail, password);
-        User loggedInUser= userEntityMapper.transform(loggedInUserEntity);
-        String sessionToken = loggedInUserEntity.getSessionToken();
-        return new LoginResult(loggedInUser, sessionToken);
+        try {
+            UserEntity loggedInUserEntity = shootrService.login(usernameOrEmail, password);
+            User loggedInUser = userEntityMapper.transform(loggedInUserEntity);
+            String sessionToken = loggedInUserEntity.getSessionToken();
+            return new LoginResult(loggedInUser, sessionToken);
+        } catch (ServerException e) {
+            if (ServerException.G025.equals(e.getErrorCode())) {
+                throw new LoginException(e);
+            } else {
+                throw new ServerCommunicationException(e);
+            }
+        }
     }
 }
