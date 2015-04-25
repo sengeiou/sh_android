c3823b5cd230392dc855ea8c13d7000b59df9869
Sloy <rafa.vazsan@gmail.com>
2015-04-25 21:36:16 +0200

    Añadidos tests para verificar el correcto comportamiento modificado en 13d05a2
    Verifican que cuando algún campo es inválido y se sale de éste se muestre error y no se pida foco,
    pero cuando se validan los campos al intentar crear la cuenta se muestre error y sí se pida foco.

--- app/src/test/java/com/shootr/android/ui/presenter/EmailRegistrationPresenterTest.java
+++ app/src/test/java/com/shootr/android/ui/presenter/EmailRegistrationPresenterTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -18,7 +19,9 @@ public class EmailRegistrationPresenterTest {
     private static final String EMAIL_VALID = "email@domain.com";
     private static final String PASSWORD_VALID = "123456";
     private static final String USERNAME_VALID = "username";
-    
+    private static final String USERNAME_INVALID = "user name invalid";
+    private static final String PASSWORD_INVALID = "p·¬ssword";
+
     @Mock EmailRegistrationView emailRegistrationView;
     @Mock CreateAccountInteractor createAccountInteractor;
     @Mock ErrorMessageFactory errorMessageFactory;
@@ -28,6 +31,7 @@ public class EmailRegistrationPresenterTest {
     @Before public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         presenter = new EmailRegistrationPresenter(createAccountInteractor, errorMessageFactory);
+        presenter.setView(emailRegistrationView);
     }
 
     @Test public void shouldShowViewLoadingWhenConfirmedCreateAccount() throws Exception {
@@ -65,4 +69,115 @@ public class EmailRegistrationPresenterTest {
 
         verify(emailRegistrationView, never()).askEmailConfirmation();
     }
+
+    //region Email validation
+    @Test public void shouldShowEmailErrorWhenFocusRemovedIfEmailInvalid() throws Exception {
+        when(emailRegistrationView.getEmail()).thenReturn(EMAIL_INVALID);
+
+        presenter.emailFocusRemoved();
+
+        verify(emailRegistrationView).showEmailError(anyString());
+    }
+
+    @Test
+    public void shouldNotRequestEmailFocusWhenFocusRemovedIfEmailInvalid() throws Exception {
+        when(emailRegistrationView.getEmail()).thenReturn(EMAIL_INVALID);
+
+        presenter.emailFocusRemoved();
+
+        verify(emailRegistrationView, never()).focusOnEmailField();
+    }
+
+    @Test
+    public void shouldShowEmailErrorWhenCreateAccountIfEmailInvalid() throws Exception {
+        when(emailRegistrationView.getEmail()).thenReturn(EMAIL_INVALID);
+
+        presenter.createAccount();
+
+        verify(emailRegistrationView).showEmailError(anyString());
+    }
+
+    @Test
+    public void shouldRequestEmailFocusWhenCreateAccountIfEmailInvalid() throws Exception {
+        when(emailRegistrationView.getEmail()).thenReturn(EMAIL_INVALID);
+
+        presenter.createAccount();
+
+        verify(emailRegistrationView).focusOnEmailField();
+    }
+    //endregion
+
+    //region Username validation
+    @Test public void shouldShowUsernameErrorWhenFocusRemovedIfUsernameInvalid() throws Exception {
+        when(emailRegistrationView.getUsername()).thenReturn(USERNAME_INVALID);
+
+        presenter.usernameFocusRemoved();
+
+        verify(emailRegistrationView).showUsernameError(anyString());
+    }
+
+    @Test
+    public void shouldNotRequestUsernameFocusWhenFocusRemovedIfUsernameInvalid() throws Exception {
+        when(emailRegistrationView.getUsername()).thenReturn(USERNAME_INVALID);
+
+        presenter.usernameFocusRemoved();
+
+        verify(emailRegistrationView, never()).focusOnUsernameField();
+    }
+
+    @Test
+    public void shouldShowUsernameErrorWhenCreateAccountIfUsernameInvalid() throws Exception {
+        when(emailRegistrationView.getUsername()).thenReturn(USERNAME_INVALID);
+
+        presenter.createAccount();
+
+        verify(emailRegistrationView).showUsernameError(anyString());
+    }
+
+    @Test
+    public void shouldRequestUsernameFocusWhenCreateAccountIfUsernameInvalid() throws Exception {
+        when(emailRegistrationView.getUsername()).thenReturn(USERNAME_INVALID);
+
+        presenter.createAccount();
+
+        verify(emailRegistrationView).focusOnUsernameField();
+    }
+    //endregion
+
+    //region Password validation
+    @Test public void shouldShowPasswordErrorWhenFocusRemovedIfPasswordInvalid() throws Exception {
+        when(emailRegistrationView.getPassword()).thenReturn(PASSWORD_INVALID);
+
+        presenter.passwordFocusRemoved();
+
+        verify(emailRegistrationView).showPasswordError(anyString());
+    }
+
+    @Test
+    public void shouldNotRequestPasswordFocusWhenFocusRemovedIfPasswordInvalid() throws Exception {
+        when(emailRegistrationView.getPassword()).thenReturn(PASSWORD_INVALID);
+
+        presenter.passwordFocusRemoved();
+
+        verify(emailRegistrationView, never()).focusOnPasswordField();
+    }
+
+    @Test
+    public void shouldShowPasswordErrorWhenCreateAccountIfPasswordInvalid() throws Exception {
+        when(emailRegistrationView.getPassword()).thenReturn(PASSWORD_INVALID);
+
+        presenter.createAccount();
+
+        verify(emailRegistrationView).showPasswordError(anyString());
+    }
+
+    @Test
+    public void shouldRequestPasswordFocusWhenCreateAccountIfPasswordInvalid() throws Exception {
+        when(emailRegistrationView.getPassword()).thenReturn(PASSWORD_INVALID);
+
+        presenter.createAccount();
+
+        verify(emailRegistrationView).focusOnPasswordField();
+    }
+    //endregion
 }
