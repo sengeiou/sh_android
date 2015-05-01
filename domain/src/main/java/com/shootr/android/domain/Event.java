3d8f484a0c49cb27375f93da2c05c4fa3e59561d
Sloy <rafa.vazsan@gmail.com>
2015-05-01 20:18:22 +0200

    Añadidos equals, hashcode y toString a Event y EventSearchResult

--- domain/src/main/java/com/shootr/android/domain/Event.java
+++ domain/src/main/java/com/shootr/android/domain/Event.java
@@ -88,4 +88,43 @@ public class Event {
     public void setAuthorUsername(String authorUsername) {
         this.authorUsername = authorUsername;
     }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event)) return false;
+
+        Event event = (Event) o;
+
+        if (id != null ? !id.equals(event.id) : event.id != null) return false;
+        if (authorId != null ? !authorId.equals(event.authorId) : event.authorId != null) return false;
+        if (authorUsername != null ? !authorUsername.equals(event.authorUsername) : event.authorUsername != null) {
+            return false;
+        }
+        if (title != null ? !title.equals(event.title) : event.title != null) return false;
+        if (picture != null ? !picture.equals(event.picture) : event.picture != null) return false;
+        if (timezone != null ? !timezone.equals(event.timezone) : event.timezone != null) return false;
+        if (tag != null ? !tag.equals(event.tag) : event.tag != null) return false;
+        if (startDate != null ? !startDate.equals(event.startDate) : event.startDate != null) return false;
+        return !(endDate != null ? !endDate.equals(event.endDate) : event.endDate != null);
+    }
+
+    @Override public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (authorId != null ? authorId.hashCode() : 0);
+        result = 31 * result + (authorUsername != null ? authorUsername.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (picture != null ? picture.hashCode() : 0);
+        result = 31 * result + (timezone != null ? timezone.hashCode() : 0);
+        result = 31 * result + (tag != null ? tag.hashCode() : 0);
+        result = 31 * result + (startDate != null ? startDate.hashCode() : 0);
+        result = 31 * result + (endDate != null ? endDate.hashCode() : 0);
+        return result;
+    }
+
+    @Override public String toString() {
+        return "Event{" +
+          "id=" + id +
+          ", title='" + title + '\'' +
+          '}';
+    }
 }
--- domain/src/main/java/com/shootr/android/domain/EventSearchResult.java
+++ domain/src/main/java/com/shootr/android/domain/EventSearchResult.java
@@ -20,4 +20,27 @@ public class EventSearchResult {
     public void setWatchersNumber(int watchersNumber) {
         this.watchersNumber = watchersNumber;
     }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EventSearchResult)) return false;
+
+        EventSearchResult that = (EventSearchResult) o;
+
+        if (watchersNumber != that.watchersNumber) return false;
+        return event.equals(that.event);
+    }
+
+    @Override public int hashCode() {
+        int result = event.hashCode();
+        result = 31 * result + watchersNumber;
+        return result;
+    }
+
+    @Override public String toString() {
+        return "EventSearchResult{" +
+          "event=" + event +
+          ", watchersNumber=" + watchersNumber +
+          '}';
+    }
 }
