0f8b4058bf49a3970b55a0224459d270b3d21aac
ajimenez <ajimenez@fav24.com>
2015-05-18 16:22:42 +0200

    Lógica de la comprobación de la versión
    Obtengo la versión de la BD que tengo en SharedPreferences
    	Si no tengo versión guardada, guardo la de gradle
    	En caso contrario:
    		Si la versión que tengo es la misma que la de gradle,
    		no hay que hacer nada.
    		Si no es el caso, clear SharedPreferences y BD.

--- app/src/main/java/com/shootr/android/ShootrApplication.java
+++ app/src/main/java/com/shootr/android/ShootrApplication.java
@@ -2,19 +2,22 @@ package com.shootr.android;
 
 import android.app.Application;
 import android.content.Context;
-
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import com.crashlytics.android.Crashlytics;
 import com.facebook.stetho.Stetho;
-import dagger.ObjectGraph;
-import com.shootr.android.domain.repository.SessionRepository;
-import com.shootr.android.data.entity.UserEntity;
+import com.shootr.android.data.prefs.IntPreference;
+import com.shootr.android.data.prefs.LastDatabaseVersionCompatible;
+import com.shootr.android.db.ShootrDbOpenHelper;
 import com.shootr.android.util.LogTreeFactory;
+import dagger.ObjectGraph;
 import javax.inject.Inject;
 import timber.log.Timber;
 
 public class ShootrApplication extends Application {
 
     private ObjectGraph objectGraph;
+    @Inject @LastDatabaseVersionCompatible IntPreference lastCompatibleDatabaseVersion;
 
     @Override
     public void onCreate() {
@@ -24,11 +27,24 @@ public class ShootrApplication extends Application {
         if(!BuildConfig.DEBUG) {
             Crashlytics.start(this);
         }
-        Stetho.initialize(
-          Stetho.newInitializerBuilder(this)
+        Stetho.initialize(Stetho.newInitializerBuilder(this)
             .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
             .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
             .build());
+
+        ShootrDbOpenHelper shootrDbOpenHelper = new ShootrDbOpenHelper(getApplicationContext(),null);
+        SharedPreferences sharedPreferences = getSharedPreferences("DatabaseVersionPreferences", MODE_PRIVATE);
+        int databaseVersion = sharedPreferences.getInt("databaseVersion", 0);
+        if(databaseVersion == 0){
+            databaseVersion = BuildConfig.DATABASE_VERSION;
+        }
+        if (databaseVersion <= lastCompatibleDatabaseVersion.get()) {
+            sharedPreferences.edit().clear().apply();
+            SQLiteDatabase db = shootrDbOpenHelper.getWritableDatabase();
+            shootrDbOpenHelper.onUpgrade(db, databaseVersion, lastCompatibleDatabaseVersion.get());
+            databaseVersion = BuildConfig.DATABASE_VERSION;
+            sharedPreferences.edit().putInt("databaseVersion",databaseVersion).apply();
+        }
     }
 
     public void plantLoggerTrees() {
