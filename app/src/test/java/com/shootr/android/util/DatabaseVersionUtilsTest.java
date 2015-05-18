83cd4c0d90af4b46aa7763f1ecdc50bc86d240c0
ajimenez <ajimenez@fav24.com>
2015-05-18 19:07:03 +0200

    Tests de DatabaseVersionUtils
    Se comprueba que no se haga nada si la versión es correcta y que
    se actualice en preferences si no había ninguna versión o bien si
    era menor a la actual.

---
+++ app/src/test/java/com/shootr/android/util/DatabaseVersionUtilsTest.java
@@ -0,0 +1,103 @@
+package com.shootr.android.util;
+
+import android.content.SharedPreferences;
+import com.shootr.android.data.prefs.IntPreference;
+import com.shootr.android.db.ShootrDbOpenHelper;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DatabaseVersionUtilsTest {
+
+    @Mock IntPreference lastCompatibleDatabaseVersion;
+    private DatabaseVersionUtils databaseVersionUtils;
+    @Mock ShootrDbOpenHelper shootrDbOpenHelper;
+    @Mock DatabaseBuildVersionUtil databaseBuildVersionUtil;
+
+    SharedPreferences sharedPreferences = mock(SharedPreferences.class, RETURNS_DEEP_STUBS);
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        databaseVersionUtils = new DatabaseVersionUtils(lastCompatibleDatabaseVersion, databaseBuildVersionUtil);
+    }
+
+    @Test
+    public void shouldUpdateDatabaseVersionIfTheresNoVersion(){
+        when(lastCompatibleDatabaseVersion.get()).thenReturn(0);
+        when(databaseBuildVersionUtil.getDatabaseVersionFromBuild()).thenReturn(1);
+        setupSharedPreferencesClear();
+        databaseVersionUtils.manageCurrentDatabaseVersion(shootrDbOpenHelper, sharedPreferences);
+        verify(lastCompatibleDatabaseVersion).set(anyInt());
+    }
+
+    @Test
+    public void shouldUpdateDatabaseVersionIfVersionIsInferior(){
+        when(lastCompatibleDatabaseVersion.get()).thenReturn(1);
+        when(databaseBuildVersionUtil.getDatabaseVersionFromBuild()).thenReturn(2);
+        setupSharedPreferencesClear();
+        databaseVersionUtils.manageCurrentDatabaseVersion(shootrDbOpenHelper, sharedPreferences);
+        verify(lastCompatibleDatabaseVersion).set(anyInt());
+    }
+
+    @Test
+    public void shouldNotUpdateDatabaseVersionIfVersionIsTheSame(){
+        when(lastCompatibleDatabaseVersion.get()).thenReturn(1);
+        when(databaseBuildVersionUtil.getDatabaseVersionFromBuild()).thenReturn(1);
+        databaseVersionUtils.manageCurrentDatabaseVersion(shootrDbOpenHelper, sharedPreferences);
+        verify(lastCompatibleDatabaseVersion, never()).set(anyInt());
+    }
+
+    private void setupSharedPreferencesClear() {
+        when(sharedPreferences.edit().clear()).thenReturn(new SharedPreferences.Editor() {
+            @Override public SharedPreferences.Editor putString(String s, String s1) {
+                return null;
+            }
+
+            @Override public SharedPreferences.Editor putStringSet(String s, Set<String> set) {
+                return null;
+            }
+
+            @Override public SharedPreferences.Editor putInt(String s, int i) {
+                return null;
+            }
+
+            @Override public SharedPreferences.Editor putLong(String s, long l) {
+                return null;
+            }
+
+            @Override public SharedPreferences.Editor putFloat(String s, float v) {
+                return null;
+            }
+
+            @Override public SharedPreferences.Editor putBoolean(String s, boolean b) {
+                return null;
+            }
+
+            @Override public SharedPreferences.Editor remove(String s) {
+                return null;
+            }
+
+            @Override public SharedPreferences.Editor clear() {
+                return null;
+            }
+
+            @Override public boolean commit() {
+                return false;
+            }
+
+            @Override public void apply() {
+
+            }
+        });
+    }
+}
