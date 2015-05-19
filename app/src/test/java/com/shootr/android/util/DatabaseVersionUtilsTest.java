9604809055a9c8202f93951fcbcb9de5686c4696
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-05-19 13:14:07 +0200

    Añadidos tests para verificar borrado de datos
    Sólo se estaba testeando que se guardase bien el número de versión
    de la base de datos, pero no que se borrasen ésta ni las preferences.
    
    La necesidad de usar RETURNS_DEEP_STUBS para verificar el borrado de
    SharedPreferences con verify(sharedPreferences.edit().clear()).apply()
    revela una violación de la Ley de Demeter, y lo correcto sería cambiar
    el diseño del código para evitar esto. Pero bueno, de momento vale así.

--- app/src/test/java/com/shootr/android/util/DatabaseVersionUtilsTest.java
+++ app/src/test/java/com/shootr/android/util/DatabaseVersionUtilsTest.java
@@ -1,6 +1,7 @@
 package com.shootr.android.util;
 
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import com.shootr.android.data.prefs.IntPreference;
 import com.shootr.android.db.ShootrDbOpenHelper;
 import org.junit.Before;
@@ -8,6 +9,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
@@ -17,7 +19,7 @@ import static org.mockito.Mockito.when;
 
 public class DatabaseVersionUtilsTest {
 
-    public static final int NO_VERSION_STORED = 0;
+    public static final int NO_VERSION = 0;
     public static final int VERSION_1 = 1;
     private static final int VERSION_2 = 2;
 
@@ -36,7 +38,7 @@ public class DatabaseVersionUtilsTest {
 
     @Test
     public void shouldUpdateDatabaseVersionIfTheresNoVersion(){
-        when(preferencesDatabaseVersion.get()).thenReturn(NO_VERSION_STORED);
+        when(preferencesDatabaseVersion.get()).thenReturn(NO_VERSION);
         when(version.getDatabaseVersion()).thenReturn(VERSION_1);
 
         databaseVersionUtils.clearDataOnDatabaseVersionUpdated();
@@ -63,4 +65,24 @@ public class DatabaseVersionUtilsTest {
 
         verify(preferencesDatabaseVersion, never()).set(anyInt());
     }
+
+    @Test public void shouldClearPreferencesWhenVersionIsNewer() throws Exception {
+        when(preferencesDatabaseVersion.get()).thenReturn(VERSION_1);
+        when(version.getDatabaseVersion()).thenReturn(VERSION_2);
+
+        databaseVersionUtils.clearDataOnDatabaseVersionUpdated();
+
+        verify(sharedPreferences.edit().clear()).apply();
+    }
+
+    @Test public void shouldClearDatabaseWhenVersionIsNewer() throws Exception {
+        when(preferencesDatabaseVersion.get()).thenReturn(VERSION_1);
+        when(version.getDatabaseVersion()).thenReturn(VERSION_2);
+
+        databaseVersionUtils.clearDataOnDatabaseVersionUpdated();
+
+        verify(shootrDbOpenHelper).onUpgrade(any(SQLiteDatabase.class), anyInt(), anyInt());
+    }
+
+
 }
