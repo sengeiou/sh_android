8cf487c27047a75647a8e3db1f1c05b25150afa4
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-05-21 11:25:14 +0200

    Fix: Si la duración de vídeo es nula, el mapeo es nulo

--- app/src/main/java/com/shootr/android/ui/model/mappers/ShotModelMapper.java
+++ app/src/main/java/com/shootr/android/ui/model/mappers/ShotModelMapper.java
@@ -40,6 +40,9 @@ public class ShotModelMapper {
     }
 
     private String durationToText(Long durationInSeconds) {
+        if (durationInSeconds == null) {
+            return null;
+        }
         int minutes = (int) (durationInSeconds / 60);
         int seconds = (int) (durationInSeconds % 60);
         String secondsTwoDigits = seconds > 10 ? String.valueOf(seconds) : "0" + String.valueOf(seconds);
