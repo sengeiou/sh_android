0ceca73f242b98139e2e20c756184d35adb627c9
ajimenez <ajimenez@fav24.com>
2015-05-26 18:25:29 +0200

    MediaAdapter muestra video e imagen
    Es necesario aún permitir el OnClickListener del vídeo

--- app/src/main/java/com/shootr/android/ui/activities/MediaAdapter.java
+++ app/src/main/java/com/shootr/android/ui/activities/MediaAdapter.java
@@ -2,12 +2,17 @@ package com.shootr.android.ui.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import com.shootr.android.R;
+import com.shootr.android.ui.adapters.TimelineAdapter;
 import com.shootr.android.ui.model.ShotModel;
 import com.squareup.picasso.Picasso;
 import java.util.List;
@@ -27,15 +32,27 @@ public class MediaAdapter extends RecyclerView.Adapter {
         View layoutView = LayoutInflater
           .from(viewGroup.getContext())
           .inflate(R.layout.event_media_layout, null);
-        return new MediaItemHolder(layoutView);
+        return new MediaItemHolder(layoutView, new TimelineAdapter.VideoClickListener() {
+            @Override
+            public void onClick(String url) {
+                onVideoClick(url);
+            }
+        });
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
-        ShotModel shotModel = shotsWithMedia.get(position);
+        final ShotModel shotModel = shotsWithMedia.get(position);
         MediaItemHolder mediaItemHolder = (MediaItemHolder) viewHolder;
         mediaItemHolder.shotModel = shotModel;
         Picasso.with(context).load(shotModel.getImage()).into(mediaItemHolder.mediaImage);
+        mediaItemHolder.bindMedia(shotModel);
+    }
+
+    private void onVideoClick(String url) {
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        context.startActivity(intent);
     }
 
     @Override
@@ -48,9 +65,37 @@ public class MediaAdapter extends RecyclerView.Adapter {
         public ShotModel shotModel;
         public ImageView mediaImage;
 
-        public MediaItemHolder(View itemView) {
+        @InjectView(R.id.shot_video_frame) View videoFrame;
+        @InjectView(R.id.shot_video_duration) TextView videoDuration;
+        @InjectView(R.id.event_media_item_video) ImageView eventMediaItemVideo;
+
+        private TimelineAdapter.VideoClickListener videoClickListener;
+
+        public MediaItemHolder(View itemView, final TimelineAdapter.VideoClickListener videoClickListener) {
             super(itemView);
+            ButterKnife.inject(this, itemView);
+            this.videoClickListener = videoClickListener;
             mediaImage = (ImageView) itemView.findViewById(R.id.event_media_item);
+        }
+
+        public void bindMedia(final ShotModel shotModel){
+            this.shotModel = shotModel;
+            if(shotModel.hasVideo()){
+                this.videoFrame.setVisibility(View.VISIBLE);
+                this.videoDuration.setText(shotModel.getVideoDuration());
+                this.videoFrame.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Picasso.with(v.getContext()).load(shotModel.getImage()).into(eventMediaItemVideo);
+                        videoClickListener.onClick(shotModel.getVideoUrl());
+                    }
+                });
+            }else{
+                bindVideoOrPicture();
+            }
+        }
+
+        public void bindVideoOrPicture(){
             mediaImage.setOnClickListener(this);
         }
 
