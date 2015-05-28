729d980aa5b293491f8943f744cfe0911c7e941a
ajimenez <ajimenez@fav24.com>
2015-05-28 09:39:20 +0200

    Añadido el nuevo Gateway a las dependencias de Dagger

--- app/src/main/java/com/shootr/android/data/service/ServiceModule.java
+++ app/src/main/java/com/shootr/android/data/service/ServiceModule.java
@@ -3,6 +3,7 @@ package com.shootr.android.data.service;
 import com.shootr.android.domain.service.shot.ShotGateway;
 import com.shootr.android.domain.service.user.CheckinGateway;
 import com.shootr.android.domain.service.user.CreateAccountGateway;
+import com.shootr.android.domain.service.user.ResetPasswordGateway;
 import com.shootr.android.domain.service.user.LoginGateway;
 import dagger.Module;
 import dagger.Provides;
@@ -27,5 +28,10 @@ public class ServiceModule {
 
     @Provides ShotGateway provideShotGateway(SpecialserviceShotGateway specialserviceShotGateway) {
         return specialserviceShotGateway;
+    }
+
+    @Provides
+    ResetPasswordGateway provideForgotPasswordGateway(DataserviceResetPasswordGateway dataserviceForgotPasswordGateway){
+        return dataserviceForgotPasswordGateway;
     }
 }
