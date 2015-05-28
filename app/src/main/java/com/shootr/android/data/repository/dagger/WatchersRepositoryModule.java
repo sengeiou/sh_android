5716765e72e911f67fd03dbfbd3d403927f5979c
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-05-28 10:12:43 +0200

    Creada implementación local de WatchersRepository
    No es más que una copia de lo que se hace clandestinamente en los
    datasources de SearchEvent.
    
    Coge todo los usuarios que estén viendo algo y cuenta el total por
    evento. Luego, del mapa resultante, saca el count del evento pedido.

---
+++ app/src/main/java/com/shootr/android/data/repository/dagger/WatchersRepositoryModule.java
@@ -0,0 +1,22 @@
+package com.shootr.android.data.repository.dagger;
+
+import com.shootr.android.data.repository.local.LocalWatchersRepository;
+import com.shootr.android.domain.repository.Local;
+import com.shootr.android.domain.repository.WatchersRepository;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+
+@Module(
+  injects = {
+        LocalWatchersRepository.class
+  },
+  complete = false,
+  library = true)
+public class WatchersRepositoryModule {
+
+    @Provides @Local
+    WatchersRepository provideLocalWatchersRepository(LocalWatchersRepository watchersRepository) {
+        return watchersRepository;
+    }
+}
