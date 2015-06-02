c4f55fa58ae505846c6a93be8f038c34321b6e83
ajimenez <ajimenez@fav24.com>
2015-06-02 16:04:36 +0200

    Eliminado TimeUtils en WatchingRequestModelMapper
    No estaba utilzándose esta dependencia

--- app/src/main/java/com/shootr/android/ui/model/mappers/WatchingRequestModelMapper.java
+++ app/src/main/java/com/shootr/android/ui/model/mappers/WatchingRequestModelMapper.java
@@ -5,7 +5,6 @@ import com.shootr.android.R;
 import com.shootr.android.data.entity.EventEntity;
 import com.shootr.android.data.entity.UserEntity;
 import com.shootr.android.ui.model.WatchingRequestModel;
-import com.shootr.android.domain.utils.TimeUtils;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -15,22 +14,17 @@ import javax.inject.Singleton;
 public class WatchingRequestModelMapper {
 
     private final String subtitleNoneWatching;
-    private final String subtitleNoneWatchingAndNotStarted;
     private final String subtitleOneWatching;
     private final String subtitleTwoWatching;
     private final String subtitleMoreWatching;
 
-    private final TimeUtils timeUtils;
-
     private final String title;
 
-    @Inject public WatchingRequestModelMapper(Resources resources, TimeUtils timeUtils) {
-        this.timeUtils = timeUtils;
+    @Inject public WatchingRequestModelMapper(Resources resources) {
         subtitleNoneWatching = resources.getString(R.string.watching_request_subtitle_none);
         subtitleOneWatching = resources.getString(R.string.watching_request_subtitle_one);
         subtitleTwoWatching = resources.getString(R.string.watching_request_subtitle_two);
         subtitleMoreWatching = resources.getString(R.string.watching_request_subtitle_more);
-        subtitleNoneWatchingAndNotStarted = resources.getString(R.string.watching_request_subtitle_none_and_not_started);
         title = resources.getString(R.string.watching_event_question);
     }
 
@@ -39,19 +33,15 @@ public class WatchingRequestModelMapper {
         watchingRequestModel.setEventId(eventEntity.getIdEvent().toString());
         watchingRequestModel.setTitle(getTitle(eventEntity));
         watchingRequestModel.setSubtitle(getSubtitle(userEntities, eventEntity));
-        watchingRequestModel.setEventDate(eventEntity.getBeginDate().getTime());
         return watchingRequestModel;
     }
 
     private String getSubtitle(List<UserEntity> userEntities, EventEntity event) {
         List<String> namesFromUserEntities = getNamesFromUserEntities(userEntities);
         int usersWatchingNumber = namesFromUserEntities.size();
-        boolean hasStarted = event.getBeginDate().before(timeUtils.getCurrentDate());
-        if (usersWatchingNumber == 0 && hasStarted) {
+        if (usersWatchingNumber == 0) {
             return subtitleNoneWatching;
-        }else if(usersWatchingNumber == 0 && !hasStarted){
-            return subtitleNoneWatchingAndNotStarted;
-        }else if (usersWatchingNumber == 1) {
+        } else if (usersWatchingNumber == 1) {
             return String.format(subtitleOneWatching, namesFromUserEntities.get(0));
         } else if (usersWatchingNumber == 2) {
             return String.format(subtitleTwoWatching, namesFromUserEntities.get(0), namesFromUserEntities.get(1));
