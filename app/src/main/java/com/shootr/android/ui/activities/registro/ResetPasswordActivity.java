a481a847b2cbbbdd9edf468e6694bccf59654491
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-06-08 16:04:35 +0200

    Implementación de Vista de ResetPassword
    Primera iteración. Commit tocho.
    Esta pantalla se ha planteado de la siguiente forma:
    La operación de resetear la contraseña consta de dos pantallas, una
    para introducir el username o correo, y otra donde se muestra info
    básica del usuario en la que confirmar el envío del email de reset.
    Por tanto se descompone en dos presenters con sus dos vistas.
    
    La implementación son dos vistas en una misma Activity. Ésta contiene
    los dos presenters y las dos Vistas implementadas, conectadas al layout
    de la activity que también está separado en dos archivos incluídos en
    uno contenedor.
    
    En vez de tener dos activities que se navega de una a otra, la activity
    gestiona una falsa navegación intercambiando la visibilidad de las
    vistas. Los presenters ni se enteran de estos, se utilizan
    independientemente de si es la misma pantalla o distintas.
    
    No sé si es la mejor opción posible, me pareció correcta dado el lío de
    interacción con las vistas que veía cuando empecé a rehacer la pantalla.
    Ahora mismo tal como está montado quizá sería una opción mejor moverlo a
    dos Activities separadas, gestionando bien la navegación entre ellas.
    Creo que por la implementación de los Presenters y las Vistas no debe
    ser muy difícil.

--- app/src/main/java/com/shootr/android/ui/activities/registro/ResetPasswordActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/registro/ResetPasswordActivity.java
@@ -2,90 +2,219 @@ package com.shootr.android.ui.activities.registro;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.AutoCompleteTextView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import butterknife.OnTextChanged;
-import com.dd.CircularProgressButton;
 import com.shootr.android.R;
 import com.shootr.android.ui.ToolbarDecorator;
 import com.shootr.android.ui.activities.BaseToolbarDecoratedActivity;
-import com.shootr.android.ui.presenter.ResetPasswordPresenter;
-import com.shootr.android.ui.views.ResetPasswordView;
+import com.shootr.android.ui.model.ForgotPasswordUserModel;
+import com.shootr.android.ui.presenter.ResetPasswordConfirmationPresenter;
+import com.shootr.android.ui.presenter.ResetPasswordRequestPresenter;
+import com.shootr.android.ui.views.ResetPasswordConfirmationView;
+import com.shootr.android.ui.views.ResetPasswordRequestView;
+import com.shootr.android.util.PicassoWrapper;
 import javax.inject.Inject;
 
-public class ResetPasswordActivity extends BaseToolbarDecoratedActivity implements ResetPasswordView {
+public class ResetPasswordActivity extends BaseToolbarDecoratedActivity {
 
-    private static final int BUTTON_LOADING = 1;
-    private static final int BUTTON_NORMAL = 0;
-    private static final int BUTTON_ERROR = -1;
+    @InjectView(R.id.reset_password_request_view) View resetPasswordRequestLayout;
+    @InjectView(R.id.reset_password_confirmation_view) View resetPasswordConfirmationLayout;
 
-    @InjectView(R.id.reset_password_username_email) AutoCompleteTextView emailUsername;
-    @InjectView(R.id.reset_password_button) CircularProgressButton resetButton;
+    @Inject PicassoWrapper picasso;
+    @Inject ResetPasswordRequestPresenter resetPasswordRequestPresenter;
+    @Inject ResetPasswordConfirmationPresenter resetPasswordConfirmationPresenter;
 
-    @Inject ResetPasswordPresenter resetPasswordPresenter;
+    private ResetPasswordRequestView resetPasswordRequestView;
+    private ResetPasswordConfirmationView resetPasswordConfirmationView;
 
-    @Override protected int getLayoutResource() {
+    //region Initialization
+    @Override
+    protected int getLayoutResource() {
         return R.layout.activity_reset_password;
     }
 
-    @Override protected void initializeViews(Bundle savedInstanceState) {
+    @Override
+    protected void initializeViews(Bundle savedInstanceState) {
         ButterKnife.inject(this);
+        setupViewImplementations();
+        resetPasswordRequestLayout.setVisibility(View.VISIBLE);
+        resetPasswordConfirmationLayout.setVisibility(View.GONE);
     }
 
-    @Override protected void initializePresenter() {
-        resetPasswordPresenter.initialize(this);
+    private void setupViewImplementations() {
+        resetPasswordRequestView = new InnerResetPasswordRequestView();
+        ButterKnife.inject(resetPasswordRequestView, resetPasswordRequestLayout);
+
+        resetPasswordConfirmationView = new InnerResetPasswordConfirmationView();
+        ButterKnife.inject(resetPasswordConfirmationView, resetPasswordConfirmationLayout);
     }
 
-    @Override protected void setupToolbar(ToolbarDecorator toolbarDecorator) {
+    @Override
+    protected void initializePresenter() {
+        resetPasswordRequestPresenter.initialize(resetPasswordRequestView);
+    }
+
+    @Override
+    protected void setupToolbar(ToolbarDecorator toolbarDecorator) {
         /* no-op */
     }
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
+    @Override
+    protected boolean requiresUserLogin() {
+        return false;
+    }
+    //endregion
+
+    //region Activity lifecycle
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-
-    @Override protected boolean requiresUserLogin() {
-        return false;
-    }
+    //endregion
 
     @OnTextChanged(R.id.reset_password_username_email)
-    public void inputTextChanged() {
-        resetPasswordPresenter.inputTextChanged();
+    public void onInputTextChanged(CharSequence usernameOrEmail) {
+        resetPasswordRequestPresenter.onUsernameOrEmailChanged(usernameOrEmail.toString());
     }
 
-    @OnClick(R.id.reset_password_button)
-    public void onResetButtonClick(){
-        resetPasswordPresenter.attempReset();
+    @OnClick(R.id.reset_password_next)
+    public void onNextClicked() {
+        resetPasswordRequestPresenter.next();
     }
 
-    @Override public void disableResetButton() {
-        resetButton.setEnabled(false);
+    @OnClick(R.id.reset_password_confirm)
+    public void onConfirmClicked() {
+        resetPasswordConfirmationPresenter.confirm();
     }
 
-    @Override public void enableResetButton() {
-        resetButton.setEnabled(true);
+    @OnClick(R.id.reset_password_done)
+    public void onDoneClicked() {
+        resetPasswordConfirmationPresenter.done();
     }
 
-    @Override public String getUsernameOrEmail() {
-        return emailUsername.getText().toString();
+    private void transitionToConfirmationView(ForgotPasswordUserModel forgotPasswordUserModel) {
+        //TODO con animacion de transicion
+        resetPasswordRequestLayout.setVisibility(View.GONE);
+        resetPasswordConfirmationLayout.setVisibility(View.VISIBLE);
+        resetPasswordConfirmationPresenter.initialize(resetPasswordConfirmationView, forgotPasswordUserModel);
     }
 
-    @Override public void showLoading() {
-        resetButton.setIndeterminateProgressMode(true);
-        resetButton.setProgress(BUTTON_LOADING);
+    private void closeScreenAndLaunchLogin() {
+        finish();
+        //TODO open login
     }
 
-    @Override public void resetButtonToNormalStatus() {
-        resetButton.setProgress(BUTTON_NORMAL);
+    public class InnerResetPasswordRequestView implements ResetPasswordRequestView {
+
+        @InjectView(R.id.reset_password_username_email) EditText usernameOrEmailInput;
+        @InjectView(R.id.reset_password_next) View nextButton;
+        @InjectView(R.id.reset_password_progress) View progressView;
+        @InjectView(R.id.reset_password_error_message) TextView resetPasswordError;
+
+        @Override
+        public void enableNextButton() {
+            nextButton.setEnabled(true);
+        }
+
+        @Override
+        public void disableNextButton() {
+            nextButton.setEnabled(false);
+        }
+
+        @Override
+        public void navigateToResetPasswordConfirmation(ForgotPasswordUserModel forgotPasswordUserModel) {
+            transitionToConfirmationView(forgotPasswordUserModel);
+        }
+
+        @Override
+        public void showResetPasswordError() {
+            resetPasswordError.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public String getUsernameOrEmail() {
+            return usernameOrEmailInput.getText().toString();
+        }
+
+        @Override
+        public void showLoading() {
+            progressView.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void hideLoading() {
+            progressView.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void showError(String message) {
+            Toast.makeText(ResetPasswordActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 
-    @Override public void showError(String errorMessage) {
-        resetButton.setProgress(BUTTON_ERROR);
+    public class InnerResetPasswordConfirmationView implements ResetPasswordConfirmationView {
+
+        @InjectView(R.id.reset_password_avatar) ImageView avatar;
+        @InjectView(R.id.reset_password_username) TextView usernameText;
+        @InjectView(R.id.reset_password_email_confirmation_message) TextView confirmationMessage;
+        @InjectView(R.id.reset_password_confirm) View confirmButton;
+        @InjectView(R.id.reset_password_done) View doneButton;
+
+        @Override
+        public void showAvatar(String avatarUrl) {
+            picasso.loadProfilePhoto(avatarUrl).into(avatar);
+        }
+
+        @Override
+        public void showUsername(String username) {
+            usernameText.setText(username);
+        }
+
+        @Override
+        public void hideConfirmationButton() {
+            confirmButton.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void showDoneButton() {
+            doneButton.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void showPostConfirmationMessage(String email) {
+            confirmationMessage.setVisibility(View.VISIBLE);
+            confirmationMessage.setText(getString(R.string.reset_password_confirmation_message_email_pattern, email));
+        }
+
+        @Override
+        public void navigateToLogin() {
+            closeScreenAndLaunchLogin();
+        }
+
+        @Override
+        public void showLoading() {
+            //TODO
+        }
+
+        @Override
+        public void hideLoading() {
+            //TODO
+        }
+
+        @Override
+        public void showError(String message) {
+            //TODO
+        }
     }
 }
