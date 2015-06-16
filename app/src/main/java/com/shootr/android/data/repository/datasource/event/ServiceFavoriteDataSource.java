0cfbe7448f6749289a125a9725de0aba60c0b936
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-06-16 18:06:01 +0200

    Guarda en local los eventos de los favoritos
    Cuando se piden al servidor la lista de favoritos se extraen de ellos
    los eventos embebidos y se guardan en el datasource local.
    
    No estoy seguro de que sea "legal" que un datasource acceda directamente
    a otro, pero no hay otra.

--- app/src/main/java/com/shootr/android/data/repository/datasource/event/ServiceFavoriteDataSource.java
+++ app/src/main/java/com/shootr/android/data/repository/datasource/event/ServiceFavoriteDataSource.java
@@ -3,7 +3,9 @@ package com.shootr.android.data.repository.datasource.event;
 import com.shootr.android.data.api.entity.FavoriteApiEntity;
 import com.shootr.android.data.api.entity.mapper.FavoriteApiEntityMapper;
 import com.shootr.android.data.api.service.FavoriteApiService;
+import com.shootr.android.data.entity.EventEntity;
 import com.shootr.android.data.entity.FavoriteEntity;
+import com.shootr.android.domain.repository.Local;
 import com.shootr.android.domain.repository.SessionRepository;
 import java.util.List;
 import javax.inject.Inject;
@@ -13,14 +15,17 @@ public class ServiceFavoriteDataSource implements FavoriteDataSource {
     private final FavoriteApiService favoriteApiService;
     private final SessionRepository sessionRepository;
     private final FavoriteApiEntityMapper favoriteApiEntityMapper;
+    private final EventDataSource localEventDataSource;
 
     @Inject
     public ServiceFavoriteDataSource(FavoriteApiService favoriteApiService,
       SessionRepository sessionRepository,
-      FavoriteApiEntityMapper favoriteApiEntityMapper) {
+      FavoriteApiEntityMapper favoriteApiEntityMapper,
+      @Local EventDataSource localEventDataSource) {
         this.favoriteApiService = favoriteApiService;
         this.sessionRepository = sessionRepository;
         this.favoriteApiEntityMapper = favoriteApiEntityMapper;
+        this.localEventDataSource = localEventDataSource;
     }
 
     @Override
@@ -37,12 +42,20 @@ public class ServiceFavoriteDataSource implements FavoriteDataSource {
     @Override
     public List<FavoriteEntity> getFavorites() {
         List<FavoriteApiEntity> favorites = favoriteApiService.getFavorites(currentUserId());
+        storeEmbedEvents(favorites);
         return favoriteApiEntityMapper.transform(favorites);
     }
 
     @Override
     public List<FavoriteEntity> getEntitiesNotSynchronized() {
         throw new IllegalStateException("Method not available in Service");
+    }
+
+    private void storeEmbedEvents(List<FavoriteApiEntity> favorites) {
+        for (FavoriteApiEntity favorite : favorites) {
+            EventEntity event = favorite.getEvent();
+            localEventDataSource.putEvent(event);
+        }
     }
 
     private String currentUserId() {
