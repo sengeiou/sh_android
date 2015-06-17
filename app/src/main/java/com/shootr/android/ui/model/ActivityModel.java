c10be7b75b8b602a76c514fbf64463d046ae0f4c
ajimenez <ajimenez@fav24.com>
2015-06-17 18:23:00 +0200

    Implementado Modelo de Activity
    Se ha implementado la entidad Activity en la capa de presentación

---
+++ app/src/main/java/com/shootr/android/ui/model/ActivityModel.java
@@ -0,0 +1,97 @@
+package com.shootr.android.ui.model;
+
+import java.util.Date;
+
+public class ActivityModel {
+
+    private String idActivity;
+    private String idUser;
+    private String username;
+    private String userPhoto;
+    private String idEvent;
+    private String eventTitle;
+    private String eventTag;
+    private String comment;
+    private String type;
+    private Date publishDate;
+
+    public String getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(String userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+
+    public String getIdActivity() {
+        return idActivity;
+    }
+
+    public void setIdActivity(String idActivity) {
+        this.idActivity = idActivity;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getIdEvent() {
+        return idEvent;
+    }
+
+    public void setIdEvent(String idEvent) {
+        this.idEvent = idEvent;
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public String getEventTag() {
+        return eventTag;
+    }
+
+    public void setEventTag(String eventTag) {
+        this.eventTag = eventTag;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+}
