3799abd0cabca57af2406538700e30a14c18c279
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-06-22 16:25:34 +0200

    Eliminado select event de Favoritos y Listing
    Al igual que en la lista de eventos, en favoritos y listing no se tiene
    que hacer "select event" ya que se hace solo al entrar al timeline.

--- app/src/main/java/com/shootr/android/ui/presenter/FavoritesListPresenter.java
+++ app/src/main/java/com/shootr/android/ui/presenter/FavoritesListPresenter.java
@@ -3,8 +3,6 @@ package com.shootr.android.ui.presenter;
 import com.shootr.android.domain.EventSearchResult;
 import com.shootr.android.domain.interactor.Interactor;
 import com.shootr.android.domain.interactor.event.GetFavoriteEventsInteractor;
-import com.shootr.android.domain.interactor.event.SelectEventInteractor;
-import com.shootr.android.ui.model.EventModel;
 import com.shootr.android.ui.model.EventResultModel;
 import com.shootr.android.ui.model.mappers.EventModelMapper;
 import com.shootr.android.ui.model.mappers.EventResultModelMapper;
@@ -15,20 +13,15 @@ import javax.inject.Inject;
 public class FavoritesListPresenter implements Presenter{
 
     private final GetFavoriteEventsInteractor getFavoriteEventsInteractor;
-    private final SelectEventInteractor selectEventInteractor;
     private final EventResultModelMapper eventResultModelMapper;
-    private final EventModelMapper eventModelMapper;
 
     private FavoritesListView favoritesListView;
     private boolean hasBeenPaused = false;
 
     @Inject public FavoritesListPresenter(GetFavoriteEventsInteractor getFavoriteEventsInteractor,
-      SelectEventInteractor selectEventInteractor,
-      EventResultModelMapper eventResultModelMapper, EventModelMapper eventModelMapper) {
+      EventResultModelMapper eventResultModelMapper) {
         this.getFavoriteEventsInteractor = getFavoriteEventsInteractor;
-        this.selectEventInteractor = selectEventInteractor;
         this.eventResultModelMapper = eventResultModelMapper;
-        this.eventModelMapper = eventModelMapper;
     }
 
     public void setView(FavoritesListView favoritesListView) {
@@ -64,15 +57,7 @@ public class FavoritesListPresenter implements Presenter{
     }
 
     private void selectEvent(final String idEvent, String eventTitle) {
-        selectEventInteractor.selectEvent(idEvent, new Interactor.Callback<EventSearchResult>() {
-            @Override public void onLoaded(EventSearchResult selectedEvent) {
-                onEventSelected(eventModelMapper.transform(selectedEvent.getEvent()));
-            }
-        });
-    }
-
-    private void onEventSelected(EventModel selectedEvent) {
-        favoritesListView.navigateToEventTimeline(selectedEvent.getIdEvent(), selectedEvent.getTitle());
+        favoritesListView.navigateToEventTimeline(idEvent, eventTitle);
     }
 
     @Override
--- app/src/main/java/com/shootr/android/ui/presenter/ListingListPresenter.java
+++ app/src/main/java/com/shootr/android/ui/presenter/ListingListPresenter.java
@@ -3,8 +3,6 @@ package com.shootr.android.ui.presenter;
 import com.shootr.android.domain.EventSearchResult;
 import com.shootr.android.domain.interactor.Interactor;
 import com.shootr.android.domain.interactor.event.GetUserListingEventsInteractor;
-import com.shootr.android.domain.interactor.event.SelectEventInteractor;
-import com.shootr.android.ui.model.EventModel;
 import com.shootr.android.ui.model.EventResultModel;
 import com.shootr.android.ui.model.mappers.EventModelMapper;
 import com.shootr.android.ui.model.mappers.EventResultModelMapper;
@@ -14,21 +12,16 @@ import javax.inject.Inject;
 
 public class ListingListPresenter implements Presenter{
 
-    private final SelectEventInteractor selectEventInteractor;
     private final GetUserListingEventsInteractor getUserListingEventsInteractor;
     private final EventResultModelMapper eventResultModelMapper;
-    private final EventModelMapper eventModelMapper;
 
     private ListingView listingView;
     private String profileIdUser;
 
-    @Inject public ListingListPresenter(SelectEventInteractor selectEventInteractor,
-      GetUserListingEventsInteractor getUserListingEventsInteractor, EventResultModelMapper eventResultModelMapper,
-      EventModelMapper eventModelMapper) {
-        this.selectEventInteractor = selectEventInteractor;
+    @Inject public ListingListPresenter(GetUserListingEventsInteractor getUserListingEventsInteractor,
+      EventResultModelMapper eventResultModelMapper) {
         this.getUserListingEventsInteractor = getUserListingEventsInteractor;
         this.eventResultModelMapper = eventResultModelMapper;
-        this.eventModelMapper = eventModelMapper;
     }
 
     public void setView(ListingView listingView) {
@@ -78,15 +71,6 @@ public class ListingListPresenter implements Presenter{
     }
 
     private void selectEvent(final String idEvent, String eventTitle) {
-        selectEventInteractor.selectEvent(idEvent, new Interactor.Callback<EventSearchResult>() {
-            @Override public void onLoaded(EventSearchResult selectedEvent) {
-                onEventSelected(eventModelMapper.transform(selectedEvent.getEvent()));
-            }
-        });
+        listingView.navigateToEventTimeline(idEvent, eventTitle);
     }
-
-    private void onEventSelected(EventModel selectedEvent) {
-        listingView.navigateToEventTimeline(selectedEvent.getIdEvent(), selectedEvent.getTitle());
-    }
-
 }
