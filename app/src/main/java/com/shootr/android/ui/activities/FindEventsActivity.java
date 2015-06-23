2161e4868e0496b5aa734372790ac8e213cd49f3
ajimenez <ajimenez@fav24.com>
2015-06-23 12:44:15 +0200

    Implementado métodos de la vista
    Esta es la otra parte del commit trampa. Los métodos necesarios para
    hacer la búsqueda se han migrado de EventListFragment a
    FindEventsActivity

--- app/src/main/java/com/shootr/android/ui/activities/FindEventsActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/FindEventsActivity.java
@@ -3,22 +3,43 @@ package com.shootr.android.ui.activities;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import com.shootr.android.R;
 import com.shootr.android.ShootrApplication;
+import com.shootr.android.ui.adapters.EventsListAdapter;
+import com.shootr.android.ui.adapters.listeners.OnEventClickListener;
+import com.shootr.android.ui.adapters.recyclerview.FadeDelayedItemAnimator;
 import com.shootr.android.ui.base.BaseSignedInActivity;
+import com.shootr.android.ui.model.EventResultModel;
+import com.shootr.android.ui.presenter.FindEventsPresenter;
 import com.shootr.android.ui.views.FindEventsView;
+import com.shootr.android.util.PicassoWrapper;
 import dagger.ObjectGraph;
+import java.util.List;
+import javax.inject.Inject;
 
 public class FindEventsActivity extends BaseSignedInActivity implements FindEventsView {
 
     private SearchView searchView;
     private String currentSearchQuery;
     private ObjectGraph objectGraph;
+    private EventsListAdapter adapter;
+
+    @InjectView(R.id.find_events_list) RecyclerView eventsList;
+    @InjectView(R.id.find_events_empty) View emptyView;
+    @InjectView(R.id.find_events_loading) View loadingView;
+
+    @Inject FindEventsPresenter findEventsPresenter;
+    @Inject PicassoWrapper picasso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +48,20 @@ public class FindEventsActivity extends BaseSignedInActivity implements FindEven
             return;
         }
         setContainerContent(R.layout.activity_find_events);
+
         ButterKnife.inject(this);
+        eventsList.setLayoutManager(new LinearLayoutManager(this));
+        eventsList.setItemAnimator(new FadeDelayedItemAnimator(50));
 
         setupViews();
         setupActionBar();
+        initializePresenter();
 
         objectGraph = ShootrApplication.get(getApplicationContext()).getObjectGraph();
+    }
+
+    private void initializePresenter() {
+        findEventsPresenter.initialize(this);
     }
 
     @Override public boolean onOptionsItemSelected(MenuItem item) {
@@ -63,7 +92,13 @@ public class FindEventsActivity extends BaseSignedInActivity implements FindEven
     }
 
     private void setupViews() {
-        //TODO here I will have to setup an adapter
+        adapter = new EventsListAdapter(picasso, new OnEventClickListener() {
+            @Override
+            public void onEventClick(EventResultModel event) {
+                findEventsPresenter.selectEvent(event);
+            }
+        });
+        eventsList.setAdapter(adapter);
     }
 
     private void setupQuery() {
@@ -101,9 +136,51 @@ public class FindEventsActivity extends BaseSignedInActivity implements FindEven
         searchAutoComplete.setHintTextColor(getResources().getColor(R.color.hint_black));
     }
 
-    private void hideKeyboard() {
+    //region View methods
+    @Override public void hideContent() {
+        eventsList.setVisibility(View.GONE);
+    }
+
+    @Override public void hideKeyboard() {
+        // TODO full implementation of this method
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+        //imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+    }
+
+    @Override public void showLoading() {
+        loadingView.setVisibility(View.VISIBLE);
+    }
+
+    @Override public void hideLoading() {
+        loadingView.setVisibility(View.GONE);
+    }
+
+    @Override public void setCurrentWatchingEventId(EventResultModel currentVisibleEvent) {
+        adapter.setCurrentWatchingEvent(currentVisibleEvent);
+    }
+
+    @Override public void showEmpty() {
+        emptyView.setVisibility(View.VISIBLE);
+    }
+
+    @Override public void showContent() {
+        eventsList.setVisibility(View.VISIBLE);
+    }
+
+    @Override public void hideEmpty() {
+        emptyView.setVisibility(View.GONE);
+    }
+
+    @Override public void renderEvents(List<EventResultModel> eventModels) {
+        adapter.setEvents(eventModels);
+    }
+
+    @Override public void showError(String errorMessage) {
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override public void navigateToEventTimeline(String idEvent, String eventTitle) {
+        startActivity(EventTimelineActivity.newIntent(this , idEvent, eventTitle));
     }
 
     public void startSearch() {
@@ -111,8 +188,9 @@ public class FindEventsActivity extends BaseSignedInActivity implements FindEven
     }
 
     private void searchEvents() {
-        //TODO it will need a new presenter
+        findEventsPresenter.search(currentSearchQuery);
     }
 
+    //endregion
 
 }
