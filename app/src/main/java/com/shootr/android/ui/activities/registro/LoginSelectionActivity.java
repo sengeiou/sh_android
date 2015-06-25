95de7d92e31bfd50c5aef1db8594cb645edb511d
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-06-25 16:34:38 +0200

    Integrado sdk de facebook para Login
    Se añade la opción de Login con facebook en la pantalla de
    selección de login.
    
    En vez de usar el botón de Facebook que proporciona el SDK se implementa
    el login mediante un LoginManager usando un botón propio.
    
    En la respuesta se obtiene el AccessToken con algunos datos básicos como
    el nombre del usuario y su foto.
    
    A modo de demostración estos se muestran en un AlertDialog (la foto
    se carga donde el icono de la app).
    
    Todo este código se debe mover a un Presenter.

--- app/src/main/java/com/shootr/android/ui/activities/registro/LoginSelectionActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/registro/LoginSelectionActivity.java
@@ -2,12 +2,35 @@ package com.shootr.android.ui.activities.registro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.ImageView;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.shootr.android.R;
 import com.shootr.android.ui.base.BaseActivity;
+import com.shootr.android.util.PicassoWrapper;
+import hugo.weaving.DebugLog;
+import java.util.Arrays;
+import javax.inject.Inject;
+import timber.log.Timber;
 
 public class LoginSelectionActivity extends BaseActivity {
+
+    private CallbackManager callbackManager;
+    private LoginManager loginManager;
+
+    @InjectView(R.id.login_icon) ImageView loginIcon;
+
+    @Inject PicassoWrapper picasso;
 
     @Override
     protected int getLayoutResource() {
@@ -21,11 +44,47 @@ public class LoginSelectionActivity extends BaseActivity {
 
     @Override
     protected void initializePresenter() {
+        setupFacebook();
     }
 
     @Override
     protected boolean requiresUserLogin() {
         return false;
+    }
+
+    private void setupFacebook() {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        loginManager = LoginManager.getInstance();
+        loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            @DebugLog
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Timber.d("Facebook Access Token: %s", accessToken.getToken());
+                Profile profile = Profile.getCurrentProfile();
+                String name = profile.getName();
+                new AlertDialog.Builder(LoginSelectionActivity.this).setTitle("Hello " + name)
+                  .setMessage(String.format(
+                    "Welcome to Shootr and stuff.\n\nYour name is %s and your access token is something like %s...",
+                    name,
+                    accessToken.getToken().substring(0, 20)))
+                  .setPositiveButton("Ok", null)
+                  .show();
+                picasso.load(profile.getProfilePictureUri(200, 200)).into(loginIcon);
+            }
+
+            @Override
+            public void onCancel() {
+                /* no-op */
+            }
+
+            @Override
+            @DebugLog
+            public void onError(FacebookException e) {
+                //TODO handle error
+            }
+        });
     }
 
     @OnClick(R.id.login_btn_login)
@@ -38,4 +97,14 @@ public class LoginSelectionActivity extends BaseActivity {
         startActivity(new Intent(this, EmailRegistrationActivity.class));
     }
 
+    @OnClick(R.id.login_btn_facebook)
+    public void loginWithFacebook() {
+        loginManager.logInWithReadPermissions(this, Arrays.asList("public_profile", "user_friends", "email"));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
