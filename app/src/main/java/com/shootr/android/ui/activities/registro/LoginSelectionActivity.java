5801bcab99245291bc50a67590e0f0581ff924cb
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-06-25 17:22:59 +0200

    Añadido email a los campos obtenidos de Facebook
    El email del usuario no viene en el Profile que proporciona el AccesToken,
    por lo que hay que pedirlo aparte en una llamda al Graph API.
    
    Éste devuelve un Json del cual hay que sacar el email del usuario.
    
    Esta implementación es sucia y viola la arquitectura Clean, MVP y la del
    barroco renacentista. Pero está escrito así porque el objetivo de la
    historia es puramente la integración con el SDK de facebook y obtener
    algunos datos del usuario necesarios. Pero no hay arquitectura diseñada
    porque aún no están claro los requisitos y el flujo que se usará en el
    login finalmente. Todo eso corresponde a otra historia.
    
    En ese momento habrá que deshacer esta implementación tan cruda y definir
    los Presenters, Interactors, Repositorios, envolver los datos de
    Facebook en dominio propio, etc.

--- app/src/main/java/com/shootr/android/ui/activities/registro/LoginSelectionActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/registro/LoginSelectionActivity.java
@@ -12,6 +12,8 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
@@ -21,6 +23,7 @@ import com.shootr.android.util.PicassoWrapper;
 import hugo.weaving.DebugLog;
 import java.util.Arrays;
 import javax.inject.Inject;
+import org.json.JSONObject;
 import timber.log.Timber;
 
 public class LoginSelectionActivity extends BaseActivity {
@@ -56,21 +59,35 @@ public class LoginSelectionActivity extends BaseActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         loginManager = LoginManager.getInstance();
+
+        //FIXME this violates the whole architecture. Right now it's for demo purposes only. Implement the data flow in a clean way when the actual funcionality required is defined.
         loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             @DebugLog
             public void onSuccess(LoginResult loginResult) {
-                AccessToken accessToken = loginResult.getAccessToken();
+                final AccessToken accessToken = loginResult.getAccessToken();
                 Timber.d("Facebook Access Token: %s", accessToken.getToken());
-                Profile profile = Profile.getCurrentProfile();
-                String name = profile.getName();
-                new AlertDialog.Builder(LoginSelectionActivity.this).setTitle("Hello " + name)
-                  .setMessage(String.format(
-                    "Welcome to Shootr and stuff.\n\nYour name is %s and your access token is something like %s...",
-                    name,
-                    accessToken.getToken().substring(0, 20)))
-                  .setPositiveButton("Ok", null)
-                  .show();
+
+                final Profile profile = Profile.getCurrentProfile();
+
+                GraphRequest graphRequest =
+                  GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                      @Override
+                      public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+                          Timber.d(jsonObject.toString());
+                          String name = profile.getName();
+                          String email = jsonObject.optString("email");
+                          new AlertDialog.Builder(LoginSelectionActivity.this).setTitle("Hello " + name)
+                            .setMessage(String.format(
+                              "Welcome to Shootr and stuff.\n\nYour name is %s, your email is %s and your access token is something like %s...",
+                              name,
+                              email,
+                              accessToken.getToken().substring(0, 20)))
+                            .setPositiveButton("Ok", null)
+                            .show();
+                      }
+                  });
+                graphRequest.executeAsync();
                 picasso.load(profile.getProfilePictureUri(200, 200)).into(loginIcon);
             }
 
