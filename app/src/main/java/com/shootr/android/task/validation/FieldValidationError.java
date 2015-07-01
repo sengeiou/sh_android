1f6f75ba5955465c4f5c40738c779dc29bd39b82
Arturo <arturo.jimenez@fav24.com>
2015-07-01 09:25:23 +0200

    Eliminados atributos no usados en FieldValidationError

--- app/src/main/java/com/shootr/android/task/validation/FieldValidationError.java
+++ app/src/main/java/com/shootr/android/task/validation/FieldValidationError.java
@@ -6,8 +6,6 @@ public class FieldValidationError {
     public static final int FIELD_NAME = 2;
     public static final int FIELD_WEBSITE = 3;
     public static final int FIELD_BIO = 4;
-    public static final int FIELD_COMMENT = 5;
-    public static final int FIELD_IMAGE = 6;
 
     private final String errorCode;
     private final int field;
