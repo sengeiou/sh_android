a1b57d5b1677d20179a592ac52bbeb105100711d
Arturo <arturo.jimenez@fav24.com>
2015-07-02 11:05:05 +0200

    Descargar nueva versión desde Update Warning
    Cuando se hace click en el botón se descarga la nueva versión.
    Como se trata de un link estático se ha metido en resources.

--- app/src/main/java/com/shootr/android/ui/activities/UpdateWarningActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/UpdateWarningActivity.java
@@ -2,12 +2,19 @@ package com.shootr.android.ui.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import com.shootr.android.R;
 import com.shootr.android.ui.base.BaseNoToolbarActivity;
 
 public class UpdateWarningActivity extends BaseNoToolbarActivity {
+
+    @InjectView(R.id.update_warning_button) TextView updateButton;
 
     public static Intent newIntent(@NonNull Context context) {
         return new Intent(context, UpdateWarningActivity.class);
@@ -16,5 +23,17 @@ public class UpdateWarningActivity extends BaseNoToolbarActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContainerContent(R.layout.activity_update_warning);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.update_warning_button)
+    public void onUpdateClick() {
+        goToUrl(getString(R.string.update_shootr_version_url));
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
