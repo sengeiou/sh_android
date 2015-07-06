b1716b7456415a46e2f926b3921f6b8af3213457
Arturo <arturo.jimenez@fav24.com>
2015-07-06 11:40:24 +0200

    Implementada la llamada de Reset Password
    Se ha hecho exactamente igual que la otra llamada anterior, creando una
    entidad a parte para pasarla como objeto json en el body.

--- app/src/main/java/com/shootr/android/data/api/service/ResetPasswordApiService.java
+++ app/src/main/java/com/shootr/android/data/api/service/ResetPasswordApiService.java
@@ -2,6 +2,9 @@ package com.shootr.android.data.api.service;
 
 import com.shootr.android.data.entity.ForgotPasswordEntity;
 import com.shootr.android.data.entity.ForgotPasswordResultEntity;
+import com.shootr.android.data.entity.ResetEntity;
+import java.io.IOException;
+import retrofit.client.Response;
 import retrofit.http.Body;
 import retrofit.http.POST;
 
@@ -9,6 +12,7 @@ public interface ResetPasswordApiService {
 
     @POST("/auth/forgotPassword") ForgotPasswordResultEntity passwordReset(@Body ForgotPasswordEntity forgotPasswordEntity);
 
-    
+    @POST("/auth/resetPassword") Response sendResetPasswordEmail(@Body ResetEntity resetEntity) throws IOException;
+
 
 }
---
+++ app/src/main/java/com/shootr/android/data/entity/ResetEntity.java
@@ -0,0 +1,14 @@
+package com.shootr.android.data.entity;
+
+public class ResetEntity {
+
+    private String idUser;
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+}
