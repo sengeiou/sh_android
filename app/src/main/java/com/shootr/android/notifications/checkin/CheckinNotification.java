25fab2c090dfa5c95d260efb5c8770c89563aadd
Arturo <arturo.jimenez@fav24.com>
2015-07-07 10:49:56 +0200

    Añadidos métodos de intent a CheckinNotification
    La app no se estaba abriendo desde la notificación, se ha seguido el mismo
    esquema que en AbstractShotNotification.

--- app/src/main/java/com/shootr/android/notifications/checkin/CheckinNotification.java
+++ app/src/main/java/com/shootr/android/notifications/checkin/CheckinNotification.java
@@ -1,17 +1,23 @@
 package com.shootr.android.notifications.checkin;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import com.shootr.android.R;
 import com.shootr.android.notifications.CommonNotification;
 import com.shootr.android.notifications.NotificationBuilderFactory;
+import com.shootr.android.notifications.gcm.NotificationIntentReceiver;
 import com.shootr.android.ui.model.ActivityModel;
 import com.shootr.android.util.PicassoWrapper;
 import java.io.IOException;
 
 public class CheckinNotification extends CommonNotification {
+
+    private static final int REQUEST_DELETE = 1;
+    private static final int REQUEST_OPEN = 2;
 
     private final PicassoWrapper picasso;
     private final ActivityModel activity;
@@ -27,8 +33,20 @@ public class CheckinNotification extends CommonNotification {
 
     @Override
     public void setNotificationValues(NotificationCompat.Builder builder) {
+        builder.setContentIntent(getOpenCheckinNotificationPendingIntent());
+        builder.setDeleteIntent(getDiscardCheckinNotificationPendingIntent());
         builder.setContentTitle(activity.getUsername());
         builder.setContentText(activity.getComment());
+    }
+
+    protected PendingIntent getOpenCheckinNotificationPendingIntent() {
+        return PendingIntent.getBroadcast(getContext(), REQUEST_OPEN,
+          new Intent(NotificationIntentReceiver.ACTION_OPEN_SHOT_NOTIFICATION), PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
+    protected PendingIntent getDiscardCheckinNotificationPendingIntent() {
+        return PendingIntent.getBroadcast(getContext(), REQUEST_DELETE,
+          new Intent(NotificationIntentReceiver.ACTION_DISCARD_SHOT_NOTIFICATION), 0);
     }
 
     @Override
