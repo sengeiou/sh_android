98c49169ee1bee98776387f984a79e9e3de576c9
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-07-08 17:16:46 +0200

    Eliminado AvatarUrlProvider de donde no es necesario
    Se ha eliminado por completo GeneratedAvatarUrlProvider, la clase que
    crea la url del avatar a partir del id del usuario.
    
    La que saca la url del avatar de la base de datos se mantiene porque se
    sigue usando en tres sitios:
    - Forgot Password, que en otra rama ya se ha quitado de ahí.
    - Post new shot, que se envían por dataservice con los campos rellenos.
    - Media, que es el único sitio donde se siguen pidiendo shots por dataservice.
    
    De paso también se han borrado los métodos del mapper de ActivityEntity
    para dataservice, que ya no se usan ni se usarán. Los de Shot sí se
    siguen usando por los dos casos anteriores.

--- app/src/main/java/com/shootr/android/db/mappers/ActivityEntityMapper.java
+++ app/src/main/java/com/shootr/android/db/mappers/ActivityEntityMapper.java
@@ -11,11 +11,8 @@ import javax.inject.Inject;
 
 public class ActivityEntityMapper extends GenericMapper {
 
-    private  final UserAvatarUrlProvider avatarProvider;
-
     @Inject
-    public ActivityEntityMapper(UserAvatarUrlProvider avatarProvider) {
-        this.avatarProvider = avatarProvider;
+    public ActivityEntityMapper() {
     }
 
     public ActivityEntity fromCursor(Cursor c) {
@@ -47,36 +44,4 @@ public class ActivityEntityMapper extends GenericMapper {
         setSynchronizedtoContentValues(activity,cv);
         return cv;
     }
-
-    public ActivityEntity fromDto(Map<String, Object> dto) {
-        ActivityEntity activity = new ActivityEntity();
-        activity.setIdActivity((String) dto.get(DatabaseContract.ActivityTable.ID_ACTIVITY));
-        activity.setIdUser((String) dto.get(DatabaseContract.ActivityTable.ID_USER));
-        activity.setUsername((String) dto.get(DatabaseContract.ActivityTable.USERNAME));
-        activity.setUserPhoto((String) dto.get(DatabaseContract.ActivityTable.USER_PHOTO));
-        activity.setIdEvent((String) dto.get(DatabaseContract.ActivityTable.ID_EVENT));
-        activity.setEventTitle((String) dto.get(DatabaseContract.ActivityTable.EVENT_TITLE));
-        activity.setEventTag((String) dto.get(DatabaseContract.ActivityTable.EVENT_TAG));
-        activity.setComment((String) dto.get(DatabaseContract.ActivityTable.COMMENT));
-        activity.setType((String) dto.get(DatabaseContract.ActivityTable.TYPE));
-
-        setSynchronizedfromDto(dto, activity);
-        return activity;
-    }
-
-    public Map<String, Object> toDto(ActivityEntity activity) {
-        Map<String, Object> dto = new HashMap<>();
-        dto.put(DatabaseContract.ActivityTable.ID_ACTIVITY, activity == null ? null : activity.getIdActivity());
-        dto.put(DatabaseContract.ActivityTable.ID_USER, activity == null ? null : activity.getIdUser());
-        dto.put(DatabaseContract.ActivityTable.USERNAME, activity == null ? null : activity.getUsername());
-        dto.put(DatabaseContract.ActivityTable.USER_PHOTO, activity == null ? null : activity.getUserPhoto());
-        dto.put(DatabaseContract.ActivityTable.ID_EVENT, activity == null ? null : activity.getIdEvent());
-        dto.put(DatabaseContract.ActivityTable.EVENT_TITLE, activity == null ? null : activity.getEventTitle());
-        dto.put(DatabaseContract.ActivityTable.EVENT_TAG, activity == null ? null : activity.getEventTag());
-        dto.put(DatabaseContract.ActivityTable.COMMENT, activity == null ? null : activity.getComment());
-        dto.put(DatabaseContract.ActivityTable.TYPE, activity == null ? null : activity.getType());
-        setSynchronizedtoDto(activity,dto);
-        return dto;
-    }
-
 }
