b32f86d53b91d4f9e175164878f7d28862760e4f
Arturo <arturo.jimenez@fav24.com>
2015-07-15 13:21:44 +0200

    Creada UI básica de EmailConfirmationActivity
    Se han creado las vistas necesarias que han sido inyectadas en la
    activity

--- app/src/main/java/com/shootr/android/ui/activities/EmailConfirmationActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/EmailConfirmationActivity.java
@@ -4,10 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.shootr.android.R;
 import com.shootr.android.ui.ToolbarDecorator;
 
 public class EmailConfirmationActivity extends BaseToolbarDecoratedActivity {
+
+    @Bind(R.id.email_confirmation_email) TextView email;
+    @Bind(R.id.email_confirmation_confirm_button) View confirmButton;
+    @Bind(R.id.email_confirmation_confirm_progress) View progress;
 
     public static Intent newIntent(Context context, String email) {
         Intent intent = new Intent(context, EmailConfirmationActivity.class);
@@ -25,7 +33,7 @@ public class EmailConfirmationActivity extends BaseToolbarDecoratedActivity {
     }
 
     @Override protected void initializeViews(Bundle savedInstanceState) {
-        //ButterKnife.bind(this);
+        ButterKnife.bind(this);
     }
 
     @Override protected void initializePresenter() {
