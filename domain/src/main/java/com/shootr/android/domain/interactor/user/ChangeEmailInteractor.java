fbc7c8f6704f1c25c2d6d82adf3b3e6ead223707
Arturo <arturo.jimenez@fav24.com>
2015-07-16 10:27:07 +0200

    Creado interactor de change email
    Se ha creado el interactor de change email a falta de implementar la
    llamada a service.

---
+++ domain/src/main/java/com/shootr/android/domain/interactor/user/ChangeEmailInteractor.java
@@ -0,0 +1,59 @@
+package com.shootr.android.domain.interactor.user;
+
+import com.shootr.android.domain.exception.ServerCommunicationException;
+import com.shootr.android.domain.exception.ShootrException;
+import com.shootr.android.domain.executor.PostExecutionThread;
+import com.shootr.android.domain.interactor.Interactor;
+import com.shootr.android.domain.interactor.InteractorHandler;
+import com.shootr.android.domain.service.user.ShootrUserService;
+import javax.inject.Inject;
+
+public class ChangeEmailInteractor implements Interactor {
+
+    private final InteractorHandler interactorHandler;
+    private final PostExecutionThread postExecutionThread;
+    private final ShootrUserService shootrUserService;
+
+    private String email;
+    private CompletedCallback callback;
+    private ErrorCallback errorCallback;
+
+    @Inject public ChangeEmailInteractor(InteractorHandler interactorHandler, PostExecutionThread postExecutionThread,
+      ShootrUserService shootrUserService) {
+        this.interactorHandler = interactorHandler;
+        this.postExecutionThread = postExecutionThread;
+        this.shootrUserService = shootrUserService;
+    }
+
+    public void changeEmail(String email, CompletedCallback completedCallback, ErrorCallback errorCallback) {
+        this.email = email;
+        this.callback = completedCallback;
+        this.errorCallback = errorCallback;
+        interactorHandler.execute(this);
+    }
+
+    @Override public void execute() throws Exception {
+        try {
+            // TODO real stuff
+            notifyLoaded();
+        } catch (ServerCommunicationException error) {
+            notifyError(error);
+        }
+    }
+
+    private void notifyLoaded() {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                callback.onCompleted();
+            }
+        });
+    }
+
+    private void notifyError(final ShootrException error) {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                errorCallback.onError(error);
+            }
+        });
+    }
+}
