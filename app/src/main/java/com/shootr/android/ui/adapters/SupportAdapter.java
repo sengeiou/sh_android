42feebc166a4b84211f3c6d014033a040f0f9e1f
Arturo <arturo.jimenez@fav24.com>
2015-07-21 12:16:19 +0200

    Añadido Click Listener al Adapter
    En el adapter se configura cada uno de los campos que se muestran en
    el RecyclerView, añadiendole un click listener a Privacy Policy y a
    Terms of Service. Aún queda por hacer su implementación real.

--- app/src/main/java/com/shootr/android/ui/adapters/SupportAdapter.java
+++ app/src/main/java/com/shootr/android/ui/adapters/SupportAdapter.java
@@ -1,14 +1,19 @@
 package com.shootr.android.ui.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.shootr.android.R;
+import com.shootr.android.ui.activities.SupportActivity;
+import com.shootr.android.util.VersionUtils;
 import java.util.List;
 
 public class SupportAdapter extends RecyclerView.Adapter<SupportAdapter.ViewHolder> {
@@ -25,7 +30,7 @@ public class SupportAdapter extends RecyclerView.Adapter<SupportAdapter.ViewHold
         View layoutView = LayoutInflater
           .from(parent.getContext())
           .inflate(R.layout.support_text_layout, parent, false);
-        ViewHolder vh = new ViewHolder(layoutView);
+        ViewHolder vh = new ViewHolder(layoutView, context);
         return vh;
     }
 
@@ -42,17 +47,64 @@ public class SupportAdapter extends RecyclerView.Adapter<SupportAdapter.ViewHold
 
         public View view;
         public String text;
+        private Context context;
 
         @Bind(R.id.support_text) TextView textView;
+        @Bind(R.id.support_version_number) TextView versionNumber;
+        @Bind(R.id.profile_support_container) View supportContainer;
 
-        public ViewHolder(View v) {
+        public ViewHolder(View v, Context context) {
             super(v);
             ButterKnife.bind(this, itemView);
             this.view = v;
+            this.context = context;
         }
 
         public void bindText(){
             this.textView.setText(text);
+            switch (text) {
+                case SupportActivity.FAQ:
+                    setupFAQContainer();
+                    break;
+                case SupportActivity.PRIVACY_POLICY:
+                    setupPrivacyPolicyContainer();
+                    break;
+                case SupportActivity.TERMS_OF_SERVICE:
+                    setupTermsOfServiceContainer();
+                    break;
+                case SupportActivity.VERSION:
+                    setupVersionContainer();
+                    break;
+            }
+        }
+
+        private void setupVersionContainer() {
+            supportContainer.setClickable(false);
+            versionNumber.setVisibility(View.VISIBLE);
+            versionNumber.setText(String.valueOf(VersionUtils.getVersionName(context.getApplicationContext())));
+        }
+
+        private void setupTermsOfServiceContainer() {
+            supportContainer.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View view) {
+                    //TODO Real implementation
+                }
+            });
+        }
+
+        private void setupPrivacyPolicyContainer() {
+            supportContainer.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View view) {
+                    //TODO Real implementation
+                    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        private void setupFAQContainer() {
+            supportContainer.setClickable(false);
+            textView.setTextColor(context.getResources().getColor(R.color.gray_60));
         }
     }
+
 }
