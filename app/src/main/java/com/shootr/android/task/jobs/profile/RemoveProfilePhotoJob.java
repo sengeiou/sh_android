8b0442f613c792d00fd03b306a44113f798fea86
Sloy <rafa.vazsan@gmail.com>
2015-07-29 16:06:50 +0200

    Guardaba User sin campos de sincro tras borrar avatar
    Se mapeaba de Domain a Entity para modificar le current user y se guardaba ésta directamente sin campos de sincronización, y la base de datos lanzaba un error.
    Se ha cambiado para que el mapeo se haga a través de SyncableUserEntityFactory, que se encarga de reaprovechar la entidad ya existente.

--- app/src/main/java/com/shootr/android/task/jobs/profile/RemoveProfilePhotoJob.java
+++ app/src/main/java/com/shootr/android/task/jobs/profile/RemoveProfilePhotoJob.java
@@ -6,6 +6,7 @@ import com.path.android.jobqueue.network.NetworkUtil;
 import com.shootr.android.data.bus.Main;
 import com.shootr.android.data.entity.UserEntity;
 import com.shootr.android.data.mapper.UserEntityMapper;
+import com.shootr.android.data.repository.sync.SyncableUserEntityFactory;
 import com.shootr.android.db.manager.UserManager;
 import com.shootr.android.domain.User;
 import com.shootr.android.domain.repository.SessionRepository;
@@ -28,18 +29,18 @@ public class RemoveProfilePhotoJob extends ShootrBaseJob<UploadProfilePhotoStrea
     private final SessionRepository sessionRepository;
     private final UserEntityModelMapper userModelMapper;
     private final TimeUtils timeUtils;
-    private final UserEntityMapper userEntityMapper;
+    private final SyncableUserEntityFactory syncableUserEntityFactory;
 
     @Inject public RemoveProfilePhotoJob(Application application, @Main Bus bus, NetworkUtil networkUtil,
       ShootrService shootrService, UserManager userManager, SessionRepository sessionRepository, UserEntityModelMapper userModelMapper, TimeUtils timeUtils,
-      UserEntityMapper userEntityMapper) {
+      SyncableUserEntityFactory syncableUserEntityFactory) {
         super(new Params(PRIORITY), application, bus, networkUtil);
         this.shootrService = shootrService;
         this.userManager = userManager;
         this.sessionRepository = sessionRepository;
         this.userModelMapper = userModelMapper;
         this.timeUtils = timeUtils;
-        this.userEntityMapper = userEntityMapper;
+        this.syncableUserEntityFactory = syncableUserEntityFactory;
     }
 
     @Override protected void run() throws SQLException, IOException, JSONException {
@@ -50,7 +51,7 @@ public class RemoveProfilePhotoJob extends ShootrBaseJob<UploadProfilePhotoStrea
 
     private UserEntity setCurrentUserWithoutPhoto() throws IOException {
         User currentUser= sessionRepository.getCurrentUser();
-        UserEntity currentUserEntity = userEntityMapper.transform(currentUser);
+        UserEntity currentUserEntity = syncableUserEntityFactory.updatedOrNewEntity(currentUser);
         currentUserEntity.setPhoto(null);
         currentUserEntity.setModified(timeUtils.getCurrentDate());
         userManager.saveUser(currentUserEntity);
