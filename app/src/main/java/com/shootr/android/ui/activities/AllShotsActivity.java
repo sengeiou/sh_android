d17ae384b2aa51639fa6d8389ae07eddc927de17
Arturo <arturo.jimenez@fav24.com>
2015-08-05 11:26:32 +0200

    Implementado uso del presenter en AllShotsActivity
    Se inicializa en el caso de que se quiera hacer un report y se
    implementan los métodos de la vista que usa el presenter
    
    Conflicts:
    	app/src/main/java/com/shootr/android/ui/activities/AllShotsActivity.java

--- app/src/main/java/com/shootr/android/ui/activities/AllShotsActivity.java
+++ app/src/main/java/com/shootr/android/ui/activities/AllShotsActivity.java
@@ -1,12 +1,17 @@
 package com.shootr.android.ui.activities;
 
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 import butterknife.Bind;
@@ -18,7 +23,9 @@ import com.shootr.android.ui.adapters.TimelineAdapter;
 import com.shootr.android.ui.adapters.listeners.NiceShotListener;
 import com.shootr.android.ui.model.ShotModel;
 import com.shootr.android.ui.presenter.AllShotsPresenter;
+import com.shootr.android.ui.presenter.SessionUserPresenter;
 import com.shootr.android.ui.views.AllShotsView;
+import com.shootr.android.ui.views.SessionUserView;
 import com.shootr.android.ui.widgets.ListViewScrollObserver;
 import com.shootr.android.util.AndroidTimeUtils;
 import com.shootr.android.util.UsernameClickListener;
@@ -27,11 +34,16 @@ import javax.inject.Inject;
 
 import static com.shootr.android.domain.utils.Preconditions.checkNotNull;
 
-public class AllShotsActivity extends BaseToolbarDecoratedActivity implements AllShotsView {
+public class AllShotsActivity extends BaseToolbarDecoratedActivity implements AllShotsView, SessionUserView {
+
+    private static final String[] CONTEXT_MENU_OPTIONS = {"Copy", "Report"};
+    private static final Integer[] CONTEXT_MENU_OPTIONS_INDEX = {0, 1};
+    public static final String CLIPBOARD_LABEL = "clipboard_label";
 
     private static final String EXTRA_USER = "user";
 
     @Inject AllShotsPresenter presenter;
+    @Inject SessionUserPresenter sessionUserPresenter;
     @Inject AndroidTimeUtils timeUtils;
 
     @Bind(R.id.all_shots_list) ListView listView;
@@ -92,6 +104,33 @@ public class AllShotsActivity extends BaseToolbarDecoratedActivity implements Al
     protected void onPause() {
         super.onPause();
         presenter.pause();
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+      ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        for(int i = 0; i<CONTEXT_MENU_OPTIONS.length; i++) {
+            menu.add(0, CONTEXT_MENU_OPTIONS_INDEX[i], 0, CONTEXT_MENU_OPTIONS[i]);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        ContextMenu.ContextMenuInfo menuInfo = item.getMenuInfo();
+        if(menuInfo instanceof AdapterView.AdapterContextMenuInfo) {
+            AdapterView.AdapterContextMenuInfo adapterContextMenuInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            int position = adapterContextMenuInfo.position;
+            ShotModel shotModel = adapter.getItem(position);
+            int itemId = item.getItemId();
+            if(itemId == CONTEXT_MENU_OPTIONS_INDEX[0]) {
+                ClipboardManager clipboard = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText(CLIPBOARD_LABEL, shotModel.getComment());
+                clipboard.setPrimaryClip(clip);
+            } else if (itemId == CONTEXT_MENU_OPTIONS_INDEX[1]) {
+                sessionUserPresenter.initialize(this, shotModel);
+            }
+        }
+        return true;
     }
 
     @OnItemClick(R.id.all_shots_list)
@@ -181,6 +220,7 @@ public class AllShotsActivity extends BaseToolbarDecoratedActivity implements Al
             }
         };
         listView.setAdapter(adapter);
+        registerForContextMenu(listView);
     }
 
     public void openProfile(int position) {
@@ -255,5 +295,21 @@ public class AllShotsActivity extends BaseToolbarDecoratedActivity implements Al
 
     @Override public void addOldShots(List<ShotModel> shotModels) {
         adapter.addShotsBelow(shotModels);
+    }
+
+    @Override public void goToReport(String sessionToken, ShotModel shotModel) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://report.shootr.com/#/?token=" + sessionToken + "&idShot=" + shotModel.getIdShot()));
+        startActivity(browserIntent);
+    }
+
+    @Override public void showConfirmationMessage() {
+        AlertDialog.Builder builder =
+          new AlertDialog.Builder(this);
+
+        builder.setMessage(getString(R.string.alert_report_confirmed_email_message))
+          .setTitle(getString(R.string.alert_report_confirmed_email_title))
+          .setPositiveButton(getString(R.string.alert_report_confirmed_email_ok), null);
+
+        builder.create().show();
     }
 }
