7b5911c8ef5de286cc3147ba7389aa36c0f3f528
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-08-12 18:50:58 +0200

    Añadido Invite Friends al FriendsAdapter
    Se añade como otro tipo de vista más en la posición estática 0.
    
    De momento no hace nada en el onclick.

--- app/src/main/java/com/shootr/android/ui/adapters/recyclerview/FriendsAdapter.java
+++ app/src/main/java/com/shootr/android/ui/adapters/recyclerview/FriendsAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.shootr.android.R;
 import com.shootr.android.ui.adapters.UserListAdapter;
 import com.shootr.android.ui.adapters.listeners.OnUserClickListener;
 import com.shootr.android.ui.model.UserModel;
@@ -14,6 +15,7 @@ public class FriendsAdapter extends UserListAdapter {
 
     private static final int DEFAULT_TYPE = 0;
     private static final int SUGGESTED_PEOPLE_TYPE = 1;
+    private static final int INVITE_FIENDS_TYPE = 2;
 
     private final UserListAdapter suggestedPeopleAdapter;
 
@@ -43,7 +45,9 @@ public class FriendsAdapter extends UserListAdapter {
     }
 
     @Override public int getItemViewType(int position) {
-        if (isSuggestedPeopleView(position)) {
+        if (position == 0) {
+            return INVITE_FIENDS_TYPE;
+        }else if (isSuggestedPeopleView(position)) {
             return SUGGESTED_PEOPLE_TYPE;
         } else {
             return DEFAULT_TYPE;
@@ -51,38 +55,47 @@ public class FriendsAdapter extends UserListAdapter {
     }
 
     private boolean isSuggestedPeopleView(int position) {
-        return position == super.getCount();
+        return position == super.getCount()+1;
     }
 
     @Override public int getViewTypeCount() {
-        return 2;
+        return 3;
     }
 
     @Override public int getCount() {
-        return super.getCount()+1;
+        return super.getCount()+2;
     }
 
     @Override public View newView(LayoutInflater inflater, int position, ViewGroup container) {
-        if (!isSuggestedPeopleView(position)) {
-            return super.newView(inflater, position, container);
-        } else {
-            if (suggestedPeopleListView == null) {
-                suggestedPeopleListView = new SuggestedPeopleListView(container.getContext());
-                suggestedPeopleListView.setAdapter(suggestedPeopleAdapter);
-                suggestedPeopleListView.setOnUserClickListener(onUserClickListener);
-            }
-            return suggestedPeopleListView;
+        switch (getItemViewType(position)) {
+            case SUGGESTED_PEOPLE_TYPE:
+                if (suggestedPeopleListView == null) {
+                    suggestedPeopleListView = new SuggestedPeopleListView(container.getContext());
+                    suggestedPeopleListView.setAdapter(suggestedPeopleAdapter);
+                    suggestedPeopleListView.setOnUserClickListener(onUserClickListener);
+                }
+                return suggestedPeopleListView;
+            case INVITE_FIENDS_TYPE:
+                return createInviteFriendsView(inflater, container);
+            case DEFAULT_TYPE:
+            default:
+                return super.newView(inflater, position, container);
         }
     }
 
     @Override public void bindView(UserModel item, int position, View view) {
-        if (!isSuggestedPeopleView(position)) {
+        if (getItemViewType(position) == DEFAULT_TYPE) {
             super.bindView(item, position, view);
         }
     }
 
-    @Override public UserModel getItem(int position) {
-        return isSuggestedPeopleView(position) ? null : super.getItem(position);
+    private View createInviteFriendsView(LayoutInflater inflater, ViewGroup container) {
+        return inflater.inflate(R.layout.include_invite_friends, container, false);
+    }
+
+    @Override public UserModel getItem(int layoutPosition) {
+        int userPosition = layoutPosition - 1;
+        return layoutPosition == 0 || isSuggestedPeopleView(layoutPosition) ? null : super.getItem(userPosition);
     }
 
     @Override
