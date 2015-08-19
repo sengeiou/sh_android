40da64ece3eef8268e23423f907b71f305454cd1
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-08-19 09:14:08 +0200

    Cachea la carga de nice shots
    Cuando se hace el mapeo de shots se comprueba si están marcados como nice
    haciendo peticiones a este repositorio. Pero 1 petición a la BDD por cada
    shot supone demasiado overhead y el mapeo completo puede tardar más de
    medio segundo.
    
    Se cambia para que en vez leer cada vez de BDD se inicialice en memoria
    un set con todos los ids nices, y se lee de él. Se guarda como weak
    reference, aunque no vaya a consumir mucha memoria mejor así por si acaso
    y buenas prácticas.

--- app/src/main/java/com/shootr/android/data/repository/local/LocalNiceShotRepository.java
+++ app/src/main/java/com/shootr/android/data/repository/local/LocalNiceShotRepository.java
@@ -4,27 +4,47 @@ import com.shootr.android.db.manager.NiceManager;
 import com.shootr.android.domain.exception.NiceAlreadyMarkedException;
 import com.shootr.android.domain.exception.NiceNotMarkedException;
 import com.shootr.android.domain.repository.NiceShotRepository;
+import java.lang.ref.WeakReference;
+import java.util.Set;
 import javax.inject.Inject;
 
 public class LocalNiceShotRepository implements NiceShotRepository {
 
     private final NiceManager niceManager;
+    private WeakReference<Set<String>> markedShots;
 
-    @Inject public LocalNiceShotRepository(NiceManager niceManager) {
+    @Inject
+    public LocalNiceShotRepository(NiceManager niceManager) {
         this.niceManager = niceManager;
     }
 
-    @Override public void mark(String idShot) throws NiceAlreadyMarkedException {
+    @Override
+    public void mark(String idShot) throws NiceAlreadyMarkedException {
+        initMarkedShots();
+        getMarkedShots().add(idShot);
         niceManager.mark(idShot);
     }
 
     @Override
     public boolean isMarked(String idShot) {
-        return niceManager.isMarked(idShot);
+        initMarkedShots();
+        return getMarkedShots().contains(idShot);
+    }
+
+    private void initMarkedShots() {
+        if (markedShots == null || getMarkedShots() == null) {
+            markedShots = new WeakReference<>(niceManager.getAllMarked());
+        }
     }
 
     @Override
     public void unmark(String idShot) throws NiceNotMarkedException {
+        initMarkedShots();
+        getMarkedShots().remove(idShot);
         niceManager.unmark(idShot);
+    }
+
+    public Set<String> getMarkedShots() {
+        return markedShots.get();
     }
 }
