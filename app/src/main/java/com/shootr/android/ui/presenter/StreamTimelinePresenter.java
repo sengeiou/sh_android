e3de3d2c845530928f273da1b04855647fb868f4
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-08-19 12:11:58 +0200

    loadTimeline no espera a que se haga selectStream
    Hasta ahora como GetTimelineInteractor devolvía los shots del evento
    visible, dependía de que se hubiera terminado de seleccionar el stream.
    Ahora ya se piden los shots por id del stream, por lo que se puede empezar
    a pedir el timeline antes de hacer el select, con lo que se carga más rápido.
    
    También se evita que se cargue 2 veces a la vez desde resume. Ahí se incluyó
    para actualizar los nice al volver desde el detalle de un shot, por lo que
    se añade la condición de que antes se haya pausado.

--- app/src/main/java/com/shootr/android/ui/presenter/StreamTimelinePresenter.java
+++ app/src/main/java/com/shootr/android/ui/presenter/StreamTimelinePresenter.java
@@ -38,6 +38,7 @@ public class StreamTimelinePresenter implements Presenter, ShotSent.Receiver {
     private boolean isLoadingOlderShots;
     private boolean mightHaveMoreShots = true;
     private boolean isRefreshing = false;
+    private boolean hasBeenPaused = false;
 
     @Inject public StreamTimelinePresenter(StreamTimelineInteractorsWrapper timelineInteractorWrapper,
       SelectStreamInteractor selectStreamInteractor,
@@ -82,12 +83,13 @@ public class StreamTimelinePresenter implements Presenter, ShotSent.Receiver {
     }
 
     protected void selectStream() {
+        loadTimeline();
         selectStreamInteractor.selectStream(streamId, new Interactor.Callback<StreamSearchResult>() {
             @Override
             public void onLoaded(StreamSearchResult streamSearchResult) {
-                loadTimeline();
             }
         });
+
     }
 
     protected void loadTimeline() {
@@ -195,13 +197,16 @@ public class StreamTimelinePresenter implements Presenter, ShotSent.Receiver {
 
     @Override public void resume() {
         bus.register(this);
-        loadTimeline();
         startPollingShots();
+        if (hasBeenPaused) {
+            loadTimeline();
+        }
     }
 
     @Override public void pause() {
         bus.unregister(this);
         stopPollingShots();
+        hasBeenPaused = true;
     }
 
     @Subscribe
