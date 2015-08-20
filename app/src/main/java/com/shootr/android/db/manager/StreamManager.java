ab7e1f0bfb180bf9d7c26af973961dbbd54b174a
Arturo <arturo.jimenez@fav24.com>
2015-08-20 12:48:03 +0200

    Eliminado argumento removed en StreamManager
    Los valores constantes como "0" no se pasan como argumentos, estos están
     para variables y datos introducidos de fuera, por validar cosas como
     formato e inyección sql.

--- app/src/main/java/com/shootr/android/db/manager/StreamManager.java
+++ app/src/main/java/com/shootr/android/db/manager/StreamManager.java
@@ -183,8 +183,8 @@ public class StreamManager extends AbstractManager{
     }
 
     public Integer getListingNotRemoved(String idUser) {
-        String whereSelection = DatabaseContract.StreamTable.ID_USER + " = ? AND " + DatabaseContract.StreamTable.REMOVED + " = ?";
-        String[] whereArguments = new String[] { idUser, "0" };
+        String whereSelection = DatabaseContract.StreamTable.ID_USER + " = ? AND " + DatabaseContract.StreamTable.REMOVED + " = 0";
+        String[] whereArguments = new String[] { idUser };
 
         Cursor queryResult =
           getReadableDatabase().query(DatabaseContract.StreamTable.TABLE, DatabaseContract.StreamTable.PROJECTION, whereSelection, whereArguments, null, null, null);
@@ -196,8 +196,8 @@ public class StreamManager extends AbstractManager{
     }
 
     public List<StreamEntity> getStreamsListingNotRemoved(String idUser) {
-        String whereSelection = DatabaseContract.StreamTable.ID_USER + " = ? AND " + DatabaseContract.StreamTable.REMOVED + " = ?";
-        String[] whereArguments = new String[] { idUser, "0" };
+        String whereSelection = DatabaseContract.StreamTable.ID_USER + " = ? AND " + DatabaseContract.StreamTable.REMOVED + " = 0";
+        String[] whereArguments = new String[] { idUser };
 
         Cursor queryResult =
           getReadableDatabase().query(DatabaseContract.StreamTable.TABLE, DatabaseContract.StreamTable.PROJECTION, whereSelection, whereArguments, null, null,
