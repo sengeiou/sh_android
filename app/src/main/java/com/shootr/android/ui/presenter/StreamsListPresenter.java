94357e5ca99b9fa12d8ee74087d5a2f23290f44e
Arturo <arturo.jimenez@fav24.com>
2015-08-25 16:15:30 +0200

    Unwatch sin notificacion

--- app/src/main/java/com/shootr/android/ui/presenter/StreamsListPresenter.java
+++ app/src/main/java/com/shootr/android/ui/presenter/StreamsListPresenter.java
@@ -76,7 +76,6 @@ public class StreamsListPresenter implements Presenter {
             @Override public void onCompleted() {
                 loadDefaultStreamList();
                 removeCurrentWatchingStream();
-                streamsListView.showNotificationsOff();
             }
         });
     }
