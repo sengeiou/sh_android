eb526c2076cdeb32e6fc3a18abb7fcd670637662
Arturo <arturo.jimenez@fav24.com>
2015-08-26 12:26:31 +0200

    Se envía current user id desde el presenter hasta el holder
    Se propaga este id, guardandolo como un field en el adapter para que
    se mande al holder en el momento de hacer un render, así evalúan si
    el target user existe y si soy yo para poner "Started following you!"

--- app/src/main/java/com/shootr/android/ui/adapters/NiceShotViewHolder.java
+++ app/src/main/java/com/shootr/android/ui/adapters/NiceShotViewHolder.java
@@ -48,7 +48,7 @@ public class NiceShotViewHolder extends ActivityViewHolder {
     }
 
     @Override
-    public void render(ActivityModel activityModel) {
+    public void render(ActivityModel activityModel, String currentUserId) {
         ShotModel shotModel = checkNotNull(activityModel.getShot());
         shotViewHolder.niceButton.setVisibility(View.GONE);
         shotViewHolder.render(shotModel, false);
--- app/src/main/java/com/shootr/android/ui/views/ActivityTimelineView.java
+++ app/src/main/java/com/shootr/android/ui/views/ActivityTimelineView.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ActivityTimelineView extends LoadDataView {
 
-    void setActivities(List<ActivityModel> activities);
+    void setActivities(List<ActivityModel> activities, String currentUserId);
 
     void hideActivities();
 
