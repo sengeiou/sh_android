e57a86a646849727ac16116c354b32e8d1631772
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-09-07 13:05:03 +0200

    Oculta automáticamente la notificación de Shot sent
    A los 2 segundos oculta la notificación de Shot sent, como hace Twitter.

--- app/src/main/java/com/shootr/android/notifications/shotqueue/ShotQueueNotificationListener.java
+++ app/src/main/java/com/shootr/android/notifications/shotqueue/ShotQueueNotificationListener.java
@@ -1,5 +1,7 @@
 package com.shootr.android.notifications.shotqueue;
 
+import android.os.Handler;
+import android.os.Looper;
 import com.shootr.android.domain.QueuedShot;
 import com.shootr.android.domain.service.ShotQueueListener;
 import com.shootr.android.notifications.AndroidNotificationManager;
@@ -7,6 +9,8 @@ import javax.inject.Inject;
 import timber.log.Timber;
 
 public class ShotQueueNotificationListener implements ShotQueueListener {
+
+    private static final int SHOT_SENT_HIDE_DELAY_MILLIS = 2000;
 
     private final ShotQueueNotificationManager shotQueueNotificationManager;
     private final AndroidNotificationManager androidNotificationManager;
@@ -21,8 +25,14 @@ public class ShotQueueNotificationListener implements ShotQueueListener {
         /* no-op */
     }
 
-    @Override public void onShotSent(QueuedShot shot) {
+    @Override public void onShotSent(final QueuedShot shot) {
         shotQueueNotificationManager.showShotSentNotification(shot);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                shotQueueNotificationManager.hideShotNotification(shot);
+            }
+        }, SHOT_SENT_HIDE_DELAY_MILLIS);
     }
 
     @Override public void onShotFailed(QueuedShot shot, Exception e) {
