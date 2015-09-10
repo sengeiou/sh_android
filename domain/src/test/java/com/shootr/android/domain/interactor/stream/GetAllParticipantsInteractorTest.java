4e6c954218c209d42ba691b4ae5cc103d7ad6c0f
Arturo <arturo.jimenez@fav24.com>
2015-09-10 10:44:04 +0200

    Añadido test cuando se está paginando

--- domain/src/test/java/com/shootr/android/domain/interactor/stream/GetAllParticipantsInteractorTest.java
+++ domain/src/test/java/com/shootr/android/domain/interactor/stream/GetAllParticipantsInteractorTest.java
@@ -22,11 +22,12 @@ public class GetAllParticipantsInteractorTest {
 
     public static final String ID_STREAM = "idStream";
     public static final long MAX_JOIN_DATE = 0L;
-    public static final boolean IS_PAGINATING = false;
+    public static final boolean NOT_PAGINATING = false;
     public static final String ID_USER_1 = "idUser1";
     public static final String ID_USER_2 = "idUser2";
     public static final long ANOTHER_JOIN_DATE = 1L;
     public static final String ID_USER_3 = "idUser3";
+    public static final boolean IS_PAGINATING = true;
     @Mock UserRepository remoteUserRepository;
     @Mock UserRepository localUserRepository;
     @Mock Interactor.Callback<List<User>> callback;
@@ -43,30 +44,40 @@ public class GetAllParticipantsInteractorTest {
     }
 
     @Test
-    public void shouldLoadLocalStreamStatus() {
-        when(remoteUserRepository.getPeople()).thenReturn(people());
-        when(remoteUserRepository.getAllParticipants(ID_STREAM, MAX_JOIN_DATE)).thenReturn(participants());
+    public void shouldCallbackUserFollowedAndNotFollowedInStreamWhenNotPaginating() {
+        when(remoteUserRepository.getPeople()).thenReturn(myFollowings());
+        when(remoteUserRepository.getAllParticipants(ID_STREAM, MAX_JOIN_DATE)).thenReturn(allParticipantsInStream());
+
+        interactor.obtainAllParticipants(ID_STREAM, MAX_JOIN_DATE, NOT_PAGINATING, callback, errorCallback);
+
+        verify(callback).onLoaded(participantsAndMyFollowingsInStream());
+    }
+
+    @Test
+    public void shouldCallbackUserFollowedAndNotFollowedInStreamWhenPaginating() {
+        when(remoteUserRepository.getPeople()).thenReturn(myFollowings());
+        when(remoteUserRepository.getAllParticipants(ID_STREAM, MAX_JOIN_DATE)).thenReturn(allParticipantsInStream());
 
         interactor.obtainAllParticipants(ID_STREAM, MAX_JOIN_DATE, IS_PAGINATING, callback, errorCallback);
 
-        verify(callback).onLoaded(loadedParticipants());
+        verify(callback).onLoaded(participantsAndMyFollowingsInStream());
     }
 
-    private List<User> loadedParticipants() {
+    private List<User> participantsAndMyFollowingsInStream() {
         List<User> people = new ArrayList<>();
         people.add(anotherUser());
         people.add(oneUser());
         return people;
     }
 
-    private List<User> participants() {
+    private List<User> allParticipantsInStream() {
         List<User> people = new ArrayList<>();
         people.add(oneUser());
         people.add(anotherUser());
         return people;
     }
 
-    private List<User> people() {
+    private List<User> myFollowings() {
         List<User> people = new ArrayList<>();
         people.add(oneUser());
         people.add(userNotInStream());
