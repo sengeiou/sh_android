53f5434397c7e97931fb2a0a4a0278674f548590
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-09-25 10:41:14 +0200

    Renombra métodos y variables de author por subtitle
    Ahora que ya no se usa el campo de subtítulo exclusivamente para el autor,
    se llama subtítulo para no engañar.

--- app/src/main/java/com/shootr/android/ui/adapters/StreamResultViewHolder.java
+++ app/src/main/java/com/shootr/android/ui/adapters/StreamResultViewHolder.java
@@ -33,7 +33,7 @@ public class StreamResultViewHolder extends RecyclerView.ViewHolder {
     @Bind(R.id.stream_watchers) TextView watchers;
     @Bind(R.id.separator) View separator;
     @Nullable @Bind(R.id.stream_remove) ImageView removeButton;
-    @Nullable @Bind(R.id.stream_author) TextView author;
+    @Nullable @Bind(R.id.stream_subtitle) TextView subtitle;
     @Nullable @Bind(R.id.stream_actions_container) View actionsContainer;
 
     @BindString(R.string.watching_stream_connected) String connected;
@@ -64,7 +64,7 @@ public class StreamResultViewHolder extends RecyclerView.ViewHolder {
     public void render(StreamResultModel streamResultModel, boolean showSeparator) {
         this.setClickListener(streamResultModel);
         title.setText(streamResultModel.getStreamModel().getTitle());
-        renderAuthor(streamResultModel.getStreamModel());
+        renderSubttile(streamResultModel.getStreamModel());
         int watchersCount = streamResultModel.getWatchers();
         if (watchersCount > 0 || showsWatchersText) {
             watchers.setVisibility(View.VISIBLE);
@@ -113,17 +113,17 @@ public class StreamResultViewHolder extends RecyclerView.ViewHolder {
         }
     }
 
-    protected void renderAuthor(StreamModel stream) {
-        if (author != null) {
+    protected void renderSubttile(StreamModel stream) {
+        if (subtitle != null) {
             if (isWatchingStateEnabled) {
-                author.setText(getAuthorWithConnected(stream));
+                subtitle.setText(getConnectedSubtitle(stream));
             } else {
-                author.setText(getAuthorWithDescription(stream));
+                subtitle.setText(getAuthorSubtitleWithDescription(stream));
             }
         }
     }
 
-    private CharSequence getAuthorWithConnected(StreamModel stream) {
+    private CharSequence getConnectedSubtitle(StreamModel stream) {
         return new Truss()
           .pushSpan(new TextAppearanceSpan(itemView.getContext(), R.style.InlineConnectedAppearance))
           .append(connected)
@@ -131,7 +131,7 @@ public class StreamResultViewHolder extends RecyclerView.ViewHolder {
           .build();
     }
 
-    private CharSequence getAuthorWithDescription(StreamModel stream) {
+    private CharSequence getAuthorSubtitleWithDescription(StreamModel stream) {
         if (stream.getDescription() == null) {
             return stream.getAuthorUsername();
         } else {
