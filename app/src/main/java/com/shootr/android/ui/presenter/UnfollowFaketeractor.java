b1fbd077983598f527cdcfffb42dba1d88e05342
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-09-29 15:45:19 +0200

    UnfollowFaketeractor envuelve al interactor de verdad
    Preparación para refactor

--- app/src/main/java/com/shootr/android/ui/presenter/UnfollowFaketeractor.java
+++ app/src/main/java/com/shootr/android/ui/presenter/UnfollowFaketeractor.java
@@ -1,80 +1,19 @@
 package com.shootr.android.ui.presenter;
 
-import android.util.Pair;
-import com.path.android.jobqueue.JobManager;
-import com.shootr.android.data.bus.Main;
-import com.shootr.android.domain.executor.PostExecutionThread;
-import com.shootr.android.domain.interactor.Fast;
 import com.shootr.android.domain.interactor.Interactor;
-import com.shootr.android.domain.interactor.InteractorHandler;
-import com.shootr.android.service.dataservice.dto.UserDtoFactory;
-import com.shootr.android.task.events.follows.FollowUnFollowResultEvent;
-import com.shootr.android.task.jobs.follows.GetFollowUnFollowUserOfflineJob;
-import com.shootr.android.task.jobs.follows.GetFollowUnfollowUserOnlineJob;
-import com.squareup.otto.Bus;
-import com.squareup.otto.Subscribe;
+import com.shootr.android.domain.interactor.user.UnfollowInteractor;
 import javax.inject.Inject;
-import javax.inject.Provider;
 
-public class UnfollowFaketeractor implements Interactor {
+public class UnfollowFaketeractor {
 
-    private final Bus bus;
-    private final PostExecutionThread postExecutionThread;
-    private final InteractorHandler interactorHandler;
-    private final Provider<GetFollowUnfollowUserOnlineJob> unfollowOnlineJobProvider;
-    private final Provider<GetFollowUnFollowUserOfflineJob> unfollowOfflineJobProvider;
-    private final JobManager jobManager;
+    private final UnfollowInteractor unfollowInteractor;
 
-    private String idUser;
-    private CompletedCallback callback;
-
-    @Inject public UnfollowFaketeractor(@Main Bus bus, PostExecutionThread postExecutionThread, @Fast InteractorHandler interactorHandler,
-      Provider<GetFollowUnfollowUserOnlineJob> unfollowOnlineJobProvider,
-      Provider<GetFollowUnFollowUserOfflineJob> unfollowOfflineJobProvider, JobManager jobManager) {
-        this.bus = bus;
-        this.postExecutionThread = postExecutionThread;
-        this.interactorHandler = interactorHandler;
-        this.unfollowOnlineJobProvider = unfollowOnlineJobProvider;
-        this.unfollowOfflineJobProvider = unfollowOfflineJobProvider;
-        this.jobManager = jobManager;
+    @Inject
+    public UnfollowFaketeractor(UnfollowInteractor unfollowInteractor) {
+        this.unfollowInteractor = unfollowInteractor;
     }
 
-    protected void unfollow(String idUser, CompletedCallback callback) {
-        bus.register(this);
-        this.idUser = idUser;
-        this.callback = callback;
-        interactorHandler.execute(this);
+    protected void unfollow(String idUser, Interactor.CompletedCallback callback) {
+        unfollowInteractor.unfollow(idUser, callback);
     }
-
-    @Override public void execute() throws Exception {
-        startUnfollowJob(idUser);
-    }
-
-    protected void startUnfollowJob(String idUser) {
-        GetFollowUnFollowUserOfflineJob offlineJob = unfollowOfflineJobProvider.get();
-        offlineJob.init(idUser, UserDtoFactory.UNFOLLOW_TYPE);
-        jobManager.addJobInBackground(offlineJob);
-
-        GetFollowUnfollowUserOnlineJob onlineJob = unfollowOnlineJobProvider.get();
-        jobManager.addJobInBackground(onlineJob);
-    }
-
-    @Subscribe
-    public void onFollowUnfollowResultReceived(FollowUnFollowResultEvent event) {
-        Pair<String, Boolean> result = event.getResult();
-        Boolean following = result.second;
-        if (!following) {
-            notifyUnfollow();
-        }
-    }
-
-    private void notifyUnfollow() {
-        postExecutionThread.post(new Runnable() {
-            @Override public void run() {
-                callback.onCompleted();
-                bus.unregister(this);
-            }
-        });
-    }
-
 }
