a8ae70120ae6c1976983afc46767389a2cc1d891
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-09-30 18:13:00 +0200

    Añade excepciones en la API de device
    Se debían declarar las excepciones en la cabecera del método de la api.

--- app/src/main/java/com/shootr/android/data/repository/remote/RemoteDeviceRepository.java
+++ app/src/main/java/com/shootr/android/data/repository/remote/RemoteDeviceRepository.java
@@ -1,10 +1,13 @@
 package com.shootr.android.data.repository.remote;
 
+import com.shootr.android.data.api.exception.ApiException;
 import com.shootr.android.data.api.service.DeviceApiService;
 import com.shootr.android.data.entity.DeviceEntity;
 import com.shootr.android.data.mapper.DeviceEntityMapper;
 import com.shootr.android.domain.Device;
+import com.shootr.android.domain.exception.ServerCommunicationException;
 import com.shootr.android.domain.repository.DeviceRepository;
+import java.io.IOException;
 import javax.inject.Inject;
 
 public class RemoteDeviceRepository implements DeviceRepository {
@@ -26,7 +29,11 @@ public class RemoteDeviceRepository implements DeviceRepository {
 
     @Override
     public Device putDevice(Device device) {
-        DeviceEntity createdDevice = deviceApiService.createUpdateDevice(mapper.transform(device));
-        return mapper.transform(createdDevice);
+        try {
+            DeviceEntity createdDevice = deviceApiService.createUpdateDevice(mapper.transform(device));
+            return mapper.transform(createdDevice);
+        } catch (IOException | ApiException e) {
+            throw new ServerCommunicationException(e);
+        }
     }
 }
