ae5064dec440a1ede04e524ce4e40bdd69154f84
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-10-02 16:07:57 +0200

    Elimina molta duplicidad de código en ShotManager
    La lectura de shots se saca a un método común para no repetir la
    creación del cursor y mapeo de shots.

--- app/src/main/java/com/shootr/android/db/manager/ShotManager.java
+++ app/src/main/java/com/shootr/android/db/manager/ShotManager.java
@@ -4,6 +4,8 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import com.shootr.android.data.entity.ShotEntity;
 import com.shootr.android.db.DatabaseContract;
 import com.shootr.android.db.DatabaseContract.ShotTable;
@@ -14,127 +16,114 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 
-public class ShotManager extends  AbstractManager{
+public class ShotManager extends AbstractManager {
 
     @Inject ShotEntityDBMapper shotEntityMapper;
 
     private static final String SHOT_TABLE = ShotTable.TABLE;
 
     @Inject
-    public ShotManager(SQLiteOpenHelper openHelper, ShotEntityDBMapper shotEntityMapper){
+    public ShotManager(SQLiteOpenHelper openHelper, ShotEntityDBMapper shotEntityMapper) {
         super(openHelper);
         this.shotEntityMapper = shotEntityMapper;
     }
 
-    /**
-     * Insert a Shot
-     */
     public void saveShot(ShotEntity shot) throws SQLException {
-        ContentValues contentValues = shotEntityMapper.toContentValues(shot);
-        if (contentValues.getAsLong(DatabaseContract.SyncColumns.DELETED) != null) {
-            deleteShot(shot);
-        } else {
-            getWritableDatabase().insertWithOnConflict(SHOT_TABLE, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
-        }
-        insertInSync();
-    }
-
-    public List<ShotEntity> getShotsFromUser(String idUser, Integer latestShotsNumber) {
-        List<ShotEntity> latestShots = new ArrayList<>();
-        String whereSelection = ShotTable.ID_USER + " = ?";
-        String[] whereArguments = new String[]{idUser};
-
-        Cursor queryResult =
-          getReadableDatabase().query(ShotTable.TABLE,
-            ShotTable.PROJECTION,
-            whereSelection,
-            whereArguments,
-            null,
-            null,
-            ShotTable.BIRTH + " DESC",
-            String.valueOf(latestShotsNumber));
-
-        ShotEntity shotEntity;
-        if (queryResult.getCount() > 0) {
-            queryResult.moveToFirst();
-            do {
-                shotEntity = shotEntityMapper.fromCursor(queryResult);
-                latestShots.add(shotEntity);
-            } while (queryResult.moveToNext());
-        }
-        queryResult.close();
-        return latestShots;
-    }
-
-    public List<ShotEntity> getAllShotsFromUser(String idUser) {
-        List<ShotEntity> latestShots = new ArrayList<>();
-        String whereSelection = ShotTable.ID_USER + " = ?";
-        String[] whereArguments = new String[]{idUser};
-
-        Cursor queryResult =
-          getReadableDatabase().query(ShotTable.TABLE,
-            ShotTable.PROJECTION,
-            whereSelection,
-            whereArguments,
-            null,
-            null,
-            ShotTable.BIRTH + " DESC", null);
-
-        ShotEntity shotEntity;
-        if (queryResult.getCount() > 0) {
-            queryResult.moveToFirst();
-            do {
-                shotEntity = shotEntityMapper.fromCursor(queryResult);
-                latestShots.add(shotEntity);
-            } while (queryResult.moveToNext());
-        }
-        queryResult.close();
-        return latestShots;
+        insertShot(shot, getWritableDatabase());
     }
 
     public void saveShots(List<ShotEntity> shotList) {
         SQLiteDatabase database = getWritableDatabase();
-        try{
+        try {
             database.beginTransaction();
             for (ShotEntity shot : shotList) {
-                if (shot.getDeleted() != null) {
-                    deleteShot(shot.getIdShot());
-                } else {
-                    ContentValues contentValues = shotEntityMapper.toContentValues(shot);
-                    database.insertWithOnConflict(ShotTable.TABLE, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
-                }
+                insertShot(shot, database);
             }
             database.setTransactionSuccessful();
-        }finally {
+        } finally {
             database.endTransaction();
         }
     }
 
-    /**
-     * Delete a shot
-     */
-    public long deleteShot(ShotEntity shot) {
-        return deleteShot(shot.getIdShot());
+    public List<ShotEntity> getShotsFromUser(String idUser, Integer limit) {
+        String whereSelection = ShotTable.ID_USER + " = ?";
+        String[] whereArguments = new String[] { idUser };
+
+        return readShots(whereSelection, whereArguments, String.valueOf(limit));
     }
 
-    public void insertInSync(){
-        insertInTableSync(SHOT_TABLE, 3, 1000, 0);
+    public List<ShotEntity> getAllShotsFromUser(String idUser) {
+        String whereSelection = ShotTable.ID_USER + " = ?";
+        String[] whereArguments = new String[] { idUser };
+
+        return readShots(whereSelection, whereArguments);
     }
 
     public List<ShotEntity> getShotsByStreamParameters(StreamTimelineParameters parameters) {
         String streamSelection = ShotTable.ID_STREAM + " = ?";
         String typeSelection = ShotTable.TYPE + " = ?";
-        //TODO since & max
-        //TODO limit
 
         String[] whereArguments = new String[2];
         whereArguments[0] = String.valueOf(parameters.getStreamId());
         whereArguments[1] = String.valueOf(parameters.getShotType());
         String whereClause = streamSelection + " AND " + typeSelection;
 
-        Cursor queryResult =
-          getReadableDatabase().query(ShotTable.TABLE, ShotTable.PROJECTION, whereClause, whereArguments, null, null,
-            ShotTable.BIRTH +" DESC");
+        return readShots(whereClause, whereArguments);
+    }
+
+    public ShotEntity getShotById(String idShot) {
+        String whereClause = ShotTable.ID_SHOT + " = ?";
+        String[] whereArguments = new String[] { String.valueOf(idShot) };
+
+        return readShot(whereClause, whereArguments);
+    }
+
+    public List<ShotEntity> getRepliesTo(String shotParentId) {
+        String whereClause = ShotTable.ID_SHOT_PARENT + " = ?";
+        String[] whereArguments = new String[] { shotParentId };
+
+        return readShots(whereClause, whereArguments);
+    }
+
+    public List<ShotEntity> getStreamMediaShots(String idStream, List<String> idUsers) {
+        String usersSelection = ShotTable.ID_USER + " IN (" + createListPlaceholders(idUsers.size()) + ")";
+        String streamSelection = ShotTable.ID_STREAM + " = ?";
+        String imageSelection = ShotTable.IMAGE + " IS NOT NULL ";
+
+        String[] whereArguments = new String[idUsers.size() + 1];
+
+        for (int i = 0; i < idUsers.size(); i++) {
+            whereArguments[i] = String.valueOf(idUsers.get(i));
+        }
+
+        whereArguments[idUsers.size()] = String.valueOf(idStream);
+
+        String whereClause = usersSelection + " AND " + streamSelection + " AND " + imageSelection;
+
+        return readShots(whereClause, whereArguments);
+    }
+
+    public Long deleteShot(String idShot) {
+        String where = DatabaseContract.ShotTable.ID_SHOT + "=?";
+        String[] whereArgs = new String[] { idShot };
+        return (long) getWritableDatabase().delete(SHOT_TABLE, where, whereArgs);
+    }
+
+    @NonNull
+    private List<ShotEntity> readShots(String whereClause, String[] whereArguments) {
+        return readShots(whereClause, whereArguments, null);
+    }
+
+    @NonNull
+    private List<ShotEntity> readShots(String whereClause, String[] whereArguments, String limit) {
+        Cursor queryResult = getReadableDatabase().query(ShotTable.TABLE,
+          ShotTable.PROJECTION,
+          whereClause,
+          whereArguments,
+          null,
+          null,
+          ShotTable.BIRTH + " DESC",
+          limit);
 
         List<ShotEntity> resultShots = new ArrayList<>(queryResult.getCount());
         ShotEntity shotEntity;
@@ -149,10 +138,8 @@ public class ShotManager extends  AbstractManager{
         return resultShots;
     }
 
-    public ShotEntity getShotById(String idShot) {
-        String whereClause = ShotTable.ID_SHOT + " = ?";
-        String[] whereArguments = new String[]{String.valueOf(idShot)};
-
+    @Nullable
+    private ShotEntity readShot(String whereClause, String[] whereArguments) {
         Cursor queryResult = getReadableDatabase().query(ShotTable.TABLE,
           ShotTable.PROJECTION,
           whereClause,
@@ -170,73 +157,12 @@ public class ShotManager extends  AbstractManager{
         return shotEntity;
     }
 
-    public List<ShotEntity> getRepliesTo(String shotParentId) {
-        String whereClause = ShotTable.ID_SHOT_PARENT + " = ?";
-        String[] whereArguments = new String[]{shotParentId};
-
-        Cursor queryResult = getReadableDatabase().query(ShotTable.TABLE,
-          ShotTable.PROJECTION,
-          whereClause,
-          whereArguments,
-          null,
-          null,
-          null);
-
-        List<ShotEntity> resultShots = new ArrayList<>(queryResult.getCount());
-        ShotEntity shotEntity;
-        if (queryResult.getCount() > 0) {
-            queryResult.moveToFirst();
-            do {
-                shotEntity = shotEntityMapper.fromCursor(queryResult);
-                resultShots.add(shotEntity);
-            } while (queryResult.moveToNext());
+    private void insertShot(ShotEntity shot, SQLiteDatabase writableDatabase) {
+        ContentValues contentValues = shotEntityMapper.toContentValues(shot);
+        if (contentValues.getAsLong(DatabaseContract.SyncColumns.DELETED) != null) {
+            deleteShot(shot.getIdShot());
+        } else {
+            writableDatabase.insertWithOnConflict(SHOT_TABLE, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
         }
-        queryResult.close();
-        return resultShots;
-    }
-
-    public List<ShotEntity> getStreamMediaShots(String idStream, List<String> idUsers) {
-        String usersSelection = ShotTable.ID_USER + " IN (" + createListPlaceholders(idUsers.size()) + ")";
-        String streamSelection = ShotTable.ID_STREAM + " = ?";
-        String imageSelection = ShotTable.IMAGE + " IS NOT NULL ";
-
-        String[] whereArguments = new String[idUsers.size()+1];
-
-        for (int i = 0; i < idUsers.size(); i++) {
-            whereArguments[i] = String.valueOf(idUsers.get(i));
-        }
-
-        whereArguments[idUsers.size()] = String.valueOf(idStream);
-
-        String whereClause = usersSelection + " AND " + streamSelection + " AND " + imageSelection;
-
-        Cursor queryResult =
-          getReadableDatabase().query(ShotTable.TABLE, ShotTable.PROJECTION, whereClause, whereArguments, null, null,
-            ShotTable.BIRTH +" DESC");
-
-        List<ShotEntity> resultShots = new ArrayList<>(queryResult.getCount());
-        ShotEntity shotEntity;
-        if (queryResult.getCount() > 0) {
-            queryResult.moveToFirst();
-            do {
-                shotEntity = shotEntityMapper.fromCursor(queryResult);
-                resultShots.add(shotEntity);
-            } while (queryResult.moveToNext());
-        }
-        queryResult.close();
-
-        return resultShots;
-    }
-
-    public Long deleteShot(String idShot) {
-        long res = 0;
-        String args = DatabaseContract.ShotTable.ID_SHOT + "=?";
-        String[] stringArgs = new String[]{idShot};
-        Cursor c = getReadableDatabase().query(SHOT_TABLE, DatabaseContract.ShotTable.PROJECTION, args, stringArgs, null, null, null);
-        if (c.getCount() > 0) {
-            res = getWritableDatabase().delete(SHOT_TABLE, args, new String[]{idShot});
-        }
-        c.close();
-        return res;
     }
 }
