d395155040e2ee27887e2189d7085f4641cea1e7
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-10-05 12:02:09 +0200

    Simplifica UserEntityDBMapper
    Se elimina el método intermedio ya innecesario, que sólo ofuscaba más el código.
    Se eliminan comprobaciones de email y nulidad que no tienen mucho sentido ya.
    Si un campo viene nulo se guarda nulo, y si está nulo se devuelve nulo.
    
    También se quita el if defensivo de convertir un UserEntity nulo. Esto
    simplemente no se debería hacer nunca, si esta precondición falla el
    problema está en otro sitio.

--- app/src/main/java/com/shootr/android/db/mappers/UserEntityDBMapper.java
+++ app/src/main/java/com/shootr/android/db/mappers/UserEntityDBMapper.java
@@ -8,56 +8,15 @@ import java.text.Normalizer;
 
 public class UserEntityDBMapper extends GenericDBMapper {
 
-
-    public UserEntityDBMapper(){
+    public UserEntityDBMapper() {
 
     }
 
     public UserEntity fromCursor(Cursor c) {
-        UserEntity user = userEntityWithCommonFieldsFromCursor(c);
-        // Fields that might not come from server for all users
-        int emailTokenIndex = c.getColumnIndex(UserTable.EMAIL);
-        if (emailTokenIndex >= 0) {
-            user.setEmail(c.getString(emailTokenIndex));
-        }
-        setSynchronizedfromCursor(c, user);
-        return user;
-    }
-
-    public  ContentValues toContentValues(UserEntity u) {
-        ContentValues cv = new ContentValues();
-        if(u != null){
-            String email = u.getEmail();
-            if (email != null) {
-                cv.put(UserTable.EMAIL, email);
-            }
-            cv.put(UserTable.ID, u.getIdUser());
-            cv.put(UserTable.USER_NAME, u.getUserName());
-            cv.put(UserTable.NAME, u.getName());
-            cv.put(UserTable.PHOTO, u.getPhoto());
-            cv.put(UserTable.NUM_FOLLOWERS, u.getNumFollowers());
-            cv.put(UserTable.NUM_FOLLOWINGS, u.getNumFollowings());
-            cv.put(UserTable.POINTS, u.getPoints());
-            cv.put(UserTable.RANK, u.getRank());
-            cv.put(UserTable.BIO, u.getBio());
-            cv.put(UserTable.WEBSITE, u.getWebsite());
-            cv.put(UserTable.NAME_NORMALIZED,normalizedText(u.getName()));
-            cv.put(UserTable.EMAIL_NORMALIZED,normalizedText(u.getEmail()));
-            cv.put(UserTable.EMAIL_CONFIRMED,u.getEmailConfirmed());
-            cv.put(UserTable.USER_NAME_NORMALIZED,normalizedText(u.getUserName()));
-            cv.put(UserTable.ID_WATCHING_STREAM, u.getIdWatchingStream());
-            cv.put(UserTable.WATCHING_STREAM_TITLE, u.getWatchingStreamTitle());
-            cv.put(UserTable.JOIN_STREAM_DATE, u.getJoinStreamDate());
-            cv.put(UserTable.WATCHING_SYNCHRONIZED, u.getWatchSynchronizedStatus());
-            setSynchronizedtoContentValues(u, cv);
-        }
-        return cv;
-    }
-
-    public UserEntity userEntityWithCommonFieldsFromCursor(Cursor c){
         UserEntity user = new UserEntity();
         user.setIdUser(c.getString(c.getColumnIndex(UserTable.ID)));
         user.setUserName(c.getString(c.getColumnIndex(UserTable.USER_NAME)));
+        user.setEmail(c.getString(c.getColumnIndex(UserTable.EMAIL)));
         user.setName(c.getString(c.getColumnIndex(UserTable.NAME)));
         user.setEmail(c.getString(c.getColumnIndex(UserTable.EMAIL)));
         user.setEmailConfirmed(c.getInt(c.getColumnIndex(UserTable.EMAIL_CONFIRMED)));
@@ -72,18 +31,41 @@ public class UserEntityDBMapper extends GenericDBMapper {
         user.setWatchingStreamTitle(c.getString(c.getColumnIndex(UserTable.WATCHING_STREAM_TITLE)));
         user.setJoinStreamDate(c.getLong(c.getColumnIndex(UserTable.JOIN_STREAM_DATE)));
         user.setWatchSynchronizedStatus(c.getString(c.getColumnIndex(UserTable.WATCHING_SYNCHRONIZED)));
+
+        setSynchronizedfromCursor(c, user);
         return user;
     }
 
-
-    public String normalizedText(String s){
-        if(s!=null){
-            return Normalizer.normalize(s, Normalizer.Form.NFD)
-              .replaceAll("[^\\p{ASCII}]", "");
-        }else{
-            return null;
-        }
-
+    public ContentValues toContentValues(UserEntity u) {
+        ContentValues cv = new ContentValues();
+        cv.put(UserTable.ID, u.getIdUser());
+        cv.put(UserTable.USER_NAME, u.getUserName());
+        cv.put(UserTable.EMAIL, u.getEmail());
+        cv.put(UserTable.NAME, u.getName());
+        cv.put(UserTable.PHOTO, u.getPhoto());
+        cv.put(UserTable.NUM_FOLLOWERS, u.getNumFollowers());
+        cv.put(UserTable.NUM_FOLLOWINGS, u.getNumFollowings());
+        cv.put(UserTable.POINTS, u.getPoints());
+        cv.put(UserTable.RANK, u.getRank());
+        cv.put(UserTable.BIO, u.getBio());
+        cv.put(UserTable.WEBSITE, u.getWebsite());
+        cv.put(UserTable.NAME_NORMALIZED, normalizedText(u.getName()));
+        cv.put(UserTable.EMAIL_NORMALIZED, normalizedText(u.getEmail()));
+        cv.put(UserTable.EMAIL_CONFIRMED, u.getEmailConfirmed());
+        cv.put(UserTable.USER_NAME_NORMALIZED, normalizedText(u.getUserName()));
+        cv.put(UserTable.ID_WATCHING_STREAM, u.getIdWatchingStream());
+        cv.put(UserTable.WATCHING_STREAM_TITLE, u.getWatchingStreamTitle());
+        cv.put(UserTable.JOIN_STREAM_DATE, u.getJoinStreamDate());
+        cv.put(UserTable.WATCHING_SYNCHRONIZED, u.getWatchSynchronizedStatus());
+        setSynchronizedtoContentValues(u, cv);
+        return cv;
     }
 
+    public String normalizedText(String s) {
+        if (s != null) {
+            return Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+        } else {
+            return null;
+        }
+    }
 }
