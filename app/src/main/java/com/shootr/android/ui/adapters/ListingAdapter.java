d18c3f7d74acba46ef3e389cf4ca36a63c6e1f48
Rafa Vázquez <rafael.vazquez@fav24.com>
2015-10-06 09:57:28 +0200

    Creado ListingAdapter con secciones
    Se usan dos secciones en ListingAdapter (que reemplaza ListingStreamsAdapter)
    para mostrar los streams holding y los favoritos.
    
    Se usa la librería SectionedRecyclerView (https://github.com/truizlop/SectionedRecyclerView),
    pero tiene varios bugs y se ha tenido que copiar el código dentro del proyecto.
    Pull requests han sido enviados con correcciones, cuando se integren y publiquen
    se debería eliminar dicho código e importar la librería externa.
    Las clases copiadas son HeaderViewHolder y SectionedRecyclerViewAdapter.
    La librería tiene un SimpleSectionedAdapter para omitir el uso de footers.
    
    El adapter contine 2 listas de streams, los creados y los favoritos.
    Ahora mismo y por propósitos de prueba se mantiene el método setStreams
    del antiguo adapter, que setteará por duplicado los streams en holding y
    en favorites. Cuando la vista se integre con la lógica del presenter se
    deberán crear métodos independientes.
    
    Falta la gestión de mostrar o no los títulos.

---
+++ app/src/main/java/com/shootr/android/ui/adapters/ListingAdapter.java
@@ -0,0 +1,127 @@
+package com.shootr.android.ui.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.shootr.android.R;
+import com.shootr.android.ui.adapters.listeners.OnFavoriteClickListener;
+import com.shootr.android.ui.adapters.listeners.OnStreamClickListener;
+import com.shootr.android.ui.adapters.sectionedrecyclerview.HeaderViewHolder;
+import com.shootr.android.ui.adapters.sectionedrecyclerview.SectionedRecyclerViewAdapter;
+import com.shootr.android.ui.model.StreamResultModel;
+import com.shootr.android.util.ImageLoader;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ListingAdapter extends SectionedRecyclerViewAdapter<HeaderViewHolder, StreamResultViewHolder> {
+
+    private final ImageLoader imageLoader;
+    private final boolean isCurrentUser;
+    private final OnStreamClickListener onStreamClickListener;
+    private final OnFavoriteClickListener onFavoriteClickListener;
+
+    private List<StreamResultModel> createdStreams = Collections.emptyList();
+    private List<StreamResultModel> favoritedStreams = Collections.emptyList();
+
+    private Set<String> favoriteStreamsIds = Collections.emptySet();
+    private boolean showTitles = true;
+
+    public ListingAdapter(ImageLoader imageLoader,
+      boolean isCurrentUser,
+      OnStreamClickListener onStreamClickListener,
+      OnFavoriteClickListener onFavoriteClickListener) {
+        this.imageLoader = imageLoader;
+        this.isCurrentUser = isCurrentUser;
+        this.onStreamClickListener = onStreamClickListener;
+        this.onFavoriteClickListener = onFavoriteClickListener;
+    }
+
+    @Override
+    protected int getSectionCount() {
+        return 2;
+    }
+
+    @Override
+    protected int getItemCountForSection(int section) {
+        if (section == 0) {
+            return createdStreams.size();
+        } else if (section == 1) {
+            return favoritedStreams.size();
+        } else {
+            throw new IllegalArgumentException(String.format("Section %d not allowed", section));
+        }
+    }
+
+    @Override
+    protected HeaderViewHolder onCreateSectionHeaderViewHolder(ViewGroup parent, int viewType) {
+        View text = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card_title_separator, parent, false);
+        return new HeaderViewHolder(text, android.R.id.text1);
+    }
+
+    @Override
+    protected StreamResultViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
+        int layoutRes = isCurrentUser ? R.layout.item_list_listing_stream : R.layout.item_list_stream;
+        View view = LayoutInflater.from(parent.getContext()).inflate(layoutRes, parent, false);
+
+        ListingStreamResultViewHolder listingStreamResultViewHolder =
+          new ListingStreamResultViewHolder(view, onStreamClickListener, imageLoader, onFavoriteClickListener);
+
+        if (isCurrentUser) {
+            listingStreamResultViewHolder.setShowsWatchersText(true);
+        }
+        return listingStreamResultViewHolder;
+    }
+
+    @Override
+    protected void onBindSectionHeaderViewHolder(HeaderViewHolder holder, int section) {
+        if (section == 0) {
+            //TODO from resources
+            holder.render("Holding$");
+        } else {
+            holder.render("Favorites$");
+        }
+        holder.itemView.findViewById(android.R.id.text1).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onBindItemViewHolder(StreamResultViewHolder holder, int section, int position) {
+        ((ListingStreamResultViewHolder) holder).setFavorite(isFavorite(section, position));
+        StreamResultModel stream = getItem(section, position);
+        boolean showSeparator = position != 0;
+        if (isCurrentUser) {
+            holder.render(stream, showSeparator);
+        } else {
+            holder.render(stream, showSeparator);
+        }
+    }
+
+    private StreamResultModel getItem(int section, int position) {
+        if (section == 0) {
+            return createdStreams.get(position);
+        } else {
+            return favoritedStreams.get(position);
+        }
+    }
+
+    private boolean isFavorite(int section, int position) {
+        String idStream = getItem(section, position).getStreamModel().getIdStream();
+        return favoriteStreamsIds != null && favoriteStreamsIds.contains(idStream);
+    }
+
+    public void setFavoriteStreams(List<StreamResultModel> favoriteStreams) {
+        favoriteStreamsIds = new HashSet<>(favoriteStreams.size());
+        for (StreamResultModel stream : favoriteStreams) {
+            favoriteStreamsIds.add(stream.getStreamModel().getIdStream());
+        }
+    }
+
+    @Deprecated
+    public void setStreams(List<StreamResultModel> streams) {
+        //TODO decompose in two methods
+        this.createdStreams = streams;
+        this.favoritedStreams = streams;
+        this.notifyDataSetChanged();
+    }
+}
