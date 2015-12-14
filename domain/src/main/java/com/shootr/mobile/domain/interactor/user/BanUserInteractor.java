1080e4cb737e0fcea040441a038baff931c698a5
Arturo <arturo.jimenez@fav24.com>
2015-12-14 16:27:01 +0100

    BanUserInteractor
    Es practicamente un copypaste de BlockUserInteractor

--- domain/src/main/java/com/shootr/mobile/domain/interactor/user/BanUserInteractor.java
+++ domain/src/main/java/com/shootr/mobile/domain/interactor/user/BanUserInteractor.java
@@ -1,5 +1,67 @@
 package com.shootr.mobile.domain.interactor.user;
 
-public class BanUserInteractor {
+import com.shootr.mobile.domain.exception.ServerCommunicationException;
+import com.shootr.mobile.domain.exception.ShootrException;
+import com.shootr.mobile.domain.executor.PostExecutionThread;
+import com.shootr.mobile.domain.interactor.Interactor;
+import com.shootr.mobile.domain.interactor.InteractorHandler;
+import com.shootr.mobile.domain.repository.FollowRepository;
+import com.shootr.mobile.domain.repository.Local;
+import com.shootr.mobile.domain.repository.Remote;
+import javax.inject.Inject;
+
+import static com.shootr.mobile.domain.utils.Preconditions.checkNotNull;
+
+public class BanUserInteractor implements Interactor {
+
+    private final InteractorHandler interactorHandler;
+    private final PostExecutionThread postExecutionThread;
+    private final FollowRepository localFollowRepository;
+    private final FollowRepository remoteFollowRepository;
+
+    private String idUser;
+    private Interactor.CompletedCallback callback;
+    private Interactor.ErrorCallback errorCallback;
+
+    @Inject public BanUserInteractor(InteractorHandler interactorHandler, PostExecutionThread postExecutionThread,
+      @Local FollowRepository localFollowRepository, @Remote FollowRepository remoteFollowRepository) {
+        this.interactorHandler = interactorHandler;
+        this.postExecutionThread = postExecutionThread;
+        this.localFollowRepository = localFollowRepository;
+        this.remoteFollowRepository = remoteFollowRepository;
+    }
+
+    public void ban(String idUser, Interactor.CompletedCallback callback, Interactor.ErrorCallback errorCallback) {
+        this.idUser = checkNotNull(idUser);
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+        interactorHandler.execute(this);
+    }
+
+    @Override public void execute() throws Exception {
+        try {
+            remoteFollowRepository.ban(idUser);
+            localFollowRepository.ban(idUser);
+            notifyCompleted();
+        } catch (ServerCommunicationException error) {
+            notifyError(error);
+        }
+    }
+
+    private void notifyCompleted() {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                callback.onCompleted();
+            }
+        });
+    }
+
+    private void notifyError(final ShootrException error) {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                errorCallback.onError(error);
+            }
+        });
+    }
 
 }
