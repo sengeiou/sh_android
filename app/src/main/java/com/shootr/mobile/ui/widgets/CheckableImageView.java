de7c39d56a1631442fb0eb64fd4344fdcee10f32
Arturo <arturo.jimenez@fav24.com>
2016-01-07 15:52:15 +0100

    Constante no cumplia la convencion del naming

--- app/src/main/java/com/shootr/mobile/ui/widgets/CheckableImageView.java
+++ app/src/main/java/com/shootr/mobile/ui/widgets/CheckableImageView.java
@@ -30,7 +30,7 @@ public class CheckableImageView extends ImageView implements Checkable {
         super(context, attrs);
     }
 
-    private static final int[] CheckedStateSet = {android.R.attr.state_checked};
+    private static final int[] CHECKED_STATE_SET = {android.R.attr.state_checked};
 
     private boolean mChecked = false;
 
@@ -53,7 +53,7 @@ public class CheckableImageView extends ImageView implements Checkable {
     public int[] onCreateDrawableState(int extraSpace) {
         final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
         if (isChecked()) {
-            mergeDrawableStates(drawableState, CheckedStateSet);
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
         }
         return drawableState;
     }
