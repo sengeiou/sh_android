3ea4a152892b80d27135ba0319bc1013366075e5
Arturo <arturo.jimenez@fav24.com>
2016-01-07 11:12:07 +0100

    Optimizados add/remove to favorites
    Se utiliza RxJava en los interactors para agilizar el add o remove y que no
    haya inconsistencias cuando se eliminan.

--- domain/src/main/java/com/shootr/mobile/domain/interactor/stream/RemoveFromFavoritesInteractor.java
+++ domain/src/main/java/com/shootr/mobile/domain/interactor/stream/RemoveFromFavoritesInteractor.java
@@ -4,10 +4,13 @@ import com.shootr.mobile.domain.Favorite;
 import com.shootr.mobile.domain.executor.PostExecutionThread;
 import com.shootr.mobile.domain.interactor.Interactor;
 import com.shootr.mobile.domain.interactor.InteractorHandler;
+import com.shootr.mobile.domain.interactor.OnCompletedObserver;
 import com.shootr.mobile.domain.repository.FavoriteRepository;
 import com.shootr.mobile.domain.repository.Local;
 import com.shootr.mobile.domain.repository.Remote;
 import javax.inject.Inject;
+import rx.Observable;
+import rx.Subscriber;
 
 public class RemoveFromFavoritesInteractor implements Interactor {
 
@@ -38,10 +41,36 @@ public class RemoveFromFavoritesInteractor implements Interactor {
     @Override public void execute() throws Exception {
         Favorite existingFavorite = localFavoriteRepository.getFavoriteByStream(idStream);
         if (existingFavorite != null) {
-            localFavoriteRepository.removeFavoriteByStream(existingFavorite.getIdStream());
-            notifyCompleted();
-            remoteFavoriteRepository.removeFavoriteByStream(existingFavorite.getIdStream());
+            subscribeOnCompletedObserverToObservable(remoteRemoveFromFavoritesObservable(existingFavorite));
+            subscribeOnCompletedObserverToObservable(localRemoveFromFavoritesObservable(existingFavorite));
         }
+    }
+
+    private Observable<Void> localRemoveFromFavoritesObservable(final Favorite favorite) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override public void call(Subscriber<? super Void> subscriber) {
+                localFavoriteRepository.removeFavoriteByStream(favorite.getIdStream());
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    private Observable<Void> remoteRemoveFromFavoritesObservable(final Favorite favorite) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override public void call(Subscriber<? super Void> subscriber) {
+                remoteFavoriteRepository.removeFavoriteByStream(favorite.getIdStream());
+                subscriber.onCompleted();
+                notifyCompleted();
+            }
+        });
+    }
+
+    private void subscribeOnCompletedObserverToObservable(Observable<Void> observable) {
+        observable.subscribe(new OnCompletedObserver<Void>() {
+            @Override public void onError(Throwable error) {
+                /* no-op */
+            }
+        });
     }
 
     protected void notifyCompleted() {
