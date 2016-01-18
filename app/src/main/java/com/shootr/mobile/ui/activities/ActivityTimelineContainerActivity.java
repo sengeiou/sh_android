5e03d38f910cf9a7524cfdf89728e356ec24eb0b
Arturo <arturo.jimenez@fav24.com>
2016-01-18 11:55:56 +0100

    Corregido back intent de Activity Timeline
    Anteriormente había un finish() que mataba la app si no teniamos ninguna task
    en el backstack. Ahora manejamos el backstak como en todas las activities que
    son accedidas desde notificaciones push.

--- app/src/main/java/com/shootr/mobile/ui/activities/ActivityTimelineContainerActivity.java
+++ app/src/main/java/com/shootr/mobile/ui/activities/ActivityTimelineContainerActivity.java
@@ -1,11 +1,14 @@
 package com.shootr.mobile.ui.activities;
 
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
 import com.shootr.mobile.R;
 import com.shootr.mobile.ui.ToolbarDecorator;
 import com.shootr.mobile.ui.fragments.ActivityTimelineFragment;
+import java.util.List;
 
 public class ActivityTimelineContainerActivity extends BaseToolbarDecoratedActivity {
 
@@ -37,10 +40,23 @@ public class ActivityTimelineContainerActivity extends BaseToolbarDecoratedActiv
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+            handleBackIntent();
             return true;
         } else {
             return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void handleBackIntent() {
+        ActivityManager mngr = (ActivityManager) getSystemService( ACTIVITY_SERVICE );
+        List<ActivityManager.RunningTaskInfo> taskList = mngr.getRunningTasks(10);
+        if(taskList.get(0).numActivities == 1 &&
+          taskList.get(0).topActivity.getClassName().equals(this.getClass().getName())) {
+            Intent intent = new Intent(this, MainTabbedActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } else {
+            finish();
         }
     }
 }
