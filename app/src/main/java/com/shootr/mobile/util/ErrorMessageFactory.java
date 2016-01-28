17e78983ee0caf5ac49563b75e70d36812ac7978
Arturo <arturo.jimenez@fav24.com>
2016-01-28 15:32:48 +0100

    Se eliminan strings y excepciones no usadas

--- app/src/main/java/com/shootr/mobile/util/ErrorMessageFactory.java
+++ app/src/main/java/com/shootr/mobile/util/ErrorMessageFactory.java
@@ -50,7 +50,6 @@ public class ErrorMessageFactory {
         codeResourceMap.put(ShootrError.ERROR_CODE_SEARCH_TOO_SHORT, R.string.error_message_search_too_short);
         codeResourceMap.put(ShootrError.ERROR_CODE_STREAM_TITLE_TOO_SHORT, R.string.error_message_event_title_too_short);
         codeResourceMap.put(ShootrError.ERROR_CODE_STREAM_TITLE_TOO_LONG, R.string.error_message_event_title_too_long);
-        codeResourceMap.put(ShootrError.ERROR_CODE_STREAM_START_DATE_TOO_LATE, R.string.error_message_event_start_date_too_late);
         codeResourceMap.put(ShootrError.ERROR_CODE_UPDATE_REQUIRED, R.string.error_message_update_required);
         codeResourceMap.put(ShootrError.ERROR_CODE_REGISTRATION_USERNAME_DUPLICATE, R.string.error_message_registration_username_duplicated);
         codeResourceMap.put(ShootrError.ERROR_CODE_REGISTRATION_USERNAME_NULL, R.string.error_message_registration_username_empty);
