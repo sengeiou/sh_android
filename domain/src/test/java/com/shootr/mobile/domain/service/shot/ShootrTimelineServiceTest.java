9a033552b4a55aaf4d4a68055e2c74ecf60e3df3
Arturo <arturo.jimenez@fav24.com>
2016-02-08 11:08:24 +0100

    Revert "En el refresh activities si se pide todas las activities"
    This reverts commit 0c541c7d87f7f4758cec84bd76d65f52300e8bf3.
    
    Esto no es necesario. Es un error, solo hay que pedir activities visibles.

--- domain/src/test/java/com/shootr/mobile/domain/service/shot/ShootrTimelineServiceTest.java
+++ domain/src/test/java/com/shootr/mobile/domain/service/shot/ShootrTimelineServiceTest.java
@@ -138,7 +138,7 @@ public class ShootrTimelineServiceTest {
         assertThat(resultTimeline.getShots()).isSortedAccordingTo(new Shot.NewerAboveComparator());
     }
 
-    @Test public void shouldReturnAllActivityTypesIfIsThereWasLocalActivity() {
+    @Test public void shouldReturnVisibleActivityTypesIfIsThereWasLocalActivity() {
         when(localActivityRepository.getActivityTimeline(anyActivityParameters(), anyString())).thenReturn(activitiesList());
 
         ActivityTimeline activityTimeline = shootrTimelineService.refreshTimelinesForActivity(LANGUAGE);
@@ -146,7 +146,7 @@ public class ShootrTimelineServiceTest {
         ArgumentCaptor<ActivityTimelineParameters> argumentCaptor =
           ArgumentCaptor.forClass(ActivityTimelineParameters.class);
         verify(remoteActivityRepository).getActivityTimeline(argumentCaptor.capture(), anyString());
-        assertThat(argumentCaptor.getValue().getIncludedTypes()).containsExactly(allActivityTypes());
+        assertThat(argumentCaptor.getValue().getIncludedTypes()).containsExactly(visibleActivityTypes());
     }
 
     @Test public void shouldReturnVisibleActivityTypesIfThereWasNoLocalActivity() {
