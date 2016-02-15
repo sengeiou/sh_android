1e00d107a66ac3374fb57d742a7224b7dd2ef4a7
Arturo <arturo.jimenez@fav24.com>
2016-02-15 12:42:40 +0100

    Eliminado control de errores y string
    Se elimina el control de error de baneo al publicar un shot, porque ahora el
    usuario podrá publicarlos, pero sólo sus following podrán leerlo.

--- domain/src/main/java/com/shootr/mobile/domain/exception/UserBannedException.java
+++
@@ -1,9 +0,0 @@
-package com.shootr.mobile.domain.exception;
-
-public class UserBannedException extends ShootrException {
-
-    public UserBannedException(Throwable cause) {
-        super(cause);
-    }
-
-}
