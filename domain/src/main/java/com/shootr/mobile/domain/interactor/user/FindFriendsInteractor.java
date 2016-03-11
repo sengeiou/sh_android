18c57238c36d5a45eb74ef23756f2161b9b6e90d
jmartinez1360 <mrjem1360@gmail.com>
2016-03-11 09:55:05 +0100

    Se crea el nuevo interactor para encontrar amigos

---
+++ domain/src/main/java/com/shootr/mobile/domain/interactor/user/FindFriendsInteractor.java
@@ -0,0 +1,72 @@
+package com.shootr.mobile.domain.interactor.user;
+
+import com.shootr.mobile.domain.User;
+import com.shootr.mobile.domain.exception.ServerCommunicationException;
+import com.shootr.mobile.domain.exception.ShootrException;
+import com.shootr.mobile.domain.executor.PostExecutionThread;
+import com.shootr.mobile.domain.interactor.Interactor;
+import com.shootr.mobile.domain.interactor.InteractorHandler;
+import com.shootr.mobile.domain.repository.Local;
+import com.shootr.mobile.domain.repository.Remote;
+import com.shootr.mobile.domain.repository.UserRepository;
+import com.shootr.mobile.domain.utils.LocaleProvider;
+import java.util.List;
+import javax.inject.Inject;
+
+public class FindFriendsInteractor implements Interactor {
+
+    private final InteractorHandler interactorHandler;
+    private final UserRepository remoteUserRepository;
+    private final UserRepository localUserRepository;
+    private final PostExecutionThread postExecutionThread;
+    private final LocaleProvider localeProvider;
+
+    private Callback<List<User>> callback;
+    private ErrorCallback errorCallback;
+    private String search;
+    private Integer currentPage;
+
+    @Inject public FindFriendsInteractor(InteractorHandler interactorHandler, @Local UserRepository localUserRepository,
+      @Remote UserRepository remoteUserRepository, PostExecutionThread postExecutionThread,
+      LocaleProvider localeProvider) {
+        this.interactorHandler = interactorHandler;
+        this.remoteUserRepository = remoteUserRepository;
+        this.localUserRepository = localUserRepository;
+        this.postExecutionThread = postExecutionThread;
+        this.localeProvider = localeProvider;
+    }
+
+    public void FindFriends(String search, Integer currentPage, Callback<List<User>> callback,
+      ErrorCallback errorCallback) {
+        this.search = search;
+        this.currentPage = currentPage;
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+        interactorHandler.execute(this);
+    }
+
+    @Override public void execute() throws Exception {
+        try {
+            notifyLoaded(localUserRepository.getUsersForSearch(search, currentPage, localeProvider.getLocale() ));
+            notifyLoaded(remoteUserRepository.getUsersForSearch(search, currentPage, localeProvider.getLocale()));
+        } catch (ServerCommunicationException error) {
+            notifyError(error);
+        }
+    }
+
+    private void notifyLoaded(final List<User> results) {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                callback.onLoaded(results);
+            }
+        });
+    }
+
+    private void notifyError(final ShootrException error) {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                errorCallback.onError(error);
+            }
+        });
+    }
+}
