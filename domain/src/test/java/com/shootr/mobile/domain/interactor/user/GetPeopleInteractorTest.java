bbd6b9378143cee98a2fec18985c9c81e07f69de
jmartinez1360 <mrjem1360@gmail.com>
2016-03-22 10:25:25 +0100

    Modificación del test del getPeopleInteractor
    Debido a los cambios en el interactor los tests de esta clase han sufrido cambis para
    probar su funcionalidad

--- domain/src/test/java/com/shootr/mobile/domain/interactor/user/GetPeopleInteractorTest.java
+++ domain/src/test/java/com/shootr/mobile/domain/interactor/user/GetPeopleInteractorTest.java
@@ -2,6 +2,8 @@ package com.shootr.mobile.domain.interactor.user;
 
 import com.shootr.mobile.domain.User;
 import com.shootr.mobile.domain.UserList;
+import com.shootr.mobile.domain.executor.TestPostExecutionThread;
+import com.shootr.mobile.domain.interactor.Interactor;
 import com.shootr.mobile.domain.interactor.TestInteractorHandler;
 import com.shootr.mobile.domain.repository.UserRepository;
 import java.util.ArrayList;
@@ -12,44 +14,48 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class GetPeopleInteractorTest {
 
-    @Spy TestInteractorHandler interactorHandler = new TestInteractorHandler();
     @Mock UserRepository localUserRepository;
     @Mock UserRepository remoteUserRepository;
+    @Mock Interactor.Callback<UserList> callback;
+    @Mock Interactor.ErrorCallback errorCallback;
     private GetPeopleInteractor getPeopleInteractor;
 
     @Before public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        getPeopleInteractor = new GetPeopleInteractor(interactorHandler, remoteUserRepository, localUserRepository);
+        TestInteractorHandler interactorHandler = new TestInteractorHandler();
+        TestPostExecutionThread postExecutionThread = new TestPostExecutionThread();
+        getPeopleInteractor =
+          new GetPeopleInteractor(interactorHandler, remoteUserRepository, localUserRepository, postExecutionThread);
     }
 
     @Test public void resultsAreSortedByUsername() throws Exception {
         setupRepositoryReturnsUserList();
 
-        getPeopleInteractor.obtainPeople();
+        getPeopleInteractor.obtainPeople(callback, errorCallback);
 
         ArgumentCaptor<UserList> argumentCaptor = ArgumentCaptor.forClass(UserList.class);
-        verify(interactorHandler, atLeastOnce()).sendUiMessage(argumentCaptor.capture());
+        verify(callback, atLeastOnce()).onLoaded(argumentCaptor.capture());
 
         assertThat(argumentCaptor.getValue().getUsers()).isSortedAccordingTo(new TestUsernameComparator());
     }
 
-    @Test public void shouldCallbackOnceWhenNoPeopleReturned() throws Exception {
+    @Test public void shouldNeverCallbackWhenNoPeopleReturned() throws Exception {
         when(localUserRepository.getPeople()).thenReturn(emptyUserList());
         when(remoteUserRepository.getPeople()).thenReturn(emptyUserList());
 
-        getPeopleInteractor.obtainPeople();
+        getPeopleInteractor.obtainPeople(callback, errorCallback);
 
-        verify(interactorHandler).sendUiMessage(anyObject());
+        verify(callback, never()).onLoaded(any(UserList.class));
     }
 
     private List<User> emptyUserList() {
@@ -68,6 +74,10 @@ public class GetPeopleInteractorTest {
         mockUsers.add(mockUser("cusername"));
         mockUsers.add(mockUser("Dusername"));
         return mockUsers;
+    }
+
+    private UserList userList() {
+        return new UserList(mockUserList());
     }
 
     private User mockUser(String username) {
