30617f9c405a48f480091b792ab5b735787fbf69
jmartinez1360 <mrjem1360@gmail.com>
2016-03-29 16:44:49 +0200

    Creación de los interactors para añadir y remover contributors
    Se crean los interactors necesarios para hacer las llamadas en local
    y remoto. Estos interactors aún se han de desarollar debido a que no se puede
    ir más profundo por ahora

---
+++ domain/src/main/java/com/shootr/mobile/domain/interactor/user/AddContributorInteractor.java
@@ -0,0 +1,69 @@
+package com.shootr.mobile.domain.interactor.user;
+
+import com.shootr.mobile.domain.exception.ShootrException;
+import com.shootr.mobile.domain.executor.PostExecutionThread;
+import com.shootr.mobile.domain.interactor.Interactor;
+import com.shootr.mobile.domain.interactor.InteractorHandler;
+import com.shootr.mobile.domain.repository.Local;
+import com.shootr.mobile.domain.repository.Remote;
+import com.shootr.mobile.domain.repository.UserRepository;
+import javax.inject.Inject;
+
+public class AddContributorInteractor implements Interactor {
+
+    private final InteractorHandler interactorHandler;
+    private final UserRepository localUserRepository;
+    private final UserRepository remoteUserRepository;
+    private final PostExecutionThread postExecutionThread;
+
+    private String idStream;
+    private String idUser;
+    private CompletedCallback callback;
+    private ErrorCallback errorCallback;
+
+    @Inject public AddContributorInteractor(InteractorHandler interactorHandler, @Local UserRepository localUserRepository,
+      @Remote UserRepository remoteUserRepository, PostExecutionThread postExecutionThread) {
+        this.interactorHandler = interactorHandler;
+        this.localUserRepository = localUserRepository;
+        this.remoteUserRepository = remoteUserRepository;
+        this.postExecutionThread = postExecutionThread;
+    }
+
+    public void addContributor(String idStream, String idUser, CompletedCallback callback, ErrorCallback errorCallback) {
+        this.idStream = idStream;
+        this.idUser = idUser;
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+        interactorHandler.execute(this);
+    }
+
+    @Override public void execute() throws Exception {
+        addLocalContributor(idStream, idUser);
+        addRemoteContributor(idStream, idUser);
+    }
+
+    private void addRemoteContributor(String idStream, String idUser) {
+        //TODO: call remoteRepository
+        notifyCompleted();
+    }
+
+    private void addLocalContributor(String idStream, String idUser) {
+        //TODO: call localRepository
+    }
+
+    private void notifyCompleted() {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                callback.onCompleted();
+            }
+        });
+    }
+
+    private void notifyError(final ShootrException error) {
+        postExecutionThread.post(new Runnable() {
+            @Override public void run() {
+                errorCallback.onError(error);
+            }
+        });
+    }
+}
