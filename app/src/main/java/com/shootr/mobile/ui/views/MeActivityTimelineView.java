884eea370cc00ea1a05bf1416e4bb6228e1a0b10
arturet <arturo.jimenez@fav24.com>
2016-04-04 16:27:43 +0200

    Se crea un nuevo Presenter genérico que controla las vistas
    Esto se hace porque la lógica es la misma en ambos presenters, ha sido necesario crear un par de vistas especificas que extieden a la vista genérica. Esta vista genérica se usa en el presenter para hacer todos los procesos, pero es necesario diferenciarlas entre ellas para que no se muestren errores.

--- app/src/main/java/com/shootr/mobile/ui/views/MeActivityTimelineView.java
+++ app/src/main/java/com/shootr/mobile/ui/views/MeActivityTimelineView.java
@@ -1,7 +1,5 @@
 package com.shootr.mobile.ui.views;
 
-import com.shootr.mobile.ui.views.nullview.GenericActivityTimelineView;
-
 public interface MeActivityTimelineView extends GenericActivityTimelineView {
 
 }
