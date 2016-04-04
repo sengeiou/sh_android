084abe5117d0beb6c40a8082f3c42e53bf2b895c
arturet <arturo.jimenez@fav24.com>
2016-04-04 15:22:35 +0200

    Creada una vista de activity genérica extendida por ActivityTimelineView y MeActivityTimelineView
    Así quitamos duplicidad de código

--- app/src/main/java/com/shootr/mobile/ui/views/MeActivityTimelineView.java
+++ app/src/main/java/com/shootr/mobile/ui/views/MeActivityTimelineView.java
@@ -1,27 +1,7 @@
 package com.shootr.mobile.ui.views;
 
-import com.shootr.mobile.ui.model.ActivityModel;
-import com.shootr.mobile.ui.views.base.LoadDataView;
+import com.shootr.mobile.ui.views.nullview.GenericActivityTimelineView;
 
-import java.util.List;
+public interface MeActivityTimelineView extends GenericActivityTimelineView {
 
-public interface MeActivityTimelineView extends LoadDataView {
-
-    void setActivities(List<ActivityModel> activities, String currentUserId);
-
-    void hideActivities();
-
-    void showActivities();
-
-    void addNewActivities(List<ActivityModel> newActivities);
-
-    void addOldActivities(List<ActivityModel> oldActivities);
-
-    void showLoadingOldActivities();
-
-    void hideLoadingOldActivities();
-
-    void showLoadingActivity();
-
-    void hideLoadingActivity();
 }
---
+++ app/src/main/java/com/shootr/mobile/ui/views/nullview/GenericActivityTimelineView.java
@@ -0,0 +1,27 @@
+package com.shootr.mobile.ui.views.nullview;
+
+import com.shootr.mobile.ui.model.ActivityModel;
+import com.shootr.mobile.ui.views.base.LoadDataView;
+
+import java.util.List;
+
+public interface GenericActivityTimelineView extends LoadDataView{
+
+    void setActivities(List<ActivityModel> activities, String currentUserId);
+
+    void hideActivities();
+
+    void showActivities();
+
+    void addNewActivities(List<ActivityModel> newActivities);
+
+    void addOldActivities(List<ActivityModel> oldActivities);
+
+    void showLoadingOldActivities();
+
+    void hideLoadingOldActivities();
+
+    void showLoadingActivity();
+
+    void hideLoadingActivity();
+}
