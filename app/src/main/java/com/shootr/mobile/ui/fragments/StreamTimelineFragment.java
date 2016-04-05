275c3c374e75ebb705cc8c8fb393c6137ac43659
jmartinez1360 <mrjem1360@gmail.com>
2016-04-05 17:08:00 +0200

    Rename de strings de pin message
    Estos nombres no eran correctos, por lo que se cambian a nombres
    más explicativos

--- app/src/main/java/com/shootr/mobile/ui/fragments/StreamTimelineFragment.java
+++ app/src/main/java/com/shootr/mobile/ui/fragments/StreamTimelineFragment.java
@@ -650,15 +650,15 @@ public class StreamTimelineFragment extends BaseFragment
     }
 
     @Override public void showNotificationConfirmation(final String topic) {
-        new AlertDialog.Builder(getActivity()).setTitle(R.string.title_message_notification)
-          .setMessage(getString(R.string.message_notification_confirmation_text))
-          .setPositiveButton(getString(R.string.message_notification_confirmation_yes),
+        new AlertDialog.Builder(getActivity()).setTitle(R.string.title_pin_message_notification)
+          .setMessage(getString(R.string.pin_message_notification_confirmation_text))
+          .setPositiveButton(getString(R.string.pin_message_notification_confirmation_yes),
             new DialogInterface.OnClickListener() {
                 @Override public void onClick(DialogInterface dialog, int which) {
                     streamTimelinePresenter.notifyMessage(topic, true);
                 }
             })
-          .setNegativeButton(getString(R.string.message_notification_confirmation_no),
+          .setNegativeButton(getString(R.string.pin_message_notification_confirmation_no),
             new DialogInterface.OnClickListener() {
                 @Override public void onClick(DialogInterface dialog, int which) {
                     streamTimelinePresenter.notifyMessage(topic, false);
