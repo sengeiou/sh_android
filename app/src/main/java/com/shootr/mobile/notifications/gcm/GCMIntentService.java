538849fc0afbf1da094b7ee77f2981fa6d7b18d8
jmartinez1360 <mrjem1360@gmail.com>
2016-04-06 11:18:39 +0200

    Se elimina el ActivityType PINNED_MESSAGE
    Debido a que este push no genera actividad, no se ha de crear
    este nuevo ActivityType

--- app/src/main/java/com/shootr/mobile/notifications/gcm/GCMIntentService.java
+++ app/src/main/java/com/shootr/mobile/notifications/gcm/GCMIntentService.java
@@ -127,7 +127,6 @@ public class GCMIntentService extends IntentService {
             case ActivityType.CHECKIN:
             case ActivityType.SHARE_STREAM:
             case ActivityType.OPENED_STREAM:
-            case ActivityType.PINNED_MESSAGE:
                 setupGoToStreamTimelineNotification(push);
                 break;
             case ActivityType.NICE_SHOT:
