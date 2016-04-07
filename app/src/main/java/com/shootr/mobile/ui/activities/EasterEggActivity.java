0693f10de34b8de2cc2358c112f9288f3c02dcc2
jmartinez1360 <mrjem1360@gmail.com>
2016-04-07 15:41:50 +0200

    Se crea la activity del easter egg
    Esta activity no hace nada, simplemente se declara, ya que todos
    los datos son estáticos en el xml.
    
    Donde hay más cosas es en el xml debido a que el frmaeLayout se repite tres veces.
    
    El manifest se ve afectado añadiendo la última activity, por eso se reformatea el código automáticamente

---
+++ app/src/main/java/com/shootr/mobile/ui/activities/EasterEggActivity.java
@@ -0,0 +1,13 @@
+package com.shootr.mobile.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.shootr.mobile.R;
+
+public class EasterEggActivity extends Activity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easter_egg);
+    }
+}
