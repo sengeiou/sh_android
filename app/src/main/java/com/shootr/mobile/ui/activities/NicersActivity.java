7af8d10131a76bb6f8ff400c93d0d9969ecef51a
jmartinez1360 <mrjem1360@gmail.com>
2016-04-11 16:22:54 +0200

    Uso del presenter en la activity
    Se enlaza la activitiy con el presenter, a demás, de usar un adapter
    Cabe mencionar que el adapter usado es del tipo UserAdapter debido
    a que erra redundate hacer un adapter que es practicamente igual
    al UserListAdapter.
    
    El showEmpty y el hideEmpty se quedan en TODO debido a que se necesita
    instnaciar el textView en el xml y se necestian los recursos strings.
    
    El showProgressView y el hidePorgressView no son operativos debido
    a que no hay un paginado en los nicers (a menos en Instagram no lo hay y no
    se ha mencionado nada de esto)

--- app/src/main/java/com/shootr/mobile/ui/activities/NicersActivity.java
+++ app/src/main/java/com/shootr/mobile/ui/activities/NicersActivity.java
@@ -1,5 +1,9 @@
 package com.shootr.mobile.ui.activities;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,23 +11,34 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 import com.shootr.mobile.R;
 import com.shootr.mobile.ui.ToolbarDecorator;
+import com.shootr.mobile.ui.adapters.UserListAdapter;
 import com.shootr.mobile.ui.model.UserModel;
+import com.shootr.mobile.ui.presenter.NicersPresenter;
 import com.shootr.mobile.ui.views.NicersView;
 import com.shootr.mobile.util.FeedbackMessage;
 import java.util.List;
 import javax.inject.Inject;
 
-public class NicersActivity extends BaseToolbarDecoratedActivity implements NicersView {
+public class NicersActivity extends BaseToolbarDecoratedActivity implements NicersView,
+  UserListAdapter.FollowUnfollowAdapterCallback {
 
+    private static final String EXTRA_ID_SHOT = "idShot";
     @Bind(R.id.nicerslist_list) ListView nicerslistListView;
     @Bind(R.id.nicerslist_progress) ProgressBar progressBar;
 
     @Inject FeedbackMessage feedbackMessage;
 
-    View progressViewContent;
-    View progressView;
+    private NicersPresenter presenter;
+    private UserListAdapter adapter;
+
+    public static Intent newIntent(Context context, String idStream) {
+        Intent intent = new Intent(context, NicersActivity.class);
+        intent.putExtra(EXTRA_ID_SHOT, idStream);
+        return intent;
+    }
 
     @Override protected int getLayoutResource() {
         return R.layout.activity_nicers;
@@ -31,10 +46,25 @@ public class NicersActivity extends BaseToolbarDecoratedActivity implements Nice
 
     @Override protected void initializeViews(Bundle savedInstanceState) {
         ButterKnife.bind(this);
-        //TODO: nicerslistListView.setAdapter(getNicersAdapter());
+        nicerslistListView.setAdapter(getNicersAdapter());
+    }
 
-        progressView = getLoadingView();
-        progressViewContent = ButterKnife.findById(progressView, R.id.loading_progress);
+    @Override public void onResume() {
+        super.onResume();
+        presenter.resume();
+    }
+
+    @Override public void onPause() {
+        super.onPause();
+        presenter.pause();
+    }
+
+    private UserListAdapter getNicersAdapter(){
+        if (adapter == null) {
+            adapter = new UserListAdapter(this, imageLoader);
+            adapter.setCallback(this);
+        }
+        return adapter;
     }
 
     private View getLoadingView() {
@@ -42,15 +72,17 @@ public class NicersActivity extends BaseToolbarDecoratedActivity implements Nice
     }
 
     @Override protected void initializePresenter() {
-        //TODO: initialize presenter
+        String idShot = getIntent().getStringExtra(EXTRA_ID_SHOT);
+        presenter.initialize(this, idShot);
     }
 
     @Override protected void setupToolbar(ToolbarDecorator toolbarDecorator) {
         /* no-op */
     }
 
-    @Override public void renderNicers(List<UserModel> transform) {
-        //TODO: call adapter.renderNicers()
+    @Override public void renderNicers(List<UserModel> users) {
+        adapter.setItems(users);
+        adapter.notifyDataSetChanged();
     }
 
     @Override public void showNicersList() {
@@ -58,19 +90,19 @@ public class NicersActivity extends BaseToolbarDecoratedActivity implements Nice
     }
 
     @Override public void hideProgressView() {
-        //TODO
+        /* no-op */
     }
 
     @Override public void showProgressView() {
-        //TODO
+        /* no-op */
     }
 
     @Override public void showEmpty() {
-        /* no-op */
+        //TODO
     }
 
     @Override public void hideEmpty() {
-        /* no-op */
+        //TODO
     }
 
     @Override public void showLoading() {
@@ -83,5 +115,28 @@ public class NicersActivity extends BaseToolbarDecoratedActivity implements Nice
 
     @Override public void showError(String message) {
         feedbackMessage.show(getView(), message);
+    }
+
+    @Override public void follow(int position) {
+        presenter.followUser(adapter.getItem(position));
+    }
+
+    @Override public void unFollow(int position) {
+        final UserModel userModel = adapter.getItem(position);
+        new AlertDialog.Builder(this).setMessage(String.format(getString(R.string.unfollow_dialog_message), userModel.getUsername()))
+          .setPositiveButton(getString(R.string.unfollow_dialog_yes), new DialogInterface.OnClickListener() {
+              @Override public void onClick(DialogInterface dialog, int which) {
+                  presenter.unfollowUser(userModel);
+              }
+          })
+          .setNegativeButton(getString(R.string.unfollow_dialog_no), null)
+          .create()
+          .show();
+    }
+
+    @OnItemClick(R.id.nicerslist_list)
+    public void openUserProfile(int position) {
+        UserModel user = adapter.getItem(position);
+        startActivityForResult(ProfileContainerActivity.getIntent(this, user.getIdUser()), 666);
     }
 }
