96840bea82234e7f50be22b75c2a9a456ed45bab
jmartinez1360 <mrjem1360@gmail.com>
2016-04-11 12:29:13 +0200

    Creación de la vista de Nicers
    Se crea la vista con los métodos que necesitará de forma segura

---
+++ app/src/main/java/com/shootr/mobile/ui/views/NicersView.java
@@ -0,0 +1,16 @@
+package com.shootr.mobile.ui.views;
+
+import com.shootr.mobile.ui.model.UserModel;
+import com.shootr.mobile.ui.views.base.LoadDataView;
+import java.util.List;
+
+public interface NicersView extends LoadDataView {
+
+    void renderNicers(List<UserModel> transform);
+
+    void showNicersList();
+
+    void hideProgressView();
+
+    void showProgressView();
+}
