792567996733284d05f429a848d72dade3cd1584
jmartinez1360 <mrjem1360@gmail.com>
2016-04-20 12:44:33 +0200

    Se inclueyn llamadas correctas a la APi
    Ahora ya se pueden añadir y remover contributors!!!

--- app/src/main/java/com/shootr/mobile/data/api/service/ContributorApiService.java
+++ app/src/main/java/com/shootr/mobile/data/api/service/ContributorApiService.java
@@ -4,18 +4,22 @@ import com.shootr.mobile.data.api.exception.ApiException;
 import com.shootr.mobile.data.entity.ContributorEntity;
 import java.io.IOException;
 import java.util.List;
+import retrofit.client.Response;
+import retrofit.http.DELETE;
 import retrofit.http.GET;
+import retrofit.http.PUT;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface ContributorApiService {
 
-    @GET("/contributor?includeEmbed=false") List<ContributorEntity> getContributors(@Query("idStream") String idShot)
+    @GET("/contributor?includeEmbed=false") List<ContributorEntity> getContributors(@Query("idStream") String idStream)
       throws ApiException, IOException;
 
     @GET("/contributor?includeEmbed=true") List<ContributorEntity> getContributorsWithUser(
-      @Query("idStream") String idShot) throws ApiException, IOException;
+      @Query("idStream") String idStream) throws ApiException, IOException;
 
-    void addContributor(String idStream, String idUser) throws ApiException, IOException;
+    @PUT("/stream/{idStream}/contributor/{idUser}") Response addContributor(@Path("idStream") String idStream, @Path("idUser") String idUser) throws ApiException, IOException;
 
-    void removeContributor(String idStream, String idUser) throws ApiException, IOException;
+    @DELETE("/stream/{idStream}/contributor/{idUser}") Response removeContributor(@Path("idStream") String idStream, @Path("idUser") String idUser) throws ApiException, IOException;
 }
