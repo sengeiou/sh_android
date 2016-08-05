78520696f0c96d20b211247701f75c9016176a83
jmartinez360 <mrjem1360@gmail.com>
2016-08-05 12:37:30 +0200

    se eliminan las animaciones del timeline (#152)

--- app/src/main/java/com/shootr/mobile/ui/adapters/ShotsTimelineAdapter.java
+++ app/src/main/java/com/shootr/mobile/ui/adapters/ShotsTimelineAdapter.java
@@ -1,14 +1,10 @@
 package com.shootr.mobile.ui.adapters;
 
 import android.content.Context;
-import android.graphics.Point;
 import android.support.v7.widget.RecyclerView;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.view.animation.DecelerateInterpolator;
 import com.shootr.mobile.R;
 import com.shootr.mobile.ui.adapters.holders.LoadingViewHolder;
 import com.shootr.mobile.ui.adapters.holders.ShotTimelineViewHolder;
@@ -30,7 +26,6 @@ import java.util.List;
 
 public class ShotsTimelineAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-  private static final int ANIMATED_ITEMS_COUNT = 10;
   private final int VIEW_TYPE_ITEM = 0;
   private final ImageLoader imageLoader;
   private final OnAvatarClickListener avatarClickListener;
@@ -48,9 +43,6 @@ public class ShotsTimelineAdapter extends RecyclerView.Adapter<RecyclerView.View
   private final Context context;
 
   private List<ShotModel> shots;
-
-  private int screenHeight;
-  private int lastAnimatedPosition = -1;
 
   public ShotsTimelineAdapter(Context context, ImageLoader imageLoader, AndroidTimeUtils timeUtils,
       OnAvatarClickListener avatarClickListener, OnVideoClickListener videoClickListener,
@@ -95,40 +87,12 @@ public class ShotsTimelineAdapter extends RecyclerView.Adapter<RecyclerView.View
   }
 
   @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-    runEnterAnimation(holder.itemView, position);
     if (holder instanceof ShotTimelineViewHolder) {
       ((ShotTimelineViewHolder) holder).render(shots.get(position), shotClickListener,
           onShotLongClick, onLongClickListener, onTouchListener, onImageClickListener);
     } else if (holder instanceof LoadingViewHolder) {
       ((LoadingViewHolder) holder).render();
     }
-  }
-
-  private void runEnterAnimation(View view, int position) {
-    if (position >= ANIMATED_ITEMS_COUNT) {
-      return;
-    }
-
-    if (position > lastAnimatedPosition) {
-      lastAnimatedPosition = position;
-      view.setTranslationY(getScreenHeight(context));
-      view.animate()
-          .translationY(0)
-          .setInterpolator(new DecelerateInterpolator(3.f))
-          .setDuration(600)
-          .start();
-    }
-  }
-
-  private int getScreenHeight(Context c) {
-    if (screenHeight == 0) {
-      WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
-      Display display = wm.getDefaultDisplay();
-      Point size = new Point();
-      display.getSize(size);
-      screenHeight = size.y;
-    }
-    return screenHeight;
   }
 
   @Override public int getItemCount() {
