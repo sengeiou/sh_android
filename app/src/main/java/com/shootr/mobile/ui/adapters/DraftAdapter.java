a71fab5e3fa9e34a8e0bed16c4da4069ad26db5f
Arturo <artjimlop@users.noreply.github.com>
2016-08-29 17:47:52 +0200

    Corregido el crash de Truss (#180)
    * Corregido el crash de Truss
    
    Corresponde al crash https://fabric.io/fav24/android/apps/com.shootr.mobile/issues/572f0e62ffcdc04250857876
    
    Resulta que el Stream Title en algunos casos podía ser nulo. ¿qué casos? yo que se, no soy cientifico
    
    * Reformat

--- app/src/main/java/com/shootr/mobile/ui/adapters/DraftAdapter.java
+++ app/src/main/java/com/shootr/mobile/ui/adapters/DraftAdapter.java
@@ -22,164 +22,179 @@ import java.util.List;
 
 public class DraftAdapter extends RecyclerView.Adapter<DraftAdapter.DraftViewHolder> {
 
-    public static final int NONE_EXPANDED_POSITION = -1;
-    public static final DraftViewHolder NONE_EXPANDED_ITEM = null;
-    private final ImageLoader imageLoader;
-    private final DraftActionListener draftActionListener;
+  public static final int NONE_EXPANDED_POSITION = -1;
+  public static final DraftViewHolder NONE_EXPANDED_ITEM = null;
+  private final ImageLoader imageLoader;
+  private final DraftActionListener draftActionListener;
 
-    private List<DraftModel> drafts = new ArrayList<>();
-    private DraftViewHolder currentExpandedItem;
-    private int currentExpandedItemPosition = -1;
+  private List<DraftModel> drafts = new ArrayList<>();
+  private DraftViewHolder currentExpandedItem;
+  private int currentExpandedItemPosition = -1;
 
-    public DraftAdapter(ImageLoader imageLoader, DraftActionListener draftActionListener) {
-        this.imageLoader = imageLoader;
-        this.draftActionListener = draftActionListener;
+  public DraftAdapter(ImageLoader imageLoader, DraftActionListener draftActionListener) {
+    this.imageLoader = imageLoader;
+    this.draftActionListener = draftActionListener;
+  }
+
+  public void setDrafts(List<DraftModel> drafts) {
+    this.drafts = drafts;
+    notifyDataSetChanged();
+  }
+
+  @Override public DraftViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View itemView = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.item_list_shot_draft, parent, false);
+    return new DraftViewHolder(itemView);
+  }
+
+  @Override public int getItemCount() {
+    return drafts.size();
+  }
+
+  @Override public void onBindViewHolder(DraftViewHolder holder, int position) {
+    DraftModel draftModel = drafts.get(position);
+    ShotModel shotModel = draftModel.getShotModel();
+
+    holder.name.setText(shotModel.getUsername());
+    holder.text.setText(getShotCommentWithStream(shotModel, holder));
+    holder.text.addLinks();
+    imageLoader.loadProfilePhoto(shotModel.getPhoto(), holder.avatar);
+    bindShotImageIfPresent(holder, draftModel);
+    if (isExpandedLocked(position)) {
+      currentExpandedItemPosition = position;
+      holder.draftItemView.setClickable(false);
+    } else {
+      holder.draftItemView.setClickable(true);
     }
-
-    public void setDrafts(List<DraftModel> drafts) {
-        this.drafts = drafts;
-        notifyDataSetChanged();
+    if (currentExpandedItemPosition == position) {
+      currentExpandedItem = holder;
+      holder.draftItemView.expand(false);
+    } else {
+      holder.draftItemView.collapse(false);
     }
+  }
 
-    @Override public DraftViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_shot_draft, parent, false);
-        return new DraftViewHolder(itemView);
+  private CharSequence getShotCommentWithStream(ShotModel shot, DraftViewHolder holder) {
+    if (shot.getComment() == null) {
+      return new Truss().pushSpan(new TextAppearanceSpan(holder.draftItemView.getContext(),
+          R.style.InlineDescriptionAppearance)).append(shot.getStreamTitle()).popSpan().build();
+    } else {
+      return setupShotCommentAppearance(shot, holder);
     }
+  }
 
-    @Override public int getItemCount() {
-        return drafts.size();
+  private CharSequence setupShotCommentAppearance(ShotModel shot, DraftViewHolder holder) {
+    if (shot.getStreamTitle() != null) {
+      return new Truss().append(shot.getComment())
+          .pushSpan(new TextAppearanceSpan(holder.draftItemView.getContext(),
+              R.style.InlineDescriptionAppearance))
+          .append(" ")
+          .append(shot.getStreamTitle())
+          .popSpan()
+          .build();
+    } else {
+      return new Truss().append(shot.getComment())
+          .pushSpan(new TextAppearanceSpan(holder.draftItemView.getContext(),
+              R.style.InlineDescriptionAppearance))
+          .popSpan()
+          .build();
     }
+  }
 
-    @Override public void onBindViewHolder(DraftViewHolder holder, int position) {
-        DraftModel draftModel = drafts.get(position);
-        ShotModel shotModel = draftModel.getShotModel();
+  private boolean isExpandedLocked(int position) {
+    return getItemCount() <= 1;
+  }
 
-        holder.name.setText(shotModel.getUsername());
-        holder.text.setText(getShotCommentWithStream(shotModel, holder));
-        holder.text.addLinks();
-        imageLoader.loadProfilePhoto(shotModel.getPhoto(), holder.avatar);
-        bindShotImageIfPresent(holder, draftModel);
-        if (isExpandedLocked(position)) {
-            currentExpandedItemPosition = position;
-            holder.draftItemView.setClickable(false);
-        } else {
-            holder.draftItemView.setClickable(true);
+  private void bindShotImageIfPresent(DraftViewHolder holder, DraftModel draftModel) {
+    ShotModel shotModel = draftModel.getShotModel();
+    if (shotModel.getImage() != null && shotModel.getImage().getImageUrl() != null) {
+      imageLoader.loadTimelineImage(shotModel.getImage().getImageUrl(), holder.image);
+      holder.image.setVisibility(View.VISIBLE);
+    } else if (draftModel.getImageFile() != null) {
+      imageLoader.load(draftModel.getImageFile(), holder.image);
+      holder.image.setVisibility(View.VISIBLE);
+    } else {
+      holder.image.setVisibility(View.GONE);
+    }
+  }
+
+  private void toggleItem(DraftViewHolder holder) {
+    if (currentExpandedItemPosition == holder.getPosition()) {
+      collapseItem(holder);
+    } else {
+      expandItem(holder);
+    }
+  }
+
+  private void collapseItem(DraftViewHolder holder) {
+    holder.draftItemView.collapse(true);
+    boolean isItemAtExpandedPosition = currentExpandedItemPosition == holder.getPosition();
+    if (isItemAtExpandedPosition) {
+      currentExpandedItemPosition = NONE_EXPANDED_POSITION;
+    }
+    boolean isCurrentExpandedItem = currentExpandedItem == holder;
+    if (isCurrentExpandedItem) {
+      currentExpandedItem = NONE_EXPANDED_ITEM;
+    }
+  }
+
+  private void expandItem(DraftViewHolder holder) {
+    collapseCurrentExpandedItem();
+    holder.draftItemView.expand(true);
+    currentExpandedItem = holder;
+    currentExpandedItemPosition = holder.getPosition();
+  }
+
+  private void collapseCurrentExpandedItem() {
+    if (currentExpandedItem != null) {
+      boolean isExpandedItemVisible =
+          currentExpandedItemPosition == currentExpandedItem.getPosition();
+      if (isExpandedItemVisible) {
+        currentExpandedItem.draftItemView.collapse(true);
+      }
+    }
+    currentExpandedItemPosition = NONE_EXPANDED_POSITION;
+  }
+
+  class DraftViewHolder extends RecyclerView.ViewHolder {
+
+    @Bind(R.id.shot_avatar) ImageView avatar;
+    @Bind(R.id.shot_user_name) TextView name;
+    @Bind(R.id.shot_text) ClickableTextView text;
+    @Bind(R.id.shot_image) ImageView image;
+    @Bind(R.id.shot_draft_buttons) View draftButtons;
+    DraftItemView draftItemView;
+
+    public DraftViewHolder(View itemView) {
+      super(itemView);
+      draftItemView = (DraftItemView) itemView;
+      ButterKnife.bind(this, itemView);
+      itemView.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View v) {
+          if (!isExpandedLocked(getPosition())) {
+            toggleItem(DraftViewHolder.this);
+          }
         }
-        if (currentExpandedItemPosition == position) {
-            currentExpandedItem = holder;
-            holder.draftItemView.expand(false);
-        } else {
-            holder.draftItemView.collapse(false);
-        }
+      });
     }
 
-    private CharSequence getShotCommentWithStream(ShotModel shot, DraftViewHolder holder) {
-        if (shot.getComment() == null) {
-            return new Truss().pushSpan(new TextAppearanceSpan(holder.draftItemView.getContext(),
-              R.style.InlineDescriptionAppearance)).append(shot.getStreamTitle()).popSpan().build();
-        } else {
-            return new Truss().append(shot.getComment())
-              .pushSpan(new TextAppearanceSpan(holder.draftItemView.getContext(), R.style.InlineDescriptionAppearance))
-              .append(" ")
-              .append(shot.getStreamTitle())
-              .popSpan()
-              .build();
-        }
+    @OnClick(R.id.shot_draft_shoot) public void shoot() {
+      draftActionListener.onShootDraft(getDraftForThisPosition());
     }
 
-    private boolean isExpandedLocked(int position) {
-        return getItemCount() <= 1;
+    @OnClick(R.id.shot_draft_delete) public void delete() {
+      draftActionListener.onDeleteDraft(getDraftForThisPosition());
     }
 
-    private void bindShotImageIfPresent(DraftViewHolder holder, DraftModel draftModel) {
-        ShotModel shotModel = draftModel.getShotModel();
-        if (shotModel.getImage() != null && shotModel.getImage().getImageUrl() != null) {
-            imageLoader.loadTimelineImage(shotModel.getImage().getImageUrl(), holder.image);
-            holder.image.setVisibility(View.VISIBLE);
-        } else if (draftModel.getImageFile() != null) {
-            imageLoader.load(draftModel.getImageFile(), holder.image);
-            holder.image.setVisibility(View.VISIBLE);
-        } else {
-            holder.image.setVisibility(View.GONE);
-        }
+    private DraftModel getDraftForThisPosition() {
+      return drafts.get(this.getPosition());
     }
+  }
 
-    private void toggleItem(DraftViewHolder holder) {
-        if (currentExpandedItemPosition == holder.getPosition()) {
-            collapseItem(holder);
-        } else {
-            expandItem(holder);
-        }
-    }
+  public interface DraftActionListener {
 
-    private void collapseItem(DraftViewHolder holder) {
-        holder.draftItemView.collapse(true);
-        boolean isItemAtExpandedPosition = currentExpandedItemPosition == holder.getPosition();
-        if (isItemAtExpandedPosition) {
-            currentExpandedItemPosition = NONE_EXPANDED_POSITION;
-        }
-        boolean isCurrentExpandedItem = currentExpandedItem == holder;
-        if (isCurrentExpandedItem) {
-            currentExpandedItem = NONE_EXPANDED_ITEM;
-        }
-    }
+    void onShootDraft(DraftModel draftModel);
 
-    private void expandItem(DraftViewHolder holder) {
-        collapseCurrentExpandedItem();
-        holder.draftItemView.expand(true);
-        currentExpandedItem = holder;
-        currentExpandedItemPosition = holder.getPosition();
-    }
-
-    private void collapseCurrentExpandedItem() {
-        if (currentExpandedItem != null) {
-            boolean isExpandedItemVisible = currentExpandedItemPosition == currentExpandedItem.getPosition();
-            if (isExpandedItemVisible) {
-                currentExpandedItem.draftItemView.collapse(true);
-            }
-        }
-        currentExpandedItemPosition = NONE_EXPANDED_POSITION;
-    }
-
-    class DraftViewHolder extends RecyclerView.ViewHolder {
-
-        @Bind(R.id.shot_avatar) ImageView avatar;
-        @Bind(R.id.shot_user_name) TextView name;
-        @Bind(R.id.shot_text) ClickableTextView text;
-        @Bind(R.id.shot_image) ImageView image;
-        @Bind(R.id.shot_draft_buttons) View draftButtons;
-        DraftItemView draftItemView;
-
-        public DraftViewHolder(View itemView) {
-            super(itemView);
-            draftItemView = (DraftItemView) itemView;
-            ButterKnife.bind(this, itemView);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override public void onClick(View v) {
-                    if (!isExpandedLocked(getPosition())) {
-                        toggleItem(DraftViewHolder.this);
-                    }
-                }
-            });
-        }
-
-        @OnClick(R.id.shot_draft_shoot) public void shoot() {
-            draftActionListener.onShootDraft(getDraftForThisPosition());
-        }
-
-        @OnClick(R.id.shot_draft_delete) public void delete() {
-            draftActionListener.onDeleteDraft(getDraftForThisPosition());
-        }
-
-        private DraftModel getDraftForThisPosition() {
-            return drafts.get(this.getPosition());
-        }
-    }
-
-    public interface DraftActionListener {
-
-        void onShootDraft(DraftModel draftModel);
-
-        void onDeleteDraft(DraftModel draftModel);
-    }
+    void onDeleteDraft(DraftModel draftModel);
+  }
 }
