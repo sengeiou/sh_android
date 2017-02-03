6e55634b7f8dea723a9a6ce0874d1491eef273be
jmartinez360 <mrjem1360@gmail.com>
2017-02-03 14:25:59 +0100

    Se arregla el checkstyle que estaba petando en develop (#424)

--- app/src/main/java/com/shootr/mobile/ui/presenter/StreamTimelinePresenter.java
+++ app/src/main/java/com/shootr/mobile/ui/presenter/StreamTimelinePresenter.java
@@ -281,7 +281,7 @@ public class StreamTimelinePresenter implements Presenter, ShotSent.Receiver {
           }
         });
   }
-  
+
   private void manageCallImportantShots() {
     if (filterActivated
         && !calledForImportant) {
