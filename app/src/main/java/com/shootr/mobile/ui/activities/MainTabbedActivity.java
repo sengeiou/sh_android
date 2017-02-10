1bd9937d2a3e9256a892d8647b0cbdbfde3d3ea4
Jorge <mrjem1360@gmail.com>
2017-02-10 15:14:14 +0100

    Se hacen los cambios necesarios para que la barra quede guay

--- app/src/main/java/com/shootr/mobile/ui/activities/MainTabbedActivity.java
+++ app/src/main/java/com/shootr/mobile/ui/activities/MainTabbedActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
