64d2a1c92df3dc3bcae0a0246e3dbc252e36e3b6
vsaldanya <victor.saldanya@fav24.com>
2017-02-15 15:43:21 +0100

    Se controla un null Pointer exception gráfico en reset password (#426)
    Esto es muy extraño, demasiado, pero así cerramos crashlytics

--- app/src/main/java/com/shootr/mobile/ui/activities/registro/ResetPasswordActivity.java
+++ app/src/main/java/com/shootr/mobile/ui/activities/registro/ResetPasswordActivity.java
@@ -28,187 +28,193 @@ import javax.inject.Inject;
 
 public class ResetPasswordActivity extends BaseToolbarDecoratedActivity {
 
-    @BindView(R.id.reset_password_request_view) View resetPasswordRequestLayout;
-    @BindView(R.id.reset_password_confirmation_view) View resetPasswordConfirmationLayout;
-    @BindString(R.string.analytics_screen_forgot_password) String analyticsScreenForgotPassword;
+  @BindView(R.id.reset_password_request_view) View resetPasswordRequestLayout;
+  @BindView(R.id.reset_password_confirmation_view) View resetPasswordConfirmationLayout;
+  @BindString(R.string.analytics_screen_forgot_password) String analyticsScreenForgotPassword;
 
-    @Inject ImageLoader imageLoader;
-    @Inject ResetPasswordRequestPresenter resetPasswordRequestPresenter;
-    @Inject ResetPasswordConfirmationPresenter resetPasswordConfirmationPresenter;
-    @Inject FeedbackMessage feedbackMessage;
-    @Inject AnalyticsTool analyticsTool;
+  @Inject ImageLoader imageLoader;
+  @Inject ResetPasswordRequestPresenter resetPasswordRequestPresenter;
+  @Inject ResetPasswordConfirmationPresenter resetPasswordConfirmationPresenter;
+  @Inject FeedbackMessage feedbackMessage;
+  @Inject AnalyticsTool analyticsTool;
 
-    private ResetPasswordRequestView resetPasswordRequestView;
-    private ResetPasswordConfirmationView resetPasswordConfirmationView;
+  private ResetPasswordRequestView resetPasswordRequestView;
+  private ResetPasswordConfirmationView resetPasswordConfirmationView;
 
-    //region Initialization
-    @Override protected int getLayoutResource() {
-        return R.layout.activity_reset_password;
-    }
+  //region Initialization
+  @Override protected int getLayoutResource() {
+    return R.layout.activity_reset_password;
+  }
 
-    @Override protected void initializeViews(Bundle savedInstanceState) {
-        ButterKnife.bind(this);
-        analyticsTool.analyticsStart(getBaseContext(), analyticsScreenForgotPassword);
-        setupViewImplementations();
-        resetPasswordRequestLayout.setVisibility(View.VISIBLE);
-        resetPasswordConfirmationLayout.setVisibility(View.GONE);
-    }
+  @Override protected void initializeViews(Bundle savedInstanceState) {
+    ButterKnife.bind(this);
+    analyticsTool.analyticsStart(getBaseContext(), analyticsScreenForgotPassword);
+    setupViewImplementations();
+    resetPasswordRequestLayout.setVisibility(View.VISIBLE);
+    resetPasswordConfirmationLayout.setVisibility(View.GONE);
+  }
 
-    private void setupViewImplementations() {
-        resetPasswordRequestView = new InnerResetPasswordRequestView();
-        ButterKnife.bind(resetPasswordRequestView, resetPasswordRequestLayout);
+  private void setupViewImplementations() {
+    resetPasswordRequestView = new InnerResetPasswordRequestView();
+    ButterKnife.bind(resetPasswordRequestView, resetPasswordRequestLayout);
 
-        resetPasswordConfirmationView = new InnerResetPasswordConfirmationView();
-        ButterKnife.bind(resetPasswordConfirmationView, resetPasswordConfirmationLayout);
-    }
+    resetPasswordConfirmationView = new InnerResetPasswordConfirmationView();
+    ButterKnife.bind(resetPasswordConfirmationView, resetPasswordConfirmationLayout);
+  }
 
-    @Override protected void initializePresenter() {
-        resetPasswordRequestPresenter.initialize(resetPasswordRequestView);
-    }
+  @Override protected void initializePresenter() {
+    resetPasswordRequestPresenter.initialize(resetPasswordRequestView);
+  }
 
-    @Override protected void setupToolbar(ToolbarDecorator toolbarDecorator) {
+  @Override protected void setupToolbar(ToolbarDecorator toolbarDecorator) {
         /* no-op */
+  }
+
+  @Override protected boolean requiresUserLogin() {
+    return false;
+  }
+  //endregion
+
+  //region Activity lifecycle
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == android.R.id.home) {
+      finish();
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+  //endregion
+
+  @OnTextChanged(R.id.reset_password_username_email)
+  public void onInputTextChanged(CharSequence usernameOrEmail) {
+    resetPasswordRequestPresenter.onUsernameOrEmailChanged(usernameOrEmail.toString());
+  }
+
+  @OnClick(R.id.reset_password_next) public void onNextClicked() {
+    resetPasswordRequestPresenter.next();
+  }
+
+  @OnClick(R.id.reset_password_confirm) public void onConfirmClicked() {
+    resetPasswordConfirmationPresenter.confirm();
+  }
+
+  @OnClick(R.id.reset_password_done) public void onDoneClicked() {
+    resetPasswordConfirmationPresenter.done();
+  }
+
+  private void transitionToConfirmationView(ForgotPasswordUserModel forgotPasswordUserModel) {
+    resetPasswordRequestLayout.setVisibility(View.GONE);
+    resetPasswordConfirmationLayout.setVisibility(View.VISIBLE);
+    resetPasswordConfirmationPresenter.initialize(resetPasswordConfirmationView,
+        forgotPasswordUserModel);
+  }
+
+  private void closeScreenAndLaunchLogin() {
+    finish();
+  }
+
+  public class InnerResetPasswordRequestView implements ResetPasswordRequestView {
+
+    @BindView(R.id.reset_password_username_email) EditText usernameOrEmailInput;
+    @BindView(R.id.reset_password_next) View nextButton;
+    @BindView(R.id.reset_password_progress) View progressView;
+    @BindView(R.id.reset_password_error_message) TextView resetPasswordError;
+
+    @Override public void enableNextButton() {
+      nextButton.setEnabled(true);
     }
 
-    @Override protected boolean requiresUserLogin() {
-        return false;
-    }
-    //endregion
-
-    //region Activity lifecycle
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    //endregion
-
-    @OnTextChanged(R.id.reset_password_username_email) public void onInputTextChanged(CharSequence usernameOrEmail) {
-        resetPasswordRequestPresenter.onUsernameOrEmailChanged(usernameOrEmail.toString());
+    @Override public void disableNextButton() {
+      nextButton.setEnabled(false);
     }
 
-    @OnClick(R.id.reset_password_next) public void onNextClicked() {
-        resetPasswordRequestPresenter.next();
+    @Override public void navigateToResetPasswordConfirmation(
+        ForgotPasswordUserModel forgotPasswordUserModel) {
+      transitionToConfirmationView(forgotPasswordUserModel);
     }
 
-    @OnClick(R.id.reset_password_confirm) public void onConfirmClicked() {
-        resetPasswordConfirmationPresenter.confirm();
+    @Override public void showResetPasswordError() {
+      resetPasswordError.setVisibility(View.VISIBLE);
     }
 
-    @OnClick(R.id.reset_password_done) public void onDoneClicked() {
-        resetPasswordConfirmationPresenter.done();
+    @Override public void hideKeyboard() {
+      InputMethodManager imm = (InputMethodManager) ResetPasswordActivity.this.getSystemService(
+          Context.INPUT_METHOD_SERVICE);
+      imm.hideSoftInputFromWindow(usernameOrEmailInput.getWindowToken(), 0);
     }
 
-    private void transitionToConfirmationView(ForgotPasswordUserModel forgotPasswordUserModel) {
-        resetPasswordRequestLayout.setVisibility(View.GONE);
-        resetPasswordConfirmationLayout.setVisibility(View.VISIBLE);
-        resetPasswordConfirmationPresenter.initialize(resetPasswordConfirmationView, forgotPasswordUserModel);
+    @Override public String getUsernameOrEmail() {
+      return usernameOrEmailInput.getText().toString();
     }
 
-    private void closeScreenAndLaunchLogin() {
-        finish();
+    @Override public void showLoading() {
+      progressView.setVisibility(View.VISIBLE);
     }
 
-    public class InnerResetPasswordRequestView implements ResetPasswordRequestView {
-
-        @BindView(R.id.reset_password_username_email) EditText usernameOrEmailInput;
-        @BindView(R.id.reset_password_next) View nextButton;
-        @BindView(R.id.reset_password_progress) View progressView;
-        @BindView(R.id.reset_password_error_message) TextView resetPasswordError;
-
-        @Override public void enableNextButton() {
-            nextButton.setEnabled(true);
-        }
-
-        @Override public void disableNextButton() {
-            nextButton.setEnabled(false);
-        }
-
-        @Override public void navigateToResetPasswordConfirmation(ForgotPasswordUserModel forgotPasswordUserModel) {
-            transitionToConfirmationView(forgotPasswordUserModel);
-        }
-
-        @Override public void showResetPasswordError() {
-            resetPasswordError.setVisibility(View.VISIBLE);
-        }
-
-        @Override public void hideKeyboard() {
-            InputMethodManager imm =
-              (InputMethodManager) ResetPasswordActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(usernameOrEmailInput.getWindowToken(), 0);
-        }
-
-        @Override public String getUsernameOrEmail() {
-            return usernameOrEmailInput.getText().toString();
-        }
-
-        @Override public void showLoading() {
-            progressView.setVisibility(View.VISIBLE);
-        }
-
-        @Override public void hideLoading() {
-            progressView.setVisibility(View.GONE);
-        }
-
-        @Override public void showError(String message) {
-            feedbackMessage.show(getView(), message);
-        }
+    @Override public void hideLoading() {
+      progressView.setVisibility(View.GONE);
     }
 
-    public class InnerResetPasswordConfirmationView implements ResetPasswordConfirmationView {
+    @Override public void showError(String message) {
+      feedbackMessage.show(getView(), message);
+    }
+  }
 
-        @BindView(R.id.reset_password_avatar) ImageView avatar;
-        @BindView(R.id.reset_password_username) TextView usernameText;
-        @BindView(R.id.reset_password_email_confirmation_message) TextView confirmationMessage;
-        @BindView(R.id.reset_password_confirm) View confirmButton;
-        @BindView(R.id.reset_password_done) View doneButton;
-        @BindView(R.id.reset_password_progress) View progressView;
+  public class InnerResetPasswordConfirmationView implements ResetPasswordConfirmationView {
 
-        @Override public void showAvatar(String avatarUrl) {
-            imageLoader.loadProfilePhoto(avatarUrl, avatar);
-        }
+    @BindView(R.id.reset_password_avatar) ImageView avatar;
+    @BindView(R.id.reset_password_username) TextView usernameText;
+    @BindView(R.id.reset_password_email_confirmation_message) TextView confirmationMessage;
+    @BindView(R.id.reset_password_confirm) View confirmButton;
+    @BindView(R.id.reset_password_done) View doneButton;
+    @BindView(R.id.reset_password_progress) View progressView;
 
-        @Override public void showUsername(String username) {
-            usernameText.setText(username);
-        }
-
-        @Override public void hideConfirmationButton() {
-            confirmButton.setVisibility(View.GONE);
-        }
-
-        @Override public void showConfirmationButton() {
-            confirmButton.setVisibility(View.VISIBLE);
-        }
-
-        @Override public void showDoneButton() {
-            doneButton.setVisibility(View.VISIBLE);
-        }
-
-        @Override public void showPostConfirmationMessage(String email) {
-            confirmationMessage.setVisibility(View.VISIBLE);
-            confirmationMessage.setText(getString(R.string.reset_password_confirmation_message_email_pattern, email));
-        }
-
-        @Override public void navigateToLogin() {
-            closeScreenAndLaunchLogin();
-        }
-
-        @Override public void showLoading() {
-            progressView.setVisibility(View.VISIBLE);
-        }
-
-        @Override public void hideLoading() {
-            progressView.setVisibility(View.GONE);
-        }
-
-        @Override public void showError(String message) {
-            feedbackMessage.show(getView(), message);
-        }
+    @Override public void showAvatar(String avatarUrl) {
+      imageLoader.loadProfilePhoto(avatarUrl, avatar);
     }
 
-    @Override protected void onPause() {
-        super.onPause();
+    @Override public void showUsername(String username) {
+      usernameText.setText(username);
     }
+
+    @Override public void hideConfirmationButton() {
+      confirmButton.setVisibility(View.GONE);
+    }
+
+    @Override public void showConfirmationButton() {
+      confirmButton.setVisibility(View.VISIBLE);
+    }
+
+    @Override public void showDoneButton() {
+      doneButton.setVisibility(View.VISIBLE);
+    }
+
+    @Override public void showPostConfirmationMessage(String email) {
+      confirmationMessage.setVisibility(View.VISIBLE);
+      confirmationMessage.setText(
+          getString(R.string.reset_password_confirmation_message_email_pattern, email));
+    }
+
+    @Override public void navigateToLogin() {
+      closeScreenAndLaunchLogin();
+    }
+
+    @Override public void showLoading() {
+      if (progressView != null) {
+        progressView.setVisibility(View.VISIBLE);
+      }
+    }
+
+    @Override public void hideLoading() {
+      progressView.setVisibility(View.GONE);
+    }
+
+    @Override public void showError(String message) {
+      feedbackMessage.show(getView(), message);
+    }
+  }
+
+  @Override protected void onPause() {
+    super.onPause();
+  }
 }
