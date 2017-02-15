df6550a0e27ef1e77524a2943bfb9289af7bd5dd
Jorge <mrjem1360@gmail.com>
2017-02-15 12:47:39 +0100

    QUick return para el bottomBar
    APlicamos este comportamiento para poder ocultar el bottomBr al hacer scroll y obtener más espacio en las pantallas

---
+++ app/src/main/java/com/shootr/mobile/ui/widgets/ConnectControllerBehavior.java
@@ -0,0 +1,39 @@
+package com.shootr.mobile.ui.widgets;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import com.roughike.bottombar.BottomBar;
+import com.shootr.mobile.R;
+
+public class ConnectControllerBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
+
+  private int textIndicatorHeight = 48;
+  private Context context;
+
+  public ConnectControllerBehavior() {
+  }
+
+  public ConnectControllerBehavior(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    this.context = context;
+  }
+
+  @Override
+  public boolean layoutDependsOn(CoordinatorLayout parent, LinearLayout child, View dependency) {
+    return dependency.getId() == R.id.bottomBar;
+  }
+
+  @Override
+  public boolean onDependentViewChanged(CoordinatorLayout parent, LinearLayout linearLayout, View dependency) {
+    if (dependency.getId() == R.id.bottomBar) {
+      linearLayout.setTranslationY(-dependency.getHeight() + dependency.getTranslationY());
+    }
+    return true;
+  }
+}
+
