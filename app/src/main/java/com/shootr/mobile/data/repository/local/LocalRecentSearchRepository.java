e4f8d7736cf2e17ae0eed4a07bed26aa82129f8a
Jorge <mrjem1360@gmail.com>
2017-04-18 11:51:50 +0200

    Se ven reflejados los follows y unfollows en el recent
    Cuando nos mostraba el historial no se estaba mostrando si seguia o no a determinado user, por lo que ahora mostramos esta información con un pequeño retoque al repositorio

--- app/src/main/java/com/shootr/mobile/data/repository/local/LocalRecentSearchRepository.java
+++ app/src/main/java/com/shootr/mobile/data/repository/local/LocalRecentSearchRepository.java
@@ -1,31 +1,38 @@
 package com.shootr.mobile.data.repository.local;
 
 import com.shootr.mobile.data.entity.RecentSearchEntity;
-import com.shootr.mobile.data.mapper.RecentSearchEntityMapper;
+import com.shootr.mobile.data.entity.UserEntity;
 import com.shootr.mobile.data.mapper.StreamEntityMapper;
 import com.shootr.mobile.data.mapper.UserEntityMapper;
 import com.shootr.mobile.data.repository.datasource.stream.RecentSearchDataSource;
+import com.shootr.mobile.data.repository.datasource.user.UserDataSource;
 import com.shootr.mobile.domain.model.Searchable;
+import com.shootr.mobile.domain.model.SearchableType;
 import com.shootr.mobile.domain.model.stream.Stream;
 import com.shootr.mobile.domain.model.user.User;
+import com.shootr.mobile.domain.repository.Local;
+import com.shootr.mobile.domain.repository.SessionRepository;
 import com.shootr.mobile.domain.repository.stream.RecentSearchRepository;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 
 public class LocalRecentSearchRepository implements RecentSearchRepository {
 
   private final RecentSearchDataSource localRecentSearchDataSource;
-  private final RecentSearchEntityMapper recentSearchEntityMapper;
   private final StreamEntityMapper streamEntityMapper;
   private final UserEntityMapper userEntityMapper;
+  private final UserDataSource localUserDataSource;
+  private final SessionRepository sessionRepository;
 
   @Inject public LocalRecentSearchRepository(RecentSearchDataSource localRecentSearchDataSource,
-      RecentSearchEntityMapper recentSearchEntityMapper, StreamEntityMapper streamEntityMapper,
-      UserEntityMapper userEntityMapper) {
+      StreamEntityMapper streamEntityMapper, UserEntityMapper userEntityMapper,
+      @Local UserDataSource localUserDataSource, SessionRepository sessionRepository) {
     this.localRecentSearchDataSource = localRecentSearchDataSource;
-    this.recentSearchEntityMapper = recentSearchEntityMapper;
     this.streamEntityMapper = streamEntityMapper;
     this.userEntityMapper = userEntityMapper;
+    this.localUserDataSource = localUserDataSource;
+    this.sessionRepository = sessionRepository;
   }
 
   @Override public void putRecentStream(Stream stream, long currentTime) {
@@ -43,7 +50,33 @@ public class LocalRecentSearchRepository implements RecentSearchRepository {
   }
 
   @Override public List<Searchable> getDefaultSearch() {
-    List<RecentSearchEntity> recentStreamEntities = localRecentSearchDataSource.getRecentSearches();
-    return recentSearchEntityMapper.transform(recentStreamEntities);
+
+    List<Searchable> searchables = new ArrayList<>();
+    List<RecentSearchEntity> searchableEntities = localRecentSearchDataSource.getRecentSearches();
+    try {
+      for (RecentSearchEntity searchableEntity : searchableEntities) {
+        switch (searchableEntity.getSearchableType()) {
+          case SearchableType.STREAM:
+            searchables.add(streamEntityMapper.transform(searchableEntity.getStream()));
+            break;
+          case SearchableType.USER:
+            UserEntity userEntity = searchableEntity.getUser();
+            searchables.add(
+                userEntityMapper.transform(userEntity, sessionRepository.getCurrentUserId(),
+                    false, isFollowing(userEntity.getIdUser())));
+            break;
+          default:
+            break;
+        }
+      }
+    } catch (NullPointerException e) {
+      /* no-op */
+    }
+
+    return searchables;
+  }
+
+  private boolean isFollowing(String userId) {
+    return localUserDataSource.isFollowing(sessionRepository.getCurrentUserId(), userId);
   }
 }
