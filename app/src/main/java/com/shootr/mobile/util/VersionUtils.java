ca7be2047bee319b3d0d6c6f4c55e56f29062493
vsaldanya <victor.saldanya@fav24.com>
2017-04-24 09:52:55 +0200

    Se añade el número de versión de base de datos a la versión en support (#505)
    De esta manera siempre podemos ver qué versión de DB tiene un dispositivo

--- app/src/main/java/com/shootr/mobile/util/VersionUtils.java
+++ app/src/main/java/com/shootr/mobile/util/VersionUtils.java
@@ -8,28 +8,38 @@ import timber.log.Timber;
 
 public class VersionUtils {
 
-    private VersionUtils() {
-    }
+  private VersionUtils() {
+  }
 
-    public static int getVersionCode(Context context) {
-        try {
-            return getPackageInfo(context).versionCode;
-        } catch (PackageManager.NameNotFoundException e) {
-            Timber.e("Error obteniendo version code, devolviendo -1", e);
-            return -1;
-        }
+  public static int getVersionCode(Context context) {
+    try {
+      return getPackageInfo(context).versionCode;
+    } catch (PackageManager.NameNotFoundException e) {
+      Timber.e("Error obteniendo version code, devolviendo -1", e);
+      return -1;
     }
+  }
 
-    public static String getVersionName(Context context) {
-        try {
-            return getPackageInfo(context).versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            Timber.e("Error obteniendo version name, devolviendo 0.0", e);
-            return "0.0";
-        }
+  public static String getVersionName(Context context) {
+    try {
+      return getPackageInfo(context).versionName;
+    } catch (PackageManager.NameNotFoundException e) {
+      Timber.e("Error obteniendo version name, devolviendo 0.0", e);
+      return "0.0";
     }
+  }
 
-    private static PackageInfo getPackageInfo(Context context) throws PackageManager.NameNotFoundException {
-        return context.getPackageManager().getPackageInfo(BuildConfig.APPLICATION_ID, 0);
+  public static String getDBVersion(Context context) {
+    try {
+      return String.valueOf(BuildConfig.DATABASE_VERSION);
+    } catch (Exception e) {
+      Timber.e("Error obteniendo version name, devolviendo 0", e);
+      return "0";
     }
+  }
+
+  private static PackageInfo getPackageInfo(Context context)
+      throws PackageManager.NameNotFoundException {
+    return context.getPackageManager().getPackageInfo(BuildConfig.APPLICATION_ID, 0);
+  }
 }
