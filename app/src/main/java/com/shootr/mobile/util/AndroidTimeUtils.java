fe5f63fd6a695ab5f4dd14644eff171cd8481e0a
vsaldanya <victor.saldanya@fav24.com>
2017-06-08 18:24:07 +0200

    Se añade un método similar al existente adecuado al caso de poll
    Falta refactorizar e intentar convertirlo en un único método

--- app/src/main/java/com/shootr/mobile/util/AndroidTimeUtils.java
+++ app/src/main/java/com/shootr/mobile/util/AndroidTimeUtils.java
@@ -54,6 +54,35 @@ public class AndroidTimeUtils implements TimeUtils {
         return res.getString(R.string.now);
     }
 
+    public String getPollElapsedTime(Context context, long publishTime) {
+        long difference = getCurrentTime() - publishTime;
+
+        long days = TimeUnit.MILLISECONDS.toDays(difference);
+        Resources res = context.getResources();
+        if (days > 0) {
+            String time = res.getString(R.string.days);
+            return String.valueOf(days + time);
+        }
+
+        long hours = TimeUnit.MILLISECONDS.toHours(difference);
+        if (hours > 0) {
+            String time = res.getString(R.string.hours);
+            return String.valueOf(hours + time);
+        }
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
+        if (minutes > 0) {
+            String time = res.getString(R.string.minutes);
+            return String.valueOf(minutes + time);
+        }
+
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
+        if (seconds > 0) {
+            String time = res.getString(R.string.seconds);
+            return String.valueOf(seconds + time);
+        }
+        return res.getString(R.string.closed);
+    }
+
     public String getHourWithDate(long publishTime) {
         long difference = getCurrentTime() - publishTime;
         long days = TimeUnit.MILLISECONDS.toDays(difference);
