19ff8022250b4fa190749ebcd3c3bc92158459a5
vsaldanya <victor.saldanya@fav24.com>
2017-06-13 16:55:28 +0200

    Más cambios en los cálculos en caso de ser indefinida y nuevo literal

--- app/src/main/java/com/shootr/mobile/util/AndroidTimeUtils.java
+++ app/src/main/java/com/shootr/mobile/util/AndroidTimeUtils.java
@@ -58,49 +58,49 @@ public class AndroidTimeUtils implements TimeUtils {
     return res.getString(R.string.now);
   }
 
-  public String getPollElapsedTime(Context context, long timeToFinish) {
+  public String getPollElapsedTime(Context context, Long timeToFinish) {
 
     Resources res = context.getResources();
+    if(timeToFinish != null) {
+      long difference = timeToFinish - getCurrentTime();
+      long days = TimeUnit.MILLISECONDS.toDays(difference);
 
-    long difference = timeToFinish - getCurrentTime();
-    long days = TimeUnit.MILLISECONDS.toDays(difference);
+      if (days > 0) {
+        difference = difference - (MILISECONDS_ON_A_DAY * days);
+        long hours = TimeUnit.MILLISECONDS.toHours(difference);
+        String timeDays = res.getQuantityString(R.plurals.days_left, (int) days, (int) days);
+        String timeHours =
+            ((hours > 0) ? res.getQuantityString(R.plurals.hours_left, (int) hours, (int) hours) : "");
+        return res.getString(R.string.left, timeDays + timeHours);
+      }
 
-    if (days > 0) {
-      difference = difference - (MILISECONDS_ON_A_DAY * days);
       long hours = TimeUnit.MILLISECONDS.toHours(difference);
-      String timeDays = res.getQuantityString(R.plurals.days_left, (int) days, (int) days);
-      String timeHours =
-          ((hours > 0) ? res.getQuantityString(R.plurals.hours_left, (int) hours, (int) hours) : "");
-      return res.getString(R.string.left, timeDays + timeHours);
-    }
+      if (hours > 0) {
+        difference = difference - (MILISECONDS_ON_A_HOUR * hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
+        String timeHours = res.getQuantityString(R.plurals.hours_left, (int) hours, (int) hours);
+        String timeMinutes =
+            ((minutes > 0) ? res.getQuantityString(R.plurals.minutes_left, (int) minutes, (int) minutes) : "");
+        return res.getString(R.string.left, timeHours + timeMinutes);
+      }
 
-    long hours = TimeUnit.MILLISECONDS.toHours(difference);
-    if (hours > 0) {
-      difference = difference - (MILISECONDS_ON_A_HOUR * hours);
       long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
-      String timeHours = res.getQuantityString(R.plurals.hours_left, (int) hours, (int) hours);
-      String timeMinutes =
-          ((minutes > 0) ? res.getQuantityString(R.plurals.minutes_left, (int) minutes, (int) minutes) : "");
-      return res.getString(R.string.left, timeHours + timeMinutes);
-    }
+      if (minutes > 0) {
+        difference = difference - (MILISECONDS_ON_A_MINUTE * minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
+        String timeMinutes = res.getQuantityString(R.plurals.minutes_left, (int) minutes, (int) minutes);
+        String timeSeconds =
+            ((seconds > 0) ? res.getQuantityString(R.plurals.seconds_left, (int) seconds, (int) seconds) : "");
+        return res.getString(R.string.left, timeMinutes + timeSeconds);
+      }
 
-    long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
-    if (minutes > 0) {
-      difference = difference - (MILISECONDS_ON_A_MINUTE * minutes);
       long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
-      String timeMinutes = res.getQuantityString(R.plurals.minutes_left, (int) minutes, (int) minutes);
-      String timeSeconds =
-          ((seconds > 0) ? res.getQuantityString(R.plurals.seconds_left, (int) seconds, (int) seconds) : "");
-      return res.getString(R.string.left, timeMinutes + timeSeconds);
+      if (seconds > 0) {
+        String timeSeconds = res.getQuantityString(R.plurals.seconds_left, (int) seconds, (int) seconds);
+        return res.getString(R.string.left, timeSeconds);
+      }
     }
-
-    long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
-    if (seconds > 0) {
-      String timeSeconds = res.getQuantityString(R.plurals.seconds_left, (int) seconds, (int) seconds);
-      return res.getString(R.string.left, timeSeconds);
-    }
-
-    return res.getString(R.string.closed);
+    return res.getString(R.string.poll_manual_close);
   }
 
   public String getHourWithDate(long publishTime) {
