5cb38c35f3357aafd798e3e5e1011ca8afd2f2e0
vsaldanya <victor.saldanya@fav24.com>
2017-06-13 13:08:48 +0200

    Formato correcto en el countdown de la poll

--- app/src/main/java/com/shootr/mobile/util/AndroidTimeUtils.java
+++ app/src/main/java/com/shootr/mobile/util/AndroidTimeUtils.java
@@ -13,93 +13,110 @@ import javax.inject.Inject;
 
 public class AndroidTimeUtils implements TimeUtils {
 
-    @Inject public AndroidTimeUtils() {
+  private final long MILISECONDS_ON_A_DAY = 86400000;
+  private final long MILISECONDS_ON_A_HOUR = 3600000;
+  private final long MILISECONDS_ON_A_MINUTE = 60000;
+
+  @Inject public AndroidTimeUtils() {
+  }
+
+  @Override public long getCurrentTime() {
+    long currentOffset = 0L;
+    return getSystemCurrentTime() + currentOffset;
+  }
+
+  private long getSystemCurrentTime() {
+    return System.currentTimeMillis();
+  }
+
+  public String getElapsedTime(Context context, long publishTime) {
+    long difference = getCurrentTime() - publishTime;
+
+    long days = TimeUnit.MILLISECONDS.toDays(difference);
+    Resources res = context.getResources();
+    if (days > 0) {
+      String time = res.getString(R.string.days);
+      return String.valueOf(days + time);
     }
 
-    @Override public long getCurrentTime() {
-        long currentOffset = 0L;
-        return getSystemCurrentTime() + currentOffset;
+    long hours = TimeUnit.MILLISECONDS.toHours(difference);
+    if (hours > 0) {
+      String time = res.getString(R.string.hours);
+      return String.valueOf(hours + time);
+    }
+    long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
+    if (minutes > 0) {
+      String time = res.getString(R.string.minutes);
+      return String.valueOf(minutes + time);
     }
 
-    private long getSystemCurrentTime() {
-        return System.currentTimeMillis();
+    long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
+    if (seconds > 0) {
+      String time = res.getString(R.string.seconds);
+      return String.valueOf(seconds + time);
+    }
+    return res.getString(R.string.now);
+  }
+
+  public String getPollElapsedTime(Context context, long timeToFinish) {
+
+    Resources res = context.getResources();
+
+    long difference = timeToFinish - getCurrentTime();
+    long days = TimeUnit.MILLISECONDS.toDays(difference);
+
+    if (days > 0) {
+      difference = difference - (MILISECONDS_ON_A_DAY * days);
+      long hours = TimeUnit.MILLISECONDS.toHours(difference);
+      String timeDays = res.getQuantityString(R.plurals.days_left, (int)days);
+      String timeHours = ((hours > 0) ? res.getQuantityString(R.plurals.hours_left, (int)hours) : "");
+      return timeDays + timeHours;
     }
 
-    public String getElapsedTime(Context context, long publishTime) {
-        long difference = getCurrentTime() - publishTime;
-
-        long days = TimeUnit.MILLISECONDS.toDays(difference);
-        Resources res = context.getResources();
-        if (days > 0) {
-            String time = res.getString(R.string.days);
-            return String.valueOf(days + time);
-        }
-
-        long hours = TimeUnit.MILLISECONDS.toHours(difference);
-        if (hours > 0) {
-            String time = res.getString(R.string.hours);
-            return String.valueOf(hours + time);
-        }
-        long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
-        if (minutes > 0) {
-            String time = res.getString(R.string.minutes);
-            return String.valueOf(minutes + time);
-        }
-
-        long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
-        if (seconds > 0) {
-            String time = res.getString(R.string.seconds);
-            return String.valueOf(seconds + time);
-        }
-        return res.getString(R.string.now);
+    long hours = TimeUnit.MILLISECONDS.toHours(difference);
+    if (hours > 0) {
+      difference = difference - (MILISECONDS_ON_A_HOUR * hours);
+      long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
+      String timeHours = res.getQuantityString(R.plurals.hours_left, (int)hours);
+      String timeMinutes = ((minutes > 0) ? res.getQuantityString(R.plurals.minutes_left, (int)minutes) : "");
+      return timeHours + timeMinutes;
     }
 
-    public String getPollElapsedTime(Context context, long publishTime) {
-        long difference = getCurrentTime() - publishTime;
-
-        long days = TimeUnit.MILLISECONDS.toDays(difference);
-        Resources res = context.getResources();
-        if (days > 0) {
-            String time = res.getString(R.string.days);
-            return String.valueOf(days + time);
-        }
-
-        long hours = TimeUnit.MILLISECONDS.toHours(difference);
-        if (hours > 0) {
-            String time = res.getString(R.string.hours);
-            return String.valueOf(hours + time);
-        }
-        long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
-        if (minutes > 0) {
-            String time = res.getString(R.string.minutes);
-            return String.valueOf(minutes + time);
-        }
-
-        long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
-        if (seconds > 0) {
-            String time = res.getString(R.string.seconds);
-            return String.valueOf(seconds + time);
-        }
-        return res.getString(R.string.closed);
+    long minutes = TimeUnit.MILLISECONDS.toMinutes(difference);
+    if (minutes > 0) {
+      difference = difference - (MILISECONDS_ON_A_MINUTE * minutes);
+      long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
+      String timeMinutes = res.getQuantityString(R.plurals.minutes_left, (int)minutes);
+      String timeSeconds = ((seconds > 0) ? res.getQuantityString(R.plurals.seconds_left, (int)seconds) : "");
+      return timeMinutes + timeSeconds;
     }
 
-    public String getHourWithDate(long publishTime) {
-        long difference = getCurrentTime() - publishTime;
-        long days = TimeUnit.MILLISECONDS.toDays(difference);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeZone(TimeZone.getDefault());
-        calendar.setTimeInMillis(publishTime);
-
-        if (days > 0) {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
-            return sdf.format(calendar.getTime());
-        } else {
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-            return sdf.format(calendar.getTime());
-        }
+    long seconds = TimeUnit.MILLISECONDS.toSeconds(difference);
+    if (seconds > 0) {
+      String timeSeconds = res.getQuantityString(R.plurals.seconds_left, (int)seconds);
+      return String.valueOf(seconds + timeSeconds);
     }
 
-    @Override public Date getCurrentDate() {
-        return new Date(getCurrentTime());
+    return res.getString(R.string.closed);
+  }
+
+  public String getHourWithDate(long publishTime) {
+    long difference = getCurrentTime() - publishTime;
+    long days = TimeUnit.MILLISECONDS.toDays(difference);
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeZone(TimeZone.getDefault());
+    calendar.setTimeInMillis(publishTime);
+
+    if (days > 0) {
+      SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+      return sdf.format(calendar.getTime());
+    } else {
+      SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+      return sdf.format(calendar.getTime());
     }
+  }
+
+  @Override public Date getCurrentDate() {
+    return new Date(getCurrentTime());
+  }
 }
