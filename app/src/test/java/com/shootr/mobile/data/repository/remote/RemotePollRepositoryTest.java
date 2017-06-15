b2cfeb8c9e89f5c59866e6f07b673651962a3274
Jorge <mrjem1360@gmail.com>
2017-06-15 15:42:32 +0200

    arreglo de test

--- app/src/test/java/com/shootr/mobile/data/repository/remote/RemotePollRepositoryTest.java
+++ app/src/test/java/com/shootr/mobile/data/repository/remote/RemotePollRepositoryTest.java
@@ -99,7 +99,7 @@ public class RemotePollRepositoryTest {
   @NonNull private PollEntity pollEntity() {
     PollEntity pollEntity = new PollEntity();
     pollEntity.setIdPoll(ID_POLL);
-    pollEntity.setUserHasVoted(1L);
+    pollEntity.setUserHasVoted(true);
     pollEntity.setIdStream(ID_STREAM);
     pollEntity.setPollOptions(Collections.<PollOptionEntity>emptyList());
     pollEntity.setPublished(1L);
