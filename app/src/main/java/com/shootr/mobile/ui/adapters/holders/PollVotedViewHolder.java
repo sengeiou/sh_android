b48240fc6dab46a719371427ad94dad5ff541f34
jmartinez1360 <mrjem1360@gmail.com>
2017-10-19 10:39:56 +0200

    Rediseño del pollVotedActivity

--- app/src/main/java/com/shootr/mobile/ui/adapters/holders/PollVotedViewHolder.java
+++ app/src/main/java/com/shootr/mobile/ui/adapters/holders/PollVotedViewHolder.java
@@ -32,19 +32,54 @@ public class PollVotedViewHolder extends GenericActivityViewHolder {
   }
 
   @Override protected void renderText(ActivityModel activity) {
-    text.setText(getFormattedUserName(activity));
+    try {
+      title.setText(getFormattedUserName(activity));
+      title.setVisibility(View.VISIBLE);
+      renderEmbedPollQuestion(activity);
+    } catch (Exception e) {
+      /* no-op */
+    }
+  }
+
+  private void renderEmbedPollQuestion(ActivityModel activity) {
+    text.setVisibility(View.GONE);
+    embedCard.setVisibility(View.VISIBLE);
+    embedShotImage.setVisibility(View.GONE);
+    embedUsername.setText(getFormattedPoll(activity));
+    embedShotComment.setText("Respuesta: " + activity.getPollOptionText());
+  }
+
+  private CharSequence getFormattedPoll(ActivityModel activity) {
+    return new Truss()
+        .pushSpan(new StyleSpan(Typeface.BOLD))
+        .append(activity.getPollQuestion()).popSpan()
+        .build();
   }
 
   @Override protected CharSequence getFormattedUserName(ActivityModel activity) {
     return new Truss()
         .pushSpan(new StyleSpan(Typeface.BOLD))
         .append(activity.getUsername()).popSpan()
-        .append(formatActivityComment(activity))
-        .pushSpan(new ForegroundColorSpan(gray_60))
         .append(" ")
-        .append(androidTimeUtils.getElapsedTime(getContext(), activity.getPublishDate().getTime()))
-        .popSpan()
+        .append("voted in a poll in")
+        .append(" ")
+        .pushSpan(new StyleSpan(Typeface.BOLD))
+        .append(activity.getStreamTitle()).popSpan()
         .build();
+  }
+
+  @Override protected void renderAvatar(ActivityModel activity) {
+    //TODO poner foto de stream
+    imageLoader.loadProfilePhoto(activity.getUserPhoto(), avatar, activity.getStreamTitle());
+    avatar.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        //TODO onAvatarClickListener.onAvatarClick(activity.getIdUser(), avatar);
+      }
+    });
+  }
+
+  @Override protected void rendetTargetAvatar(ActivityModel activity) {
+    /* no-op */
   }
 
   protected CharSequence formatActivityComment(final ActivityModel activity) {
