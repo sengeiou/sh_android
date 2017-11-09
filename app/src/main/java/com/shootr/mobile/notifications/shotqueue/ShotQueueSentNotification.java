65d915534f762ae1b0953f1830fddf2b109b1148
vsaldanya <victor.saldanya@fav24.com>
2017-11-09 16:32:49 +0100

    Se cambia la notificación de PM enviado

--- app/src/main/java/com/shootr/mobile/notifications/shotqueue/ShotQueueSentNotification.java
+++ app/src/main/java/com/shootr/mobile/notifications/shotqueue/ShotQueueSentNotification.java
@@ -8,38 +8,43 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import com.shootr.mobile.R;
 import com.shootr.mobile.domain.model.shot.QueuedShot;
+import com.shootr.mobile.domain.model.shot.Shot;
 import com.shootr.mobile.notifications.CommonNotification;
 import com.shootr.mobile.notifications.NotificationBuilderFactory;
 
 public class ShotQueueSentNotification extends CommonNotification {
 
-    private final QueuedShot shot;
-    private CharSequence titleText;
+  private final QueuedShot shot;
+  private CharSequence titleText;
 
-    public ShotQueueSentNotification(Context context, NotificationBuilderFactory builderFactory, QueuedShot shot) {
-        super(context, builderFactory);
-        this.shot = shot;
-        this.titleText = context.getResources().getString(R.string.notification_shot_sent);
+  public ShotQueueSentNotification(Context context, NotificationBuilderFactory builderFactory,
+      QueuedShot shot) {
+    super(context, builderFactory);
+    this.shot = shot;
+    if (shot.getBaseMessage() instanceof Shot) {
+      this.titleText = context.getResources().getString(R.string.notification_shot_sent);
+    } else {
+      this.titleText = context.getResources().getString(R.string.notification_pm_sent);
     }
+  }
 
-    @Override public void setNotificationValues(NotificationCompat.Builder builder,
-        Boolean areShotTypesKnown) {
-        builder.setContentTitle(titleText);
-        builder.setContentText(shot.getBaseMessage().getComment());
-        builder.setPriority(NotificationCompat.PRIORITY_LOW);
-        builder.setSound(null);
-        builder.setAutoCancel(true);
+  @Override
+  public void setNotificationValues(NotificationCompat.Builder builder, Boolean areShotTypesKnown) {
+    builder.setContentTitle(titleText);
+    builder.setContentText(shot.getBaseMessage().getComment());
+    builder.setPriority(NotificationCompat.PRIORITY_LOW);
+    builder.setSound(null);
+    builder.setAutoCancel(true);
         /* Empty content intent for auto-cancel to work */
-        builder.setContentIntent(PendingIntent.getActivity(getContext(), 0, new Intent(), 0));
-        builder.setColor(ContextCompat.getColor(getContext(), R.color.shootr_orange));
-    }
+    builder.setContentIntent(PendingIntent.getActivity(getContext(), 0, new Intent(), 0));
+    builder.setColor(ContextCompat.getColor(getContext(), R.color.shootr_orange));
+  }
 
-    @Override public Bitmap getLargeIcon() {
-        return null;
-    }
+  @Override public Bitmap getLargeIcon() {
+    return null;
+  }
 
-    @Override
-    protected CharSequence getTickerText() {
-        return titleText;
-    }
+  @Override protected CharSequence getTickerText() {
+    return titleText;
+  }
 }
