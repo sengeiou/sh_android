9672b6a0ada992b59baf8894d505cccb25dd7f27
jmartinez1360 <mrjem1360@gmail.com>
2017-11-29 10:40:21 +0100

    Reducimos la subida de events a server

--- app/src/main/java/com/shootr/mobile/ui/presenter/MainScreenPresenter.java
+++ app/src/main/java/com/shootr/mobile/ui/presenter/MainScreenPresenter.java
@@ -232,7 +232,6 @@ public class MainScreenPresenter implements Presenter, BadgeChanged.Receiver, Un
     bus.register(this);
     if (hasBeenPaused) {
       loadCurrentUser();
-      sendShotEventStats();
     }
   }
 
