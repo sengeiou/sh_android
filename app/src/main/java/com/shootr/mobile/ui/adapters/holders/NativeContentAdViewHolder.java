8e10f8ed1eac9ee193f94f20cc461f598dc2bb1a
jmartinez1360 <mrjem1360@gmail.com>
2018-01-10 12:42:31 +0100

    Mostramos el icono de adChoice

--- app/src/main/java/com/shootr/mobile/ui/adapters/holders/NativeContentAdViewHolder.java
+++ app/src/main/java/com/shootr/mobile/ui/adapters/holders/NativeContentAdViewHolder.java
@@ -2,26 +2,28 @@ package com.shootr.mobile.ui.adapters.holders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.facebook.ads.NativeAd;
-import com.makeramen.roundedimageview.RoundedImageView;
 import com.shootr.mobile.R;
 import com.shootr.mobile.ui.widgets.AvatarView;
+import com.shootr.mobile.ui.widgets.ProportionalImageView;
 import java.util.ArrayList;
 import java.util.List;
 
 public class NativeContentAdViewHolder extends RecyclerView.ViewHolder {
 
   @BindView(R.id.contentad_headline) TextView headline;
-  @BindView(R.id.contentad_image) RoundedImageView contentImage;
+  @BindView(R.id.contentad_image) ProportionalImageView contentImage;
   @BindView(R.id.contentad_body) TextView body;
   @BindView(R.id.contentad_call_to_action) TextView callToAction;
   @BindView(R.id.contentad_logo) AvatarView logo;
   @BindView(R.id.contentad_advertiser) TextView advertirser;
-  @BindView(R.id.ad_container) RelativeLayout container;
+  @BindView(R.id.ad_container) FrameLayout container;
+  @BindView(R.id.promoted_icon) ImageView adChoiceImage;
 
   public NativeContentAdViewHolder(View itemView) {
     super(itemView);
@@ -38,7 +40,12 @@ public class NativeContentAdViewHolder extends RecyclerView.ViewHolder {
     NativeAd.downloadAndDisplayImage(adIcon, logo);
 
     NativeAd.Image adImage = nativeContentAd.getAdCoverImage();
+    contentImage.setInitialHeight(adImage.getHeight());
+    contentImage.setInitialWidth(adImage.getWidth());
     NativeAd.downloadAndDisplayImage(adImage, contentImage);
+
+    NativeAd.Image adChoiceIcon = nativeContentAd.getAdChoicesIcon();
+    NativeAd.downloadAndDisplayImage(adChoiceIcon, adChoiceImage);
 
     List<View> clickableViews = new ArrayList<>();
 
@@ -46,6 +53,8 @@ public class NativeContentAdViewHolder extends RecyclerView.ViewHolder {
     clickableViews.add(logo);
     clickableViews.add(contentImage);
     clickableViews.add(callToAction);
+    clickableViews.add(body);
+    clickableViews.add(advertirser);
 
     nativeContentAd.registerViewForInteraction(container, clickableViews);
   }
