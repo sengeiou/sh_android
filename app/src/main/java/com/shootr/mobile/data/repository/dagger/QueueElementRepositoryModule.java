dee7a0a64a78086caf86ea0c21a5b1085af4e918
vsaldanya <victor.saldanya@fav24.com>
2018-01-19 14:24:01 +0100

    Registramos los repositorios y DataSources en Modules ...
    ...y el propio nuevo Module

---
+++ app/src/main/java/com/shootr/mobile/data/repository/dagger/QueueElementRepositoryModule.java
@@ -0,0 +1,81 @@
+package com.shootr.mobile.data.repository.dagger;
+
+import com.shootr.mobile.data.repository.MemoryStreamListSynchronizationRepository;
+import com.shootr.mobile.data.repository.datasource.stream.DatabaseMemoryStreamSearchDataSource;
+import com.shootr.mobile.data.repository.datasource.stream.DatabaseStreamDataSource;
+import com.shootr.mobile.data.repository.datasource.stream.ServiceStreamDataSource;
+import com.shootr.mobile.data.repository.datasource.stream.ServiceStreamListDataSource;
+import com.shootr.mobile.data.repository.datasource.stream.ServiceStreamSearchDataSource;
+import com.shootr.mobile.data.repository.datasource.stream.StreamDataSource;
+import com.shootr.mobile.data.repository.datasource.stream.StreamListDataSource;
+import com.shootr.mobile.data.repository.datasource.stream.StreamSearchDataSource;
+import com.shootr.mobile.data.repository.local.LocalStreamRepository;
+import com.shootr.mobile.data.repository.local.LocalStreamSearchRepository;
+import com.shootr.mobile.data.repository.remote.RemoteStreamSearchRepository;
+import com.shootr.mobile.data.repository.remote.SyncStreamRepository;
+import com.shootr.mobile.domain.repository.Local;
+import com.shootr.mobile.domain.repository.Remote;
+import com.shootr.mobile.domain.repository.stream.ExternalStreamRepository;
+import com.shootr.mobile.domain.repository.stream.InternalStreamSearchRepository;
+import com.shootr.mobile.domain.repository.stream.StreamListSynchronizationRepository;
+import com.shootr.mobile.domain.repository.stream.StreamRepository;
+import com.shootr.mobile.domain.repository.stream.StreamSearchRepository;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+
+@Module(
+  injects = {
+    LocalStreamRepository.class, SyncStreamRepository.class
+  },
+  complete = false,
+  library = true) public class StreamRepositoryModule {
+
+    @Provides @Singleton @Local StreamRepository provideLocalStreamRepository(LocalStreamRepository streamRepository) {
+        return streamRepository;
+    }
+
+    @Provides @Singleton ExternalStreamRepository provideRemoteStreamRepository(SyncStreamRepository streamRepository) {
+        return streamRepository;
+    }
+
+    @Provides @Singleton @Local StreamDataSource provideLocalStreamDataSource(
+      DatabaseStreamDataSource streamDataSource) {
+        return streamDataSource;
+    }
+
+    @Provides @Singleton @Remote StreamDataSource provideRemoteStreamDataSource(
+      ServiceStreamDataSource streamDataSource) {
+        return streamDataSource;
+    }
+
+    @Provides InternalStreamSearchRepository provideLocalStreamSearchRepository(
+      LocalStreamSearchRepository streamSearchRepository) {
+        return streamSearchRepository;
+    }
+
+    @Provides @Remote StreamSearchRepository provideLocalStreamSearchRepository(
+      RemoteStreamSearchRepository streamSearchRepository) {
+        return streamSearchRepository;
+    }
+
+    @Provides @Remote StreamListDataSource provideRemoteStreamListDataSource(
+      ServiceStreamListDataSource serviceStreamListDataSource) {
+        return serviceStreamListDataSource;
+    }
+
+    @Provides @Remote StreamSearchDataSource provideRemoteStreamSearchDataSource(
+      ServiceStreamSearchDataSource serviceStreamSearchDataSource) {
+        return serviceStreamSearchDataSource;
+    }
+
+    @Provides @Local StreamSearchDataSource provideLocalStreamSearchDataSource(
+      DatabaseMemoryStreamSearchDataSource serviceStreamSearchDataSource) {
+        return serviceStreamSearchDataSource;
+    }
+
+    @Provides @Singleton StreamListSynchronizationRepository provideStreamListSynchronizationRepository(
+      MemoryStreamListSynchronizationRepository memoryStreamListSynchronizationRepository) {
+        return memoryStreamListSynchronizationRepository;
+    }
+}
