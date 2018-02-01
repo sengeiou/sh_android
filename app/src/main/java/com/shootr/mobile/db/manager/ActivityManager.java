470dabcaeb8dd237d10b04be04f5409a0b817385
vsaldanya <victor.saldanya@fav24.com>
2018-02-01 18:23:11 +0100

    Fix manager activities (not meActivities)

--- app/src/main/java/com/shootr/mobile/db/manager/ActivityManager.java
+++ app/src/main/java/com/shootr/mobile/db/manager/ActivityManager.java
@@ -115,11 +115,11 @@ public class ActivityManager extends AbstractManager {
   }
 
   private void updateFollowOnStreamActivities(boolean isFollow, String idStream) {
-    String whereClause = DatabaseContract.MeActivityTable.ID_STREAM + " = ?";
+    String whereClause = DatabaseContract.ActivityTable.ID_STREAM + " = ?";
     String[] whereArguments = new String[] { String.valueOf(idStream) };
     ContentValues values = new ContentValues(1);
-    values.put(DatabaseContract.MeActivityTable.IS_FOLLOWING, isFollow ? 1 : 0);
-    getWritableDatabase().update(DatabaseContract.MeActivityTable.TABLE, values, whereClause,
+    values.put(DatabaseContract.ActivityTable.IS_FOLLOWING, isFollow ? 1 : 0);
+    getWritableDatabase().update(DatabaseContract.ActivityTable.TABLE, values, whereClause,
         whereArguments);
   }
 }
